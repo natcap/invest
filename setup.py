baf15579a723bc2f5acc45e8724e74032ae2177d
James Douglass <jdouglass@stanford.edu>
2015-06-23 16:55:21 -0700

    Adding a note to setup.py (first commit on develop.)

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ InVEST - Integrated Valuation of Ecosystem Services and Tradeoffs
 
 Common functionality provided by setup.py:
     build_sphinx
+
+For other commands, try `python setup.py --help-commands`
 """
 
 import os
