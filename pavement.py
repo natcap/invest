70a9edcdbb0bb9192f61fe1b268bd8b4615a79d4
James Douglass <jdouglass@stanford.edu>
2015-06-25 10:32:18 -0700

    Correcting the SVN repo path

--- pavement.py
+++ pavement.py
@@ -129,7 +129,7 @@ class GitRepository(Repository):
 REPOS_DICT = {
     'users-guide': HgRepository('doc/users-guide', 'http://code.google.com/p/invest-natcap.users-guide'),
     'pygeoprocessing': HgRepository('src/pygeoprocessing', 'https://bitbucket.org/richpsharp/pygeoprocessing'),
-    'invest-data': SVNRepository('data/invest-data', 'http://ncp-yamato.stanford.edu/svn/sample_repo'),
+    'invest-data': SVNRepository('data/invest-data', 'http://ncp-yamato.stanford.edu/svn/invest-sample-data'),
     'invest-2': HgRepository('src/invest-natcap.default', 'http://code.google.com/p/invest-natcap'),
 }
 REPOS = REPOS_DICT.values()
