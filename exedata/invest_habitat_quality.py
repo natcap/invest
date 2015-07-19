ea7ab9099967201d9d116f12684277607f245291
Will Bierbower <wbierbow@stanford.edu>
2015-07-19 15:19:22 -0700

    removing exedata folder

--- exedata/invest_habitat_quality.py
+++
@@ -1,4 +0,0 @@
-import natcap.invest.iui.modelui
-
-if __name__ == '__main__':
-    natcap.invest.iui.modelui.main('habitat_quality.json')
--- exedata/invest_marine_water_quality_biophysical.py
+++
@@ -1,4 +0,0 @@
-import natcap.invest.iui.modelui
-
-if __name__ == '__main__':
-    natcap.invest.iui.modelui.main('marine_water_quality_biophysical.json')
