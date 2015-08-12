3684809194b374e43f43434f824ae953f1e89d7f
Rich Sharp <richpsharp@gmail.com>
2015-08-12 11:32:43 -0700

    re #BITBUCKET-3385 making stock aboveground carbon act like a density by multiplying it by the Ha pixel size of the landcover map.

--- src/natcap/invest/carbon_edge_effect/carbon_edge_effect.py
+++ src/natcap/invest/carbon_edge_effect/carbon_edge_effect.py
@@ -1,10 +1,17 @@
 """InVEST Carbon Edge Effect Model"""
 
 import os
+import logging
 
 import numpy
 import gdal
 import pygeoprocessing
+
+logging.basicConfig(format='%(asctime)s %(name)-18s %(levelname)-8s \
+    %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('natcap.invest.carbon_edge_effect')
+
 
 def execute(args):
     """InVEST Carbon Edge Model calculates the carbon due to edge effects in
@@ -23,11 +30,17 @@ def execute(args):
                 'is_forest': either 0 or 1 indicating whether the landcover type
                     is forest (1) or not (0)
 
-                'c_above': floating point number indicating the carbon density
-                    per unit area for that landcover type
-        args['lulc_uri'] (string): path to a integer landcover code raster
+                'c_above': floating point number indicating tons of carbon per
+                    hectare for that landcover type
 
-    """
+                Example:
+                    lucode, is_forest, c_above
+                    0,0,32.8
+                    1,1,n/a
+                    2,1,n/a
+                    16,0,28.1
+
+        args['lulc_uri'] (string): path to a integer landcover code raster"""
 
     pygeoprocessing.create_directories([args['workspace_dir']])
     try:
@@ -39,33 +52,37 @@ def execute(args):
 
     #TASK: (optional) clip dataset to AOI if it exists
 
-    #TASK: classify forest pixels from lulc
+    #classify forest pixels from lulc
     biophysical_table = pygeoprocessing.get_lookup_from_table(
         args['biophysical_table_uri'], 'lucode')
 
-    lucode_to_carbon = {}
+    lucode_to_per_pixel_carbon = {}
     forest_codes = []
+    cell_area_ha = pygeoprocessing.geoprocessing.get_cell_size_from_uri(
+        args['lulc_uri']) ** 2 / 10000.0
+    LOGGER.debug("cell area in ha %f" % cell_area_ha)
+
     for lucode in biophysical_table:
         try:
             is_forest = int(biophysical_table[int(lucode)]['is_forest'])
             if is_forest == 1:
                 forest_codes.append(lucode)
-            lucode_to_carbon[int(lucode)] = float(
-                biophysical_table[lucode]['c_above'])
+            lucode_to_per_pixel_carbon[int(lucode)] = float(
+                biophysical_table[lucode]['c_above']) * cell_area_ha
         except ValueError:
             #this might be because the c_above parameter is n/a or undefined
             #because of forest
-            lucode_to_carbon[int(lucode)] = 0.0
+            lucode_to_per_pixel_carbon[int(lucode)] = 0.0
 
-    #TASK: map aboveground carbon from table to lulc that is not forest
+    #map aboveground carbon from table to lulc that is not forest
     carbon_map_nodata = -1
     non_edge_carbon_map_uri = os.path.join(
         args['workspace_dir'], 'non_edge_carbon_map%s.tif' % file_suffix)
     pygeoprocessing.reclassify_dataset_uri(
-        args['lulc_uri'], lucode_to_carbon, non_edge_carbon_map_uri,
+        args['lulc_uri'], lucode_to_per_pixel_carbon, non_edge_carbon_map_uri,
         gdal.GDT_Float32, carbon_map_nodata)
 
-    #TASK: map distance to edge
+    #map distance to edge
     forest_mask_uri = os.path.join(
         args['workspace_dir'], 'non_forest_mask%s.tif' % file_suffix)
     forest_mask_nodata = 255
@@ -83,8 +100,7 @@ def execute(args):
 
     edge_distance_uri = os.path.join(
         args['workspace_dir'], 'edge_distance%s.tif' % file_suffix)
-    pygeoprocessing.distance_transform_edt(
-        forest_mask_uri, edge_distance_uri, process_pool=None)
+    pygeoprocessing.distance_transform_edt(forest_mask_uri, edge_distance_uri)
 
     #TASK rasterize points into a raster!
 
