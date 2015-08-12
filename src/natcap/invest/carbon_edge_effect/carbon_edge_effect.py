4e4f0325eb1ba37139db21476bd464c501e189fb
Rich Sharp <richpsharp@gmail.com>
2015-08-12 15:54:42 -0700

    re #BITBUCKET-3385 adding the edge effect calculation so it handles all three methods and behaves well in areas that aren't defined as forest pixels

--- src/natcap/invest/carbon_edge_effect/carbon_edge_effect.py
+++ src/natcap/invest/carbon_edge_effect/carbon_edge_effect.py
@@ -107,7 +107,7 @@ def execute(args):
         args['workspace_dir'], 'edge_distance%s.tif' % file_suffix)
     pygeoprocessing.distance_transform_edt(forest_mask_uri, edge_distance_uri)
 
-    #TASK rasterize points into a raster!
+    #rasterize: method, theta 1-3 into rasters that can be vectorized
     carbon_model_reproject_uri = os.path.join(
         args['workspace_dir'], 'local_carbon_shape.shp')
 
@@ -118,19 +118,63 @@ def execute(args):
         args['carbon_model_shape_uri'], lulc_projection_wkt,
         carbon_model_reproject_uri)
 
+    model_raster_uris = {}
     for field_id, datatype, field_raster_nodata in [
             ('method', gdal.GDT_Byte, 255),
             ('theta1', gdal.GDT_Float32, -9999),
             ('theta2', gdal.GDT_Float32, -9999),
             ('theta3', gdal.GDT_Float32, -9999)]:
         raster_uri = os.path.join(args['workspace_dir'], field_id + '.tif')
+        model_raster_uris[field_id] = raster_uri
         pygeoprocessing.new_raster_from_base_uri(
             args['lulc_uri'], raster_uri, 'GTiff', field_raster_nodata,
             datatype, fill_value=field_raster_nodata)
         pygeoprocessing.rasterize_layer_uri(
             raster_uri, carbon_model_reproject_uri,
             option_list=['ATTRIBUTE=%s' % field_id])
-    #rasterize: method, theta 1-3
+
+    #TASK: calculate edge effect carbon raster
+    carbon_edge_nodata = -9999.0
+    cell_size_in_meters = pygeoprocessing.get_cell_size_from_uri(
+        args['lulc_uri'])
+    def carbon_edge_op(
+            edge_distance, method, theta_1, theta_2, theta_3, forest_mask):
+        """calculate carbon model
+        Args:
+
+            edge_distance (numpy.array): distance from forest edge in pixels
+            method (numpy.array): values with 1, 2, or 3 indicating the three
+                model regression types
+            theta_{1,2,3} (numpy.array): parameters to regression that have
+                different meaning depending on which method
+            forest_mask (numpy.array): used to determine where this model is
+                valid so we can mask out the rest as nodata
+            """
+        nodata_mask = (forest_mask) | (edge_distance == 0)
+        edge_distance_km = edge_distance * (cell_size_in_meters / 1000.0)
+        #asymtotic model
+        biomass_1 = theta_1 - theta_2 * numpy.exp(-theta_3 * edge_distance_km)
+        #logarithmic model
+        biomass_2 = theta_1 + theta_2 * numpy.log(edge_distance_km)
+        #linear regression
+        biomass_3 = theta_1 + theta_2 * edge_distance_km
+
+        result = numpy.where(method == 1, biomass_1, carbon_edge_nodata)
+        result = numpy.where(method == 2, biomass_2, result)
+        result = numpy.where(method == 3, biomass_3, result)
+
+        return numpy.where(nodata_mask, carbon_edge_nodata, result)
+
+    edge_carbon_map_uri = os.path.join(
+        args['workspace_dir'], 'edge_carbon_map%s.tif' % file_suffix)
+    pygeoprocessing.vectorize_datasets(
+        [edge_distance_uri, model_raster_uris['method'],
+         model_raster_uris['theta1'], model_raster_uris['theta2'],
+         model_raster_uris['theta3'], forest_mask_uri], carbon_edge_op,
+        edge_carbon_map_uri, gdal.GDT_Float32, carbon_edge_nodata,
+        cell_size_in_meters, 'intersection', vectorize_op=False,
+        datasets_are_pre_aligned=True)
+
 
     #TASK: combine maps into output
     carbon_map_uri = os.path.join(
