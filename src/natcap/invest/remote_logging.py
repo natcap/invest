b87468962ab7904285c3276b69c13b7c3f895b97
Rich Sharp <richpsharp@gmail.com>
2015-08-20 11:42:48 -0700

    re #BITBUCKET-3398 getting local time from the server rather than the client.  also removing many unused functions

--- src/natcap/invest/remote_logging.py
+++ src/natcap/invest/remote_logging.py
@@ -3,11 +3,10 @@ Functions to assist with remote logging of InVEST usage.
 """
 
 import os
+import datetime
 import platform
 import sys
 import locale
-import hashlib
-import json
 import time
 from types import DictType
 from types import ListType
@@ -28,191 +27,6 @@ logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
 
 LOGGER = logging.getLogger('natcap.invest.remote_logging')
-
-def _hash_data(data):
-    """
-    Create an md5sum hash of the input data.
-    """
-    try:
-        md5 = hashlib.md5()
-        md5.update(json.dumps(data))
-        return md5.hexdigest()
-    except:
-        return None
-
-def _user_hash():
-    """Returns a hash for the user, based on the machine."""
-    data = {
-        'os': platform.platform(),
-        'hostname': platform.node(),
-        'userdir': os.path.expanduser('~')
-    }
-    return _hash_data(data)
-
-def log_model(model_name, args_dict):
-    data = {
-        'model_name': model_name,
-        'invest_release': natcap.invest.__version__,
-        'user': _user_hash(),
-        'system': {
-            'os': platform.system(),
-            'release': platform.release(),
-            'full_platform_string': platform.platform(),
-            'fs_encoding': sys.getfilesystemencoding(),
-            'preferred_encoding': locale.getdefaultlocale()[1],
-            'default_language': locale.getdefaultlocale()[0],
-            'python': {
-                'version': platform.python_version(),
-                'bits': platform.architecture()[0],
-            },
-        },
-        "args": log_args(args_dict),
-    }
-    return data
-
-def _format_gdal(gdal_path):
-    raster = gdal.Open(gdal_path)
-    rasterband = raster.GetRasterBand(1)
-
-    geotransform = raster.GetGeoTransform()
-    driver = raster.GetDriver()
-
-    raster_data = {
-        "cols": raster.RasterXSize,
-        "rows": raster.RasterYSize,
-        "pixelwidth": float(geotransform[1]),
-        "pixelheight": float(geotransform[5]),
-        "nodata": rasterband.GetNoDataValue(),
-        "bbox": pygeoprocessing.get_bounding_box(gdal_path),
-        "projection": pygeoprocessing.get_dataset_projection_wkt_uri(gdal_path),
-        "driver": driver.ShortName,
-        "type": {
-            "gdal": rasterband.DataType,
-            "numpy": geoprocessing._gdal_to_numpy_type(rasterband).__name__,
-        }
-    }
-    return raster_data
-
-def _format_ogr(ogr_path):
-    vector = ogr.Open(ogr_path)
-    layer = vector.GetLayer()
-    driver = vector.GetDriver()
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(layer.GetSpatialRef().__str__())
-
-    vector_data = {
-        "projection": srs.ExportToWkt(),
-        "features": layer.GetFeatureCount(),
-        "driver": driver.name,
-        "bbox": pygeoprocessing.get_datasource_bounding_box(ogr_path),
-    }
-    return vector_data
-
-def _is_file(filepath):
-    if os.path.exists(filepath):
-        return True
-    return False
-
-def _is_gdal(filepath):
-    if _is_file(filepath):
-        raster = gdal.Open(filepath)
-        if raster is not None:
-            return True
-    return False
-
-def _is_ogr(filepath):
-    if _is_file(filepath):
-        vector = ogr.Open(filepath)
-        if vector is not None:
-            return True
-    return False
-
-    # CSV files can be parsed by OGR, so guard against it.
-    driver = vector.GetDriver()
-    if driver.name == 'CSV':
-        return False
-    return True
-
-def log_args(args_dict):
-    """
-    Parse through an args dict and extract interesting spatial information.
-    """
-
-    test_funcs = [
-        (_is_gdal, _format_gdal, lambda x: 'gdal'),
-        (_is_ogr, _format_ogr, lambda x: 'ogr'),
-        (lambda x: True, lambda x: x, lambda x: type(x).__name__)
-    ]
-
-    def recurse_args(arg):
-        out_args_dict = {}
-        if type(arg) is DictType:
-            for key, value in arg.iteritems():
-                out_args_dict[key] = recurse_args(value)
-        elif type(arg) is ListType:
-            out_args_list = []
-            for list_value in arg:
-                out_args_list.append(recurse_args(list_value))
-            return out_args_list
-        else:
-            for test_func, format_func, typestring in test_funcs:
-                if test_func(arg) is True:
-                    return {
-                        'type': typestring(arg),
-                        'value': arg,
-                        'metadata': format_func(arg),
-                    }
-        return out_args_dict
-
-    return recurse_args(args_dict)
-
-def get_session_id():
-    data = {
-        'user_hash': _user_hash(),
-        'time': str(time.time()),
-    }
-    return '-'.join(data.values())
-
-def sanitize(dirty_args):
-    """
-    Remove data we might not want people to see or know we collect.
-    """
-    new_args = {}
-    for key, value in dirty_args.iteritems():
-        if key in ['args']:
-           continue
-        new_args[key] = value
-    return new_args
-
-
-def _default_payload(event, session_id, data):
-    default_data = {
-        "type": event,
-        "user_time": time.time(),
-        "session_id": session_id,
-        "api": "2.0",
-        "data": data,
-    }
-    return default_data
-
-def format_start(model_name, args, session_id):
-    model_data = log_model(model_name, args)
-    return _default_payload('start', session_id, model_data)
-
-
-def format_success(session_id):
-    return _default_payload('success', session_id, None)
-
-
-def format_exception(exception, session_id):
-    data = {
-        "type": exception.__class__.__name__,
-        # Limit the number of stack trace entries
-        "traceback": traceback.format_exc(limit=20),
-        "text": exception.message,
-        "args": list(exception.args),
-    }
-    return _default_payload('success', session_id, data)
 
 
 class LoggingServer(object):
@@ -241,7 +55,7 @@ class LoggingServer(object):
             None."""
 
         self.database_filepath = database_filepath
-        #make the directory if it doesn't exist and isn't the current directory
+        # make the directory if it doesn't exist and isn't the current directory
         filepath_directory = os.path.dirname(self.database_filepath)
         if filepath_directory != '' and not os.path.exists(filepath_directory):
             os.mkdir(os.path.dirname(self.database_filepath))
@@ -269,14 +83,18 @@ class LoggingServer(object):
 
         Returns:
             None."""
+
         try:
-            #Add info about the client's IP
+            # Add info about the client's IP
             data_copy = data.copy()
             data_copy['ip_address'] = (
                 Pyro4.current_context.client.sock.getpeername()[0])
-            #Get data into the same order as the field names
-            ordered_data = [data_copy[field_id] for field_id in self._FIELD_NAMES]
-            #get as many '?'s as there are fields for the insert command
+            data_copy['time'] = datetime.datetime.now().isoformat(' ')
+
+            # Get data into the same order as the field names
+            ordered_data = [
+                data_copy[field_id] for field_id in self._FIELD_NAMES]
+            # get as many '?'s as there are fields for the insert command
             position_format = ','.join(['?'] * len(self._FIELD_NAMES))
 
             insert_command = (
@@ -286,12 +104,12 @@ class LoggingServer(object):
 
             db_connection = sqlite3.connect(self.database_filepath)
             db_cursor = db_connection.cursor()
-            #pass in ordered_data to the command
+            # pass in ordered_data to the command
             db_cursor.execute(insert_command, ordered_data)
             db_connection.commit()
             db_connection.close()
         except:
-            #print something locally for our log and raise back to client
+            # print something locally for our log and raise back to client
             traceback.print_exc()
             raise
         extra_fields = set(data_copy).difference(self._FIELD_NAMES)
@@ -311,7 +129,7 @@ def launch_logging_server(database_filepath, hostname, port):
         port (int): TCP port to bind to
 
     Returns:
-        None"""
+        never"""
 
     daemon = Pyro4.Daemon(hostname, port)
     uri = daemon.register(
