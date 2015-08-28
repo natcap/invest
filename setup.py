7520a11c5d472bb56807b8851f4448e6c3c8f4b7
Rich Sharp <richpsharp@gmail.com>
2015-08-28 09:30:10 -0700

    re #BITBUCKET-3400 changed the visible name of the tool toe Agricultre Expansion and updated setup.py to note the new package name

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ setup(
         'natcap.invest.blue_carbon',
         'natcap.invest.carbon',
         'natcap.invest.coastal_vulnerability',
-        'natcap.invest.cropland_expansion',
+        'natcap.invest.ag_expansion',
         'natcap.invest.dbfpy',
         'natcap.invest.finfish_aquaculture',
         'natcap.invest.fisheries',
