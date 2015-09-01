f69832bf04175414cf7f4f727c3ca16b5c398865
Will Bierbower <wbierbow@stanford.edu>
2015-09-01 14:32:40 -0700

    editing scipy hook so that jenkins build won't crash

--- exe/hooks/hook-scipy.py
+++ exe/hooks/hook-scipy.py
@@ -1,1 +1,2 @@
-hiddenimports = ['scipy.special._ufuncs_cxx', 'scipy.io.matlab.streams', 'scipy.sparse.cgraph._validation']
+from PyInstaller.hooks.hookutils import collect_submodules
+hiddenimports = ['scipy.special._ufuncs_cxx', 'scipy.io.matlab.streams', 'scipy.sparse.cgraph._validation'] + collect_submodules('scipy.linalg')
