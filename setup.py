4ef1e5111da4d9e2e3df245d042cd5d8a71d0a0b
James Douglass <jdouglass@stanford.edu>
2015-09-25 14:21:05 -0700

    Adding an example model and a couple tests

--- setup.py
+++ setup.py
@@ -147,6 +147,7 @@ setup(
         'natcap.invest.sdr',
         'natcap.invest.seasonal_water_yield',
         'natcap.invest.testing',
+        'natcap.invest.tests',
         'natcap.invest.timber',
         'natcap.invest.wave_energy',
         'natcap.invest.wind_energy',
---
+++ src/natcap/invest/tests/test_example.py
@@ -0,0 +1,101 @@
+import unittest
+import tempfile
+import shutil
+import os
+
+from osgeo import gdal
+import numpy
+import numpy.testing
+import pygeoprocessing.testing
+from pygeoprocessing.testing import sampledata
+
+
+def _create_lulc(matrix=None):
+    """
+    Create an LULC for the _example_model.
+
+    This LULC is created with the following characteristics:
+        * SRS is in the SRS_WILLAMETTE.
+        * Nodata is -1.
+        * Raster type is `gdal.GDT_Int32`
+        * Pixel size is 30m
+
+    Parameters:
+        matrix=None (numpy.array): A numpy array to use as a landcover matrix.
+            The output raster created will be saved with these pixel values.
+            If None, a default matrix will be used.
+
+    Returns:
+        A string filepath to a new LULC raster on disk.
+    """
+    if matrix is None:
+        lulc_matrix = numpy.array([
+            [0, 1, 2, 3, 4],
+            [1, 2, 4, 5, 11],
+            [1, -1, -1, -1, -1],
+            [0, 1, 2, 3, 4]], numpy.int32)
+    else:
+        lulc_matrix = matrix
+    lulc_nodata = -1
+    srs = sampledata.SRS_WILLAMETTE
+    return pygeoprocessing.testing.create_raster_on_disk(
+        [lulc_matrix], srs.origin, srs.projection, lulc_nodata, srs.pixel_size(30),
+        datatype=gdal.GDT_Int32)
+
+
+class ExampleUnitTests(unittest.TestCase):
+    def test_execute(self):
+        """
+        Example execution to ensure correctness when called via execute()
+        """
+        from natcap.invest import _example_model
+
+        args = {
+            'workspace_uri': tempfile.mkdtemp(),
+            'example_lulc': _create_lulc(),
+        }
+        _example_model.execute(args)
+
+        expected_matrix = numpy.array([
+            [0, 6, 7, 8, 9],
+            [6, 7, 9, 10, 11],
+            [6, -1, -1, -1, -1],
+            [0, 6, 7, 8, 9]], numpy.int32)
+        expected_raster = _create_lulc(expected_matrix)
+        sum_raster = os.path.join(args['workspace_uri'], 'sum.tif')
+        pygeoprocessing.testing.assert_rasters_equal(sum_raster, expected_raster)
+
+        shutil.rmtree(args['workspace_uri'])
+        for filename in [args['example_lulc'], expected_raster]:
+            os.remove(filename)
+
+    def test_execute_with_suffix(self):
+        """
+        When a suffix is added, verify it's added correctly.
+        """
+        from natcap.invest import _example_model
+        args = {
+            'workspace_uri': tempfile.mkdtemp(),
+            'example_lulc': _create_lulc(),
+            'suffix': 'foo',
+        }
+        _example_model.execute(args)
+
+        self.assertTrue(os.path.exists(os.path.join(args['workspace_uri'],
+                                                    'sum_foo.tif')))
+
+    def test_execute_with_suffix_and_underscore(self):
+        """
+        When the user's suffix has an underscore, don't add another one.
+        """
+        from natcap.invest import _example_model
+        args = {
+            'workspace_uri': tempfile.mkdtemp(),
+            'example_lulc': _create_lulc(),
+            'suffix': 'foo',
+        }
+        _example_model.execute(args)
+
+        self.assertTrue(os.path.exists(os.path.join(args['workspace_uri'],
+                                                    'sum_foo.tif')))
+
