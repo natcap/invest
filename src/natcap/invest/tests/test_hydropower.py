d2db8f75f976224e5595c9b8668cd88cbd503552
dcdenu4 <devnull@localhost>
2015-10-20 09:08:03 -0600

    re #BITBUCKET-3430 hashing out test for execute function. adding _create_csv function to generate csv files. Adding _create_raster function for making sample rasters

--- src/natcap/invest/tests/test_hydropower.py
+++ src/natcap/invest/tests/test_hydropower.py
@@ -1,14 +1,8 @@
-"""Tests for natcap.invest.hydropower_water_yeild.
+"""Tests for natcap.invest.hydropower.hydropower_water_yeild.
 
-This module is intended only to demonstrate how a model might be tested given
-common needs of InVEST models.  Key things to note:
-
-    * Sample data creation that's repeated outside a test method has its own
-      function.
-    * The module-under-test (natcap.invest._example_model) is imported in
-      each test method.
-    * Each test method is responsible for the appropriate cleanup of created
-      sample data and workspaces.
+    This module is intended for unit testing of the hydropower_water_yield
+    model. The goal is to thoroughly test each of the modules functions
+    and verify correctness.
 """
 import unittest
 import tempfile
@@ -25,9 +19,9 @@ from pygeoprocessing.testing import sampledata
 from shapely.geometry import Polygon
 from nose.tools import nottest
 
-def _create_watershed(fields=None, attributes=None):
+def _create_watershed(fields=None, attributes=None, subshed=True):
     """
-    Create a watershed shapefile of polygons
+    Create a watershed shapefile of 3 polygons
 
     This Watershed Shapefile is created with the following characteristis:
         * SRS is in the SRS_WILLAMETTE.
@@ -46,14 +40,21 @@ def _create_watershed(fields=None, attributes=None):
             fieldname to field value.  The field value's type must match the
             type defined in the fields input.  It is an error if it doesn't.
 
+        subshed (boolean): True or False depicting whether the created vector
+            should be representative of a sub watershed or watershed.
+            Watershed will return one polygon, sub watershed will return 3.
+
+    Returns:
+        A string filepath to the vector on disk
     """
 
     srs = sampledata.SRS_WILLAMETTE
 
-    pos_x = 443800.0
-    pos_y = 4957000.0
+    pos_x = srs.origin[0]
+    pos_y = srs.origin[1]
 
-    poly_geoms = {
+    if subshed:
+        poly_geoms = {
                  'poly_1': [(pos_x, pos_y), (pos_x + 100, pos_y),
                             (pos_x, pos_y - 100), (pos_x + 100, pos_y - 100)],
                  'poly_2': [(pos_x + 100, pos_y), (pos_x + 200, pos_y),
@@ -61,42 +62,166 @@ def _create_watershed(fields=None, attributes=None):
                  'poly_3': [(pos_x, pos_y - 100), (pos_x + 100, pos_y -100),
                             (pos_x, pos_y - 200), (pos_x + 100, pos_y - 200)]}
 
+        geometries = [Polygon(poly_geoms['poly_1']), Polygon(poly_geoms['poly_2']),
+                        Polygon(poly_geoms['poly_3'])]
 
+    else:
+        poly_geoms = {
+                 'poly_1': [(pos_x, pos_y), (pos_x + 200, pos_y),
+                            (pos_x, pos_y - 200), (pos_x + 200, pos_y - 200)]}
 
-
-    geometries = [Polygon(poly_geoms['poly_1']), Polygon(poly_geoms['poly_2']),
-                    Polygon(poly_geoms['poly_3'])]
+        geometries = [Polygon(poly_geoms['poly_1'])]
 
     return pygeoprocessing.testing.create_vector_on_disk(
 		    geometries, srs.projection, fields, attributes,
 		    vector_format='ESRI Shapefile')
 
+def _create_csv(fields, data):
+    """Create a new csv table from a dictionary
+
+        filename - a URI path for the new table to be written to disk
+
+        fields - a python list of the column names. The order of the fields in
+            the list will be the order in how they are written. ex:
+            ['id', 'precip', 'total']
+
+        data - a python dictionary representing the table. The dictionary
+            should be constructed with unique numerical keys that point to a
+            dictionary which represents a row in the table:
+            data = {0 : {'id':1, 'precip':43, 'total': 65},
+                    1 : {'id':2, 'precip':65, 'total': 94}}
+
+        returns - nothing
+    """
+    temp, filename = tempfile.mkstemp(suffix='.csv')
+    os.close(temp)
+
+    csv_file = open(filename, 'wb')
+
+    #  Sort the keys so that the rows are written in order
+    row_keys = data.keys()
+    row_keys.sort()
+
+    csv_writer = csv.DictWriter(csv_file, fields)
+    #  Write the columns as the first row in the table
+    csv_writer.writerow(dict((fn, fn) for fn in fields))
+
+    # Write the rows from the dictionary
+    for index in row_keys:
+        csv_writer.writerow(data[index])
+
+    csv_file.close()
+
+    return filename
+
+def _create_raster(matrix=None, dtype=gdal.GDT_Int32):
+    """
+    Create a raster for the hydropower_water_yield model.
+
+    This raster is created with the following characteristics:
+        * SRS is in the SRS_WILLAMETTE.
+        * Nodata is -1.
+        * Raster type is `gdal.GDT_Int32`
+        * Pixel size is 30m
+
+    Parameters:
+        matrix=None (numpy.array): A numpy array to use as a landcover matrix.
+            The output raster created will be saved with these pixel values.
+            If None, a default matrix will be used.
+
+    Returns:
+        A string filepath to a new LULC raster on disk.
+    """
+    if matrix is None:
+        lulc_matrix = numpy.array([
+            [0, 1, 2, 3, 4],
+            [1, 2, 4, 5, 11],
+            [1, -1, -1, -1, -1],
+            [0, 1, 2, 3, 4]], numpy.int32)
+    else:
+        lulc_matrix = matrix
+    lulc_nodata = -1
+    srs = sampledata.SRS_WILLAMETTE
+    return pygeoprocessing.testing.create_raster_on_disk(
+        [lulc_matrix], srs.origin, srs.projection, lulc_nodata,
+        srs.pixel_size(30), datatype=dtype)
+
 class HydropowerUnitTests(unittest.TestCase):
 
-    """Tests for natcap.hydropower."""
-    @nottest
+    """Tests for natcap.invest.hydropower.hydropower_water_yeild"""
     def test_execute(self):
         """Example execution to ensure correctness when called via execute."""
-        from natcap.invest import _example_model
+        from natcap.invest.hydropower import hydropower_water_yield
+        lulc_matrix = numpy.array([
+            [0, 1],
+            [0, 2]], numpy.int32)
+        root_matrix = numpy.array([
+            [10, 10],
+            [10, 10]], numpy.float32)
+        precip_matrix = numpy.array([
+            [10, 10],
+            [10, 10]], numpy.float32)
+        eto_matrix = numpy.array([
+            [10, 10],
+            [10, 10]], numpy.float32)
+        pawc_matrix = numpy.array([
+            [10, 10],
+            [10, 10]], numpy.float32)
+
+        fields_ws = {'ws_id': 'int'}
+        fields_sub = {'subws_id': 'int'}
+        attr_ws = [{'ws_id': 1}]
+        attr_sub = [
+                {'subws_id': 1}, {'subws_id': 2}]
+
+        bio_fields = ['lucode', 'Kc', 'root_depth', 'LULC_veg']
+
+        bio_data = {0: {'lucode':0, 'Kc': 0.3, 'root_depth': 50, 'LULC_veg': 0},
+                1: {'lucode':1, 'Kc': 0.3, 'root_depth': 50, 'LULC_veg': 0},
+                2: {'lucode':2, 'Kc': 0.85, 'root_depth': 150, 'LULC_veg': 1}}
+
+        demand_fields = ['lucode', 'demand']
+
+        demand_data = {0: {'lucode':0, 'demand': 50},
+                1: {'lucode':1, 'demand': 50},
+                2: {'lucode':2, 'demand': 0}}
+
+        val_fields = ['ws_id', 'time_span', 'discount', 'efficiency',
+                        'fraction', 'cost', 'height', 'kw_price']
+
+        val_data = {0: {'ws_id': 1, 'time_span': 20, 'discount': 5, 'efficiency': 0.8,
+                        'fraction': 0.6, 'cost': 0, 'height': 25, 'kw_price': 0.07}}
+
         args = {
             'workspace_dir': tempfile.mkdtemp(),
-            'example_lulc': _create_lulc(),
+            'lulc_uri': _create_raster(lulc_matrix),
+            'depth_to_root_rest_layer_uri': _create_raster(root_matrix, gdal.GDT_Float32),
+            'precipitation_uri': _create_raster(precip_matrix, gdal.GDT_Float32),
+            'pawc_uri': _create_raster(pawc_matrix, gdal.GDT_Float32),
+            'eto_uri': _create_raster(eto_matrix, gdal.GDT_Float32),
+            'watersheds_uri': _create_watershed(fields=fields_ws, attributes=attr_ws, subshed=False),
+            'sub_watersheds_uri': _create_watershed(fields=fields_sub, attributes=attr_sub, subshed=True),
+            'biophysical_table_uri': _create_csv(bio_fields, bio_data),
+            'seasonality_constant': 5,
+            'results_suffix': '',
+            'demand_table_uri': _create_csv(demand_fields, demand_data),
+            'valuation_table_uri': _create_csv(val_fields, val_data),
+            'water_scarcity_container': True,
+            'valuation_container': True
         }
-        _example_model.execute(args)
+        hydropower_water_yield.execute(args)
 
-        expected_matrix = numpy.array([
-            [0, 6, 7, 8, 9],
-            [6, 7, 9, 10, 11],
-            [6, -1, -1, -1, -1],
-            [0, 6, 7, 8, 9]], numpy.int32)
-        expected_raster = _create_lulc(expected_matrix)
-        sum_raster = os.path.join(args['workspace_dir'], 'sum.tif')
-        pygeoprocessing.testing.assert_rasters_equal(sum_raster,
-                                                     expected_raster)
+        test_files = [
+            args['lulc_uri'], args['depth_to_root_rest_layer_uri'],
+            args['precipitation_uri'], args['pawc_uri'], args['eto_uri'],
+            args['watersheds_uri'], args['sub_watersheds_uri'], args['biophysical_table_uri'],
+            args['demand_table_uri'], args['valuation_table_uri']]
 
         shutil.rmtree(args['workspace_dir'])
-        for filename in [args['example_lulc'], expected_raster]:
+        for filename in test_files:
+            print filename
             os.remove(filename)
+
     @nottest
     def test_execute_with_suffix(self):
         """When a suffix is added, verify it's added correctly."""
@@ -123,6 +248,7 @@ class HydropowerUnitTests(unittest.TestCase):
 
         self.assertTrue(os.path.exists(os.path.join(args['workspace_dir'],
                                                     'sum_foo.tif')))
+    @nottest
     def test_compute_waterhsed_valuation(self):
         """Verify valuation is computed correctly"""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -189,6 +315,7 @@ class HydropowerUnitTests(unittest.TestCase):
         shape = None
         shape_regression = None
 
+    @nottest
     def test_compute_rsupply_volume(self):
         """Verify the real supply volume is computed correctly"""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -248,6 +375,7 @@ class HydropowerUnitTests(unittest.TestCase):
         shape = None
         shape_regression = None
 
+    @nottest
     def test_extract_datasource_table_by_key(self):
         """Test a function that returns a dictionary base on a Shapefiles attributes"""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -283,6 +411,7 @@ class HydropowerUnitTests(unittest.TestCase):
                 pygeoprocessing.testing.assert_almost_equal(
                     expected_res[exp_key][sub_key], results[exp_key][sub_key])
 
+    @nottest
     def test_write_new_table(self):
         """Verify that a new CSV table is written to properly"""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -299,13 +428,13 @@ class HydropowerUnitTests(unittest.TestCase):
 
         csv_file = open(filename, 'rb')
         reader = csv.DictReader(csv_file)
-        for row in reader:
-            for key in [0, 1, 2]:
-                for sub_key in data[key].keys():
-                    if sub_key not in row:
-                        raise AssertionError('Key %s not found in CSV table' % sub_key)
-                    pygeoprocessing.testing.assert_almost_equal(
-                        float(data[key][sub_key]), float(row[sub_key]))
+
+        for row, key in zip(reader, [0, 1, 2]):
+            for sub_key in data[key].keys():
+                if sub_key not in row:
+                    raise AssertionError('Key %s not found in CSV table' % sub_key)
+                pygeoprocessing.testing.assert_almost_equal(
+                    float(data[key][sub_key]), float(row[sub_key]))
 
         csv_file.close()
 
@@ -314,20 +443,56 @@ class HydropowerUnitTests(unittest.TestCase):
         """Verify that water yield volume is computed correctly"""
         from natcap.invest.hydropower import hydropower_water_yield
 
-        fields = {'ws_id': 'int', 'wyield_mn': 'real', 'num_pixels': 'real'}
+        fields = {'ws_id': 'int', 'wyield_mn': 'real', 'num_pixels': 'int'}
 
-        attributes = [{'ws_id': 1, 'wyield_mn': 1000, 'num_pixels': 20},
-                      {'ws_id': 2, 'wyield_mn': 1000, 'num_pixels': 15},
-                      {'ws_id': 3, 'wyield_mn': 1000, 'num_pixels': 10}]
+        attributes = [{'ws_id': 1, 'wyield_mn': 400, 'num_pixels': 20},
+                      {'ws_id': 2, 'wyield_mn': 300, 'num_pixels': 15},
+                      {'ws_id': 3, 'wyield_mn': 200, 'num_pixels': 10}]
 
         watershed_uri = _create_watershed(fields, attributes)
 
         pixel_area = 100
 
-        hydropower_water_yield.compute_water_yield_volume(shape_uri, pixel_area)
+        hydropower_water_yield.compute_water_yield_volume(watershed_uri, pixel_area)
 
-        self.assertTrue(os.path.exists(os.path.join(args['workspace_dir'],
-                                                    'sum_foo.tif')))
+        results = {
+            1: {'wyield_vol': 800},
+            2: {'wyield_vol': 450},
+            3: {'wyield_vol': 200}
+            }
+
+        # Check that the 'hp_energy' / 'hp_val' fields were added and are correct
+        shape = ogr.Open(watershed_uri)
+        # Check that the shapefiles have the same number of layers
+        layer_count = shape.GetLayerCount()
+
+        for layer_num in range(layer_count):
+            # Get the current layer
+            layer = shape.GetLayer(layer_num)
+
+            # Get the first features of the layers and loop through all the
+            # features
+            feat = layer.GetNextFeature()
+            while feat is not None:
+                # Check that the field counts for the features are the same
+                layer_def = layer.GetLayerDefn()
+                field_count = layer_def.GetFieldCount()
+                ws_id_index = feat.GetFieldIndex('ws_id')
+                ws_id = feat.GetField(ws_id_index)
+
+                try:
+                    key_index = feat.GetFieldIndex('wyield_vol')
+                    key_val = feat.GetField(key_index)
+                    pygeoprocessing.testing.assert_almost_equal(
+                        results[ws_id]['wyield_vol'], key_val)
+                except ValueError:
+                    raise AssertionError('Could not find field %s' % key)
+
+                feat = None
+                feat = layer.GetNextFeature()
+
+        shape = None
+        shape_regression = None
 
     @nottest
     def test_add_dict_to_shape(self):
@@ -340,13 +505,50 @@ class HydropowerUnitTests(unittest.TestCase):
 
         watershed_uri = _create_watershed(fields, attributes)
 
-        field_dict = {'1': 5, '2': 10, '3': 15}
+        field_dict = {1: 50.0, 2: 10.5, 3: 15.8}
 
-        field_name = 'test'
+        field_name = 'precip'
 
         key = 'ws_id'
 
-        hydropower_water_yield.add_dict_to_shape(shape_uri, field_dict, field_name, key)
+        hydropower_water_yield.add_dict_to_shape(watershed_uri, field_dict, field_name, key)
 
-        self.assertTrue(os.path.exists(os.path.join(args['workspace_dir'],
-                                                    'sum_foo.tif')))
+        results = {
+            1: {'precip': 50.0},
+            2: {'precip': 10.5},
+            3: {'precip': 15.8}
+            }
+
+        # Check that the 'hp_energy' / 'hp_val' fields were added and are correct
+        shape = ogr.Open(watershed_uri)
+        # Check that the shapefiles have the same number of layers
+        layer_count = shape.GetLayerCount()
+
+        for layer_num in range(layer_count):
+            # Get the current layer
+            layer = shape.GetLayer(layer_num)
+
+            # Get the first features of the layers and loop through all the
+            # features
+            feat = layer.GetNextFeature()
+            while feat is not None:
+                # Check that the field counts for the features are the same
+                layer_def = layer.GetLayerDefn()
+                field_count = layer_def.GetFieldCount()
+                ws_id_index = feat.GetFieldIndex('ws_id')
+                ws_id = feat.GetField(ws_id_index)
+
+                try:
+                    key_index = feat.GetFieldIndex('precip')
+                    key_val = feat.GetField(key_index)
+                    pygeoprocessing.testing.assert_almost_equal(
+                        results[ws_id]['precip'], key_val)
+                except ValueError:
+                    raise AssertionError('Could not find field %s' % key)
+
+                feat = None
+                feat = layer.GetNextFeature()
+
+        shape = None
+        shape_regression = None
+
