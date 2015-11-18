ab3b2b9e73cb4aa294190283659c2d32fa85e50a
dcdenu4 <devnull@localhost>
2015-11-18 11:41:33 -0700

    re #BITBUCKET-3430 putting a few unit tests in the same file as regression tests. I gutted the unit_test file, but it will be removed in a sec

--- tests/test_hydropower.py
+++ tests/test_hydropower.py
@@ -3,7 +3,9 @@ import unittest
 import tempfile
 import shutil
 import os
+import csv
 
+from osgeo import ogr
 import pygeoprocessing.testing
 from pygeoprocessing.testing import scm
 from nose.tools import nottest
@@ -11,6 +13,129 @@ from nose.tools import nottest
 SAMPLE_DATA = os.path.join(os.path.dirname(__file__), '..', 'data', 'invest-data')
 REGRESSION_DATA = os.path.join(os.path.dirname(__file__), 'data', 'hydropower')
 
+
+class HydropowerUnitTests(unittest.TestCase):
+    """Unit tests for Annual Water Yield Hydropower Model."""
+    def setUp(self):
+        """Overriding setUp function to create temporary workspace directory."""
+        # this lets us delete the workspace after its done no matter the
+        # the rest result
+        self.workspace_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        """Overriding tearDown function to remove temporary directory."""
+        shutil.rmtree(self.workspace_dir)
+
+    def test_extract_datasource_table(self):
+        """Hydro: testing 'extract_datasource_table_by_key' function."""
+        from natcap.invest.hydropower import hydropower_water_yield
+        # this 2 polygon shapefile was created with fields:
+        # ['ws_id': 'int', 'wyield_mn': 'real', 'wyield_vol': 'real']
+        # and with values of:
+        # {'ws_id': 1, 'wyield_mn': 1000, 'wyield_vol': 1000},
+        # {'ws_id': 2, 'wyield_mn': 1000, 'wyield_vol': 800}
+        # using the script 'create_polygon_shapefile.py'
+        shape_uri = os.path.join(
+            REGRESSION_DATA, 'two_polygon_shape.shp')
+
+        key_field = 'ws_id'
+        wanted_list = ['wyield_vol']
+
+        results = hydropower_water_yield.extract_datasource_table_by_key(
+            shape_uri, key_field, wanted_list)
+
+        expected_res = {1: {'wyield_vol': 1000}, 2: {'wyield_vol': 800}}
+
+        self.assertDictEqual(expected_res, results)
+
+    def test_write_new_table(self):
+        """Hydro: testing 'write_new_table' function."""
+        from natcap.invest.hydropower import hydropower_water_yield
+        temp_dir = self.workspace_dir
+        tmp, filename = tempfile.mkstemp(suffix='.csv', dir=temp_dir)
+        os.close(tmp)
+
+        fields = ['id', 'precip', 'volume']
+
+        data = {0: {'id':1, 'precip': 100, 'volume': 150},
+                1: {'id':2, 'precip': 150, 'volume': 350},
+                2: {'id':3, 'precip': 170, 'volume': 250}}
+
+        hydropower_water_yield.write_new_table(filename, fields, data)
+        # expected results as a dictionary, note that reading from csv will
+        # leave values as strings
+        exp_data = {0: {'id': '1', 'precip': '100', 'volume': '150'},
+                    1: {'id': '2', 'precip': '150', 'volume': '350'},
+                    2: {'id': '3', 'precip': '170', 'volume': '250'}}
+
+        # to test the CSV was written correctly, we'll read back the data
+        csv_file = open(filename, 'rb')
+        reader = csv.DictReader(csv_file)
+        # assert fieldnames are the same
+        if fields != reader.fieldnames:
+            raise AssertionError(
+                "The fields from the CSV file are not correct. "
+                "Expected vs Returned: %s vs %s", (fields, reader.fieldnames))
+
+        data_row = 0
+        for row in reader:
+            if data_row > 2:
+                raise AssertionError(
+                    "There are more rows returned than expected.")
+            self.assertDictEqual(row, exp_data[data_row])
+            data_row += 1
+
+        csv_file.close()
+
+    def test_add_dict_to_shape(self):
+        """Hydro: testing 'add_dict_to_shape' function."""
+        from natcap.invest.hydropower import hydropower_water_yield
+        # 'two_poly_shape.shp was created with fields:
+        # ['ws_id': 'int', 'wyield_mn': 'real', 'wyield_vol': 'real']
+        # and with values of:
+        # {'ws_id': 1, 'wyield_mn': 1000, 'wyield_vol': 1000},
+        # {'ws_id': 2, 'wyield_mn': 1000, 'wyield_vol': 800}
+        # using the script 'create_polygon_shapefile.py'
+        shape_uri = os.path.join(
+            REGRESSION_DATA, 'two_polygon_shape.shp')
+
+        temp_dir = self.workspace_dir
+        vector_uri = os.path.join(temp_dir, 'vector.shp')
+        # make a copy of the shapefile that can take edits
+        pygeoprocessing.geoprocessing.copy_datasource_uri(shape_uri, vector_uri)
+
+        field_dict = {1: 50.0, 2: 10.5}
+        field_name = 'precip'
+        key = 'ws_id'
+
+        hydropower_water_yield.add_dict_to_shape(
+            vector_uri, field_dict, field_name, key)
+
+        expected_results = {1: {'precip': 50.0}, 2: {'precip': 10.5}}
+
+        # open the shapefile and check that the edits were made correctly.
+        shape = ogr.Open(vector_uri)
+        layer_count = shape.GetLayerCount()
+
+        for layer_num in range(layer_count):
+            layer = shape.GetLayer(layer_num)
+
+            feat = layer.GetNextFeature()
+            while feat is not None:
+                ws_id = feat.GetField('ws_id')
+
+                try:
+                    field_val = feat.GetField(field_name)
+                    pygeoprocessing.testing.assert_close(
+                        expected_results[ws_id][field_name], field_val)
+                except ValueError:
+                    raise AssertionError(
+                        'Could not find field %s' % field_name)
+
+                feat = None
+                feat = layer.GetNextFeature()
+
+        shape = None
 
 class HydropowerRegressionTests(unittest.TestCase):
     """Regression Tests for Annual Water Yield Hydropower Model."""
@@ -51,6 +176,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_valuation(self):
         """Hydro: testing valuation component with no subwatershed."""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -87,6 +213,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_water_yield(self):
         """Hydro: testing water yield component only."""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -151,6 +278,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_scarcity(self):
         """Hydro: testing Scarcity component."""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -184,6 +312,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_scarcity_subshed(self):
         """Hydro: testing Scarcity component w/ subwatershed."""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -221,6 +350,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_valuation_subshed(self):
         """Hydro: testing Valuation component w/ subwatershed."""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -262,6 +392,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_suffix(self):
         """Hydro: testing that the suffix is handled correctly."""
         from natcap.invest.hydropower import hydropower_water_yield
@@ -317,6 +448,7 @@ class HydropowerRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
     def test_suffix_underscore(self):
         """Hydro: testing that a suffix w/ underscore is handled correctly."""
         from natcap.invest.hydropower import hydropower_water_yield
