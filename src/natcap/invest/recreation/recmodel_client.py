ccaca0f4f94fc2689508db40b32566b3c724b1d1
Rich Sharp <richpsharp@gmail.com>
2015-11-23 13:17:26 -0800

    re #BITBUCKET-3432 refactored client to have an invest style execute

--- src/natcap/invest/recreation/recmodel_client.py
+++ src/natcap/invest/recreation/recmodel_client.py
@@ -1,48 +1,51 @@
-"""recmodel client demo"""
+"""InVEST Recreation Client"""
 
-import sys
 import os
 import glob
 import zipfile
 import time
+import logging
 
 import Pyro4
+
+logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
+%(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
+
+LOGGER = logging.getLogger('natcap.invest.recmodel_client')
 
 #this serializer lets us pass null bytes in strings unlike the default
 Pyro4.config.SERIALIZER = 'marshal'
 
-def main():
-    """entry point"""
 
-    if len(sys.argv) != 4:
-        print 'usage: %s aoi_filename hostname port' % sys.argv[0]
-        sys.exit(-1)
+def execute(args):
+    """Connect to remote recreation server and pass AOI
 
-    hostname = sys.argv[2]
-    port = int(sys.argv[3])
+    Parameters:
+        args['workspace_dir'] (string): path to workspace directory
+        args['aoi_path'] (string): path to AOI vector
+        args['hostname'] (string): FQDN to recreation server
+        args['port'] (string or int): port on hostname for recreation server
 
-    workspace_uri = 'rec_client_workspace'
-    tmp_dir = os.path.join(workspace_uri, 'tmp')
-    output_dir = os.path.join(workspace_uri, 'output')
+    Returns:
+        None."""
 
-    for directory in [tmp_dir, output_dir]:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    if not os.path.exists(args['workspace_dir']):
+        os.makedirs(args['workspace_dir'])
 
     recmodel_server = Pyro4.Proxy(
-        "PYRO:natcap.invest.recreation@%s:%d" % (hostname, port))
-    aoi_shapefile_filename = sys.argv[1]
+        "PYRO:natcap.invest.recreation@%s:%d" % (
+            args['hostname'], int(args['port'])))
 
     #gather shapefile components and zip them
-    basename = os.path.splitext(aoi_shapefile_filename)[0]
-    aoi_archive_uri = os.path.join(tmp_dir, 'to_server.zip')
-    with zipfile.ZipFile(aoi_archive_uri, 'w') as myzip:
+    basename = os.path.splitext(args['aoi_path'])[0]
+    aoi_archive_path = os.path.join(args['workspace_dir'], 'aoi_zipped.zip')
+    with zipfile.ZipFile(aoi_archive_path, 'w') as myzip:
         for filename in glob.glob(basename + '.*'):
             print 'archiving ', filename
             myzip.write(filename, os.path.basename(filename))
 
     #convert shapefile to binary string for serialization
-    zip_file_binary = open(aoi_archive_uri, 'rb').read()
+    zip_file_binary = open(aoi_archive_path, 'rb').read()
 
     #transfer zipped file to server
     try:
@@ -50,18 +53,15 @@ def main():
         print 'server version is ', recmodel_server.get_version()
         result_zip_file_binary = (
             recmodel_server.calc_user_days_binary(zip_file_binary))
-        print 'recieved result, took %f seconds' % (time.time() - start_time)
+        print 'received result, took %f seconds' % (time.time() - start_time)
 
         #unpack result
-        result_zip_uri = os.path.join(output_dir, 'pud_result.zip')
-        open(result_zip_uri, 'wb').write(result_zip_file_binary)
-        zipfile.ZipFile(result_zip_uri, 'r').extractall(output_dir)
-        os.remove(result_zip_uri)
+        result_zip_path = os.path.join(args['workspace_dir'], 'pud_result.zip')
+        open(result_zip_path, 'wb').write(result_zip_file_binary)
+        zipfile.ZipFile(result_zip_path, 'r').extractall(args['workspace_dir'])
+        os.remove(result_zip_path)
 
     except Exception:
         #This catches and prints Pyro tracebacks.
         print "Pyro traceback:"
         print "".join(Pyro4.util.getPyroTraceback())
-
-if __name__ == "__main__":
-    main()
