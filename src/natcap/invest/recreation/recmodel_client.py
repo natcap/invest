6ef3534f19c460335ea73627b728caa302f5ab23
Rich Sharp <richpsharp@gmail.com>
2015-12-10 14:53:09 -0800

    re #BITBUCKET-3456 added a hexagonal and square gridder on the client side

--- src/natcap/invest/recreation/recmodel_client.py
+++ src/natcap/invest/recreation/recmodel_client.py
@@ -5,9 +5,14 @@ import glob
 import zipfile
 import time
 import logging
+import math
 
-import numpy
 import Pyro4
+from osgeo import ogr
+import numpy
+import shapely
+import shapely.wkt
+import shapely.prepared
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
 %(message)s', level=logging.DEBUG, datefmt='%m/%d/%Y %H:%M:%S ')
@@ -61,7 +66,6 @@ def execute(args):
     start_time = time.time()
     LOGGER.info('server version is %s', recmodel_server.get_version())
 
-
     result_zip_file_binary = (
         recmodel_server.calc_aggregated_points_in_aoi(
             zip_file_binary, date_range, args['aggregating_metric']))
@@ -72,3 +76,117 @@ def execute(args):
     open(result_zip_path, 'wb').write(result_zip_file_binary)
     zipfile.ZipFile(result_zip_path, 'r').extractall(args['workspace_dir'])
     os.remove(result_zip_path)
+
+
+def hexagon_generator(edge_length, offset):
+    """Generator for coordinates in a hexagon."""
+    import math
+    x_offset, y_offset = offset
+    for angle in range(0, 360, 60) + [0]:
+        x_offset += math.cos(math.radians(angle)) * edge_length
+        y_offset += math.sin(math.radians(angle)) * edge_length
+        yield (x_offset, y_offset)
+
+
+def grid_vector(vector_path, grid_type, cell_size, out_grid_vector_path):
+    """Convert a vector to a regular grid where grids are contained within
+        the original vector.
+
+    Parameters:
+        vector_path (string): path to an OGR compatable polygon vector type
+        grid_type (string): one of "square" or "hexagon"
+        cell_size (float): dimensions of the grid cell in the projected units
+            of `vector_path`
+        out_grid_vector_path (string): path to the output ESRI shapefile
+            vector that contains a gridded version of `vector_path`
+
+    Returns:
+        None"""
+
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    if os.path.exists(out_grid_vector_path):
+        driver.DeleteDataSource(out_grid_vector_path)
+
+    vector = ogr.Open(vector_path)
+    vector_layer = vector.GetLayer()
+    spat_ref = vector_layer.GetSpatialRef()
+
+    original_vector_shapes = []
+    for feature_id in xrange(vector_layer.GetFeatureCount()):
+        feature = vector_layer.GetFeature(feature_id)
+        wkt_feat = shapely.wkt.loads(feature.geometry().ExportToWkt())
+        original_vector_shapes.append(wkt_feat)
+    original_polygon = shapely.prepared.prep(
+        shapely.ops.cascaded_union(original_vector_shapes))
+
+    out_grid_vector = driver.CreateDataSource(out_grid_vector_path)
+    grid_layer = out_grid_vector.CreateLayer(
+        'grid', spat_ref, ogr.wkbPolygon)
+    grid_layer_defn = grid_layer.GetLayerDefn()
+
+    extent = vector_layer.GetExtent()  # minx maxx miny maxy
+
+    if grid_type == 'hexagon':
+        # calculate the inner domensions of the hexagons
+        grid_width = extent[1] - extent[0]
+        grid_height = extent[3] - extent[2]
+        delta_short_x = cell_size * 0.25
+        delta_long_x = cell_size * 0.5
+        delta_y = cell_size * 0.25 * (3 ** 0.5)
+
+        #Since the grid is hexagonal it's not obvious how many rows and
+        #columns there should be just based on the number of squares that
+        #could fit into it.  The solution is to calculate the width and height
+        #of the largest row and column.
+        n_cols = int(math.floor(grid_width/(3 * delta_long_x)) + 1)
+        n_rows = int(math.floor(grid_height/delta_y) + 1)
+
+        def _generate_polygon(col_index, row_index):
+            """Generate a points for a closed hexagon given row and col
+            index."""
+            if (row_index + 1) % 2:
+                centroid = (
+                    extent[0] + (delta_long_x * (1 + (3 * col_index))),
+                    extent[2] + (delta_y * (row_index + 1)))
+            else:
+                centroid = (
+                    extent[0] + (delta_long_x * (2.5 + (3 * col_index))),
+                    extent[2] + (delta_y * (row_index + 1)))
+            x_coordinate, y_coordinate = centroid
+            hexagon = [(x_coordinate - delta_long_x, y_coordinate),
+                       (x_coordinate - delta_short_x, y_coordinate + delta_y),
+                       (x_coordinate + delta_short_x, y_coordinate + delta_y),
+                       (x_coordinate + delta_long_x, y_coordinate),
+                       (x_coordinate + delta_short_x, y_coordinate - delta_y),
+                       (x_coordinate - delta_short_x, y_coordinate - delta_y),
+                       (x_coordinate - delta_long_x, y_coordinate)]
+            return hexagon
+    elif grid_type == 'square':
+        def _generate_polygon(col_index, row_index):
+            """Generate points for a closed square given row and col index."""
+            square = [
+                (extent[0] + col_index * cell_size + x,
+                 extent[2] + row_index * cell_size + y)
+                for x, y in [
+                    (0, 0), (cell_size, 0), (cell_size, cell_size),
+                    (0, cell_size), (0, 0)]]
+            return square
+        n_rows = int((extent[3] - extent[2]) / cell_size)
+        n_cols = int((extent[1] - extent[0]) / cell_size)
+    else:
+        raise ValueError('Unknown polygon type: %s' % grid_type)
+
+    for row_index in xrange(n_rows):
+        for col_index in xrange(n_cols):
+            polygon_points = _generate_polygon(col_index, row_index)
+            ring = ogr.Geometry(ogr.wkbLinearRing)
+            for xoff, yoff in polygon_points:
+                ring.AddPoint(xoff, yoff)
+            poly = ogr.Geometry(ogr.wkbPolygon)
+            poly.AddGeometry(ring)
+
+            if original_polygon.contains(
+                    shapely.wkt.loads(poly.ExportToWkt())):
+                poly_feature = ogr.Feature(grid_layer_defn)
+                poly_feature.SetGeometry(poly)
+                grid_layer.CreateFeature(poly_feature)
