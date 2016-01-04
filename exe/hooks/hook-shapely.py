bed30e98683b80100c426e252a6c517cb92c65b0
Will Bierbower <wbierbow@stanford.edu>
2016-01-04 15:09:59 -0800

    Edit shapely hook for pyinstaller 3.0

--- exe/hooks/hook-shapely.py
+++ exe/hooks/hook-shapely.py
@@ -1,7 +1,14 @@
 import os
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files, get_package_paths
+from PyInstaller.compat import is_darwin, is_win, is_linux
+
 hiddenimports = collect_submodules('shapely')
 
 pkg_base, pkg_dir = get_package_paths('shapely')
 datas = collect_data_files('shapely')
-datas += [(os.path.join(pkg_dir, 'DLLs/geos_c.dll'), '')]
+
+if is_win:
+    datas += [(os.path.join(pkg_dir, 'DLLs/geos_c.dll'), '')]
+elif is_darwin:
+    datas += [(os.path.join(pkg_dir, '.dylibs/libgeos_c.1.dylib'), '')]
+    datas += [(os.path.join(pkg_dir, '.dylibs/libgeos-3.4.2.dylib'), '')]
