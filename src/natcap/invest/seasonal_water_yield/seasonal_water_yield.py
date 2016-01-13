fixes #BITBUCKET-3475 precomputing E1, testing for zero, masking the result in that case, and otherwise computing the exponental of the sum of the original exponent and the log of E1 to avoid an overflow in exp

src/natcap/invest/seasonal_water_yield/seasonal_water_yield.py
@@ -153,7 +153,7 @@ def execute(args):
     # I found this useful to catch all kinds of weird inputs to the model
     # during debugging and think it makes sense to have in production of this
     # model too.
-    # warnings.filterwarnings('error')
+    warnings.filterwarnings('error')
     try:
         _execute(args)
     finally:
@@ -596,15 +596,23 @@ def _calculate_monthly_quick_flow(
         qf_im = numpy.empty(p_im.shape)
         qf_im[:] = qf_nodata
 
-        # qf_im is the quickflow at pixel i on month m Eq. [1]
-        # the last two terms modified to be the exponent of the sum of the
-        # logs so that the exponent in the natural log is numerically small
+        # Precompute the last two terms in quickflow so we can handle a
+        # numerical instability when s_i is large and/or a_im is small
 
+        e1 = scipy.special.expn(1, valid_si / a_im)
+        # on large valid_si/a_im this number will be zero and the latter
+        # exponent will also be zero because of a divide by zero. rather than
+        # raise that numerical warning, just handle it manually
+        nonzero_e1_mask = e1 != 0
+        exp_result = numpy.zeros(valid_si.shape)
+        exp_result[nonzero_e1_mask] = numpy.exp(
+            (0.8 * valid_si[nonzero_e1_mask]) / a_im[nonzero_e1_mask] +
+            numpy.log(e1[nonzero_e1_mask]))
+
+        # qf_im is the quickflow at pixel i on month m Eq. [1]
         qf_im[valid_mask] = (25.4 * valid_n_events * (
             (a_im - valid_si) * numpy.exp(-0.2 * valid_si / a_im) +
-            valid_si ** 2 / a_im * numpy.exp(
-                (0.8 * valid_si) / a_im +
-                numpy.log(scipy.special.expn(1, valid_si / a_im)))))
+            valid_si ** 2 / a_im * exp_result))
 
         # if precip is 0, then QF should be zero
         qf_im[(p_im == 0) | (n_events == 0)] = 0.0