10e4fd031b826c53fbea1f2f0ec865f0fc73bff7
Will Bierbower <wbierbower@gmail.com>
2016-01-25 13:20:47 -0800

    Modify dylib patch in pyinstaller hook

--- exe/hooks/rthook.py
+++ exe/hooks/rthook.py
@@ -6,3 +6,4 @@ multiprocessing.freeze_support()
 
 os.environ['MATPLOTLIBDATA'] = os.path.join(sys._MEIPASS, 'mpl-data')
 os.environ['PATH'].insert(os.path.dirname(sys.executable))
+os.environ['DYLD_LIBRARY_PATH'].insert(os.path.dirname(sys.executable))
