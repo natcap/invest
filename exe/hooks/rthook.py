cfa7c3fe1066e73525c5fc0a50ec86e0d398e29f
Will Bierbower <wbierbower@gmail.com>
2016-01-25 10:56:29 -0800

    Edit PyInstaller runtime hook to include top-level directory of distributable in PATH variable

--- exe/hooks/rthook.py
+++ exe/hooks/rthook.py
@@ -5,3 +5,4 @@ import multiprocessing
 multiprocessing.freeze_support()
 
 os.environ['MATPLOTLIBDATA'] = os.path.join(sys._MEIPASS, 'mpl-data')
+os.environ['PATH'].insert(os.path.dirname(sys.executable))
