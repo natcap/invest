702f1fd58754c7bf08601cb85748d5a1def62985
Will Bierbower <wbierbower@gmail.com>
2016-01-26 10:53:47 -0800

    pull request #BITBUCKET-124 -- add comment to hooks only needed for pyinstaller v2.x

--- exe/hooks/hook-h5py.py
+++ exe/hooks/hook-h5py.py
@@ -1,3 +1,4 @@
+# Special hook necessary for PyInstaller v2.x (our linux builds)
 import sys
 if sys.platform.startswith('linux'):
     hiddenimports = ['_proxy', 'utils', 'defs', 'h5ac']
--- exe/hooks/hook-osgeo.py
+++ exe/hooks/hook-osgeo.py
@@ -1,3 +1,4 @@
+# Special hook necessary for PyInstaller v2.x (our linux builds)
 import sys
 if sys.platform.startswith('linux'):
     from PyInstaller.hooks.hookutils import collect_data_files
