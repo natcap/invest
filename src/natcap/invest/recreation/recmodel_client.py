5fe76bde27c8e0520e81b3c2ea11e52634fadbf2
Rich Sharp <richpsharp@gmail.com>
2016-02-03 12:40:44 -0800

    using utils's version of file registry, so we don't need it in the code base

--- src/natcap/invest/recreation/recmodel_client.py
+++ src/natcap/invest/recreation/recmodel_client.py
@@ -21,6 +21,8 @@ import pygeoprocessing
 import numpy
 import numpy.linalg
 
+# prefer to do intrapackage imports to avoid case where global package is
+# installed and we import the global version of it rather than the local
 from .. import utils
 
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s \
@@ -174,7 +176,7 @@ def execute(args):
     output_dir = args['workspace_dir']
     pygeoprocessing.create_directories([output_dir])
 
-    file_registry = _build_file_registry(
+    file_registry = utils.build_file_registry(
         [(_OUTPUT_BASE_FILES, output_dir),
          (_TMP_BASE_FILES, output_dir)], file_suffix)
 
@@ -391,69 +393,6 @@ def _grid_vector(vector_path, grid_type, cell_size, out_grid_vector_path):
                 poly_feature.SetGeometry(poly)
                 grid_layer.CreateFeature(poly_feature)
     ogr.DataSource.__swig_destroy__(vector)
-
-
-def _build_file_registry(base_file_path_list, file_suffix):
-    """Combine file suffixes with base names and directories.
-
-    Parameters:
-        base_file_tuple_list (list): a list of (dict, path) tuples where
-            the dictionaries have a 'file_key': 'basefilename' pair, or
-            'file_key': list of 'basefilename's.  'path'
-            indicates the file directory path to prepend to the basefile name.
-        file_suffix (string): a string to append to every filename, can be
-            empty string
-
-    Returns:
-        dictionary of 'file_keys' from the dictionaries in
-        `base_file_tuple_list` mapping to full file paths with suffixes or
-        lists of file paths with suffixes depending on the original type of
-        the 'basefilename' pair.
-
-    Raises:
-        ValueError if there are duplicate file keys or duplicate file paths.
-    """
-    all_paths = set()
-    duplicate_keys = set()
-    duplicate_paths = set()
-    file_registry = {}
-
-    def _build_path(base_filename, path):
-        """Internal helper to avoid code duplication."""
-        pre, post = os.path.splitext(base_filename)
-        full_path = os.path.join(path, pre+file_suffix+post)
-
-        # Check for duplicate keys or paths
-        if full_path in all_paths:
-            duplicate_paths.add(full_path)
-        else:
-            all_paths.add(full_path)
-        return full_path
-
-    # foo
-    for base_file_dict, path in base_file_path_list:
-        for file_key, file_payload in base_file_dict.iteritems():
-            # check for duplicate keys
-            if file_key in file_registry:
-                duplicate_keys.add(file_key)
-            else:
-                # handle the case whether it's a filename or a list of strings
-                if isinstance(file_payload, basestring):
-                    full_path = _build_path(file_payload, path)
-                    file_registry[file_key] = full_path
-                elif isinstance(file_payload, list):
-                    file_registry[file_key] = []
-                    for filename in file_payload:
-                        full_path = _build_path(filename, path)
-                        file_registry[file_key].append(full_path)
-
-    if len(duplicate_paths) > 0 or len(duplicate_keys):
-        raise ValueError(
-            "Cannot consolidate because of duplicate paths or keys: "
-            "duplicate_keys: %s duplicate_paths: %s" % (
-                str(duplicate_keys), str(duplicate_paths)))
-
-    return file_registry
 
 
 def _build_regression_coefficients(
