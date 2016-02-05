016b39239e2c99eeca3bd01debbfb2e9290e60ef
Rich Sharp <richpsharp@gmail.com>
2016-02-05 12:37:38 -0800

    re #BITBUCKET-3432 pep8/257+docstringing file_hash

--- src/natcap/invest/recreation/file_hash.py
+++ src/natcap/invest/recreation/file_hash.py
@@ -6,6 +6,7 @@ import Queue
 import os
 import time
 
+
 def _read_file(file_path, file_buffer_queue, blocksize, fast_hash=False):
     """Divide file into blocks and add to a processing queue.
 
@@ -28,8 +29,8 @@ def _read_file(file_path, file_buffer_queue, blocksize, fast_hash=False):
     """
     with open(file_path, 'rb') as file_to_hash:
         if fast_hash:
-            #fast hash reads the first and last blocks and uses the modified
-            #stamp and filesize
+            # fast hash reads the first and last blocks and uses the modified
+            # stamp and filesize
             buf = file_to_hash.read(blocksize)
             file_buffer_queue.put(buf)
             file_size = os.path.getsize(file_path)
@@ -47,18 +48,41 @@ def _read_file(file_path, file_buffer_queue, blocksize, fast_hash=False):
                 buf = file_to_hash.read(blocksize)
     file_buffer_queue.put('STOP')
 
+
 def _hash_blocks(file_buffer_queue):
-    """Processes the file_buffer_queue one buf at a time and adds to current
-        hash"""
+    """Return sha1 hash of the in-order contents of `file_buffer_queue`."""
     hasher = hashlib.sha1()
     for row_buffer in iter(file_buffer_queue.get, "STOP"):
         hasher.update(row_buffer)
     file_buffer_queue.put(hasher.hexdigest()[:16])
 
-def hashfile(file_path, blocksize=2**20, fast_hash=False):
-    """Memory efficient and threaded function to return a hash since this
-        operation is IO bound"""
 
+def hashfile(
+        file_path, blocksize=2**20, concurent_blocks=100, fast_hash=False):
+    """Memory efficient hash of file.
+
+    This function concurrently reads `blocksize` chunks from `file_path` and
+    continously aggregates a running hash of those blocks with memory
+    efficiency and IO/bound computational efficiency in mind.
+
+    Parameters:
+        file_path (string): path to file to hash
+        blocksize (int): how many bytes to load from `file_path` at a time
+            for hashing
+        concurent_blocks (int): how many blocks of `file_path` to hold in
+            memory at one time.  Setting this larger allows the algorithm to
+            read ahead while the hash computes blocks as they come in.
+        fast_hash (boolean): if True computes hash of entire file, if False
+            computes hash of the first and last blocks of the file along with
+            the filename, file size, and creation time.
+
+    Returns:
+        if `fast_hash` is False, returns the sha1 hash of file_path
+        if `fast_hash` is True, returns the sha1 hash of the first and last
+            blocks of file_path appended to the filename, file size, and
+            access time.  The hash is also appended with the suffix
+            "_fast_hash" to avoid confusion about the source.
+    """
     file_buffer_queue = Queue.Queue(100)
     read_file_process = threading.Thread(
         target=_read_file, args=(
