6d269641f7b822c1fc790915766ecf7995df895d
Rich Sharp <richpsharp@gmail.com>
2016-02-05 12:18:26 -0800

    re #BITBUCKET-3469 adding tests for file hashing framework

--- src/natcap/invest/recreation/file_hash.py
+++ src/natcap/invest/recreation/file_hash.py
@@ -6,22 +6,22 @@ import Queue
 import os
 import time
 
-def _read_file(filename, file_buffer_queue, blocksize, fast_hash=False):
-    """Reads one blocksize at a time and adds to the file buffer queue"""
-    with open(filename, 'rb') as file_to_hash:
+def _read_file(file_path, file_buffer_queue, blocksize, fast_hash=False):
+    """Hash `file_path` one blocksize at a time and adds to file_buffer_queue."""
+    with open(file_path, 'rb') as file_to_hash:
         if fast_hash:
             #fast hash reads the first and last blocks and uses the modified
             #stamp and filesize
             buf = file_to_hash.read(blocksize)
             file_buffer_queue.put(buf)
-            file_size = os.path.getsize(filename)
+            file_size = os.path.getsize(file_path)
             if file_size - blocksize > 0:
                 file_to_hash.seek(file_size - blocksize)
                 buf = file_to_hash.read(blocksize)
             file_buffer_queue.put(buf)
-            file_buffer_queue.put(filename)
+            file_buffer_queue.put(os.path.basename(file_path))
             file_buffer_queue.put(str(file_size))
-            file_buffer_queue.put(time.ctime(os.path.getmtime(filename)))
+            file_buffer_queue.put(time.ctime(os.path.getmtime(file_path)))
         else:
             buf = file_to_hash.read(blocksize)
             while len(buf) > 0:
@@ -37,14 +37,14 @@ def _hash_blocks(file_buffer_queue):
         hasher.update(row_buffer)
     file_buffer_queue.put(hasher.hexdigest()[:16])
 
-def hashfile(filename, blocksize=2**20, fast_hash=False):
+def hashfile(file_path, blocksize=2**20, fast_hash=False):
     """Memory efficient and threaded function to return a hash since this
         operation is IO bound"""
 
     file_buffer_queue = Queue.Queue(100)
     read_file_process = threading.Thread(
         target=_read_file, args=(
-            filename, file_buffer_queue, blocksize, fast_hash))
+            file_path, file_buffer_queue, blocksize, fast_hash))
     read_file_process.start()
     hash_blocks_process = threading.Thread(
         target=_hash_blocks, args=(file_buffer_queue,))
@@ -53,5 +53,7 @@ def hashfile(filename, blocksize=2**20, fast_hash=False):
     hash_blocks_process.join()
     file_hash = file_buffer_queue.get()
     if fast_hash:
+        # this appends something so that a small file will have a different
+        # hash whether it's fast hash or slow hash
         file_hash += '_fast_hash'
     return file_hash
