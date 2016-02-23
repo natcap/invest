2955d71d697fc5d0f37911d96fabfe694260f3a1
Rich Sharp <richpsharp@gmail.com>
2016-02-23 12:11:41 -0800

    making imports relative because of issue #BITBUCKET-3450

--- src/natcap/invest/recreation/recmodel_server.py
+++ src/natcap/invest/recreation/recmodel_server.py
@@ -26,8 +26,8 @@ import shapely.geometry
 import shapely.prepared
 
 import natcap.versioner
-import natcap.invest.recreation.out_of_core_quadtree as out_of_core_quadtree  # pylint: disable=import-error,no-name-in-module
-from natcap.invest.recreation import recmodel_client
+from . import out_of_core_quadtree  # pylint: disable=import-error,no-name-in-module
+from . import recmodel_client
 __version__ = natcap.versioner.get_version('natcap.invest.recmodel_server')
 
 BLOCKSIZE = 2 ** 21
