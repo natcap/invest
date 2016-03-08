7a631414e7bc731e9eb9e0d9d2640ea3e9f2ba65
Will Bierbower <wbierbow@stanford.edu>
2016-03-07 19:51:28 -0800

    Remove hook-osgeo

--- exe/hooks/hook-osgeo.py
+++
@@ -1,9 +0,0 @@
-from PyInstaller.compat import is_win, is_darwin, is_linux
-
-# Special hook necessary for PyInstaller v2.x (our linux builds)
-if is_darwin:
-    from PyInstaller.utils.hooks import collect_data_files
-else:
-    from PyInstaller.hooks.hookutils import collect_data_files
-
-datas = collect_data_files('osgeo')
