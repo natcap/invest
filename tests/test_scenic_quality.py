3b93c54443e975d1b6893a80941dd914a39ef552
dcdenu4 <devnull@localhost>
2016-03-10 07:48:18 -0700

    re #BITBUCKET-3491 working on a few tests for SQ

--- tests/test_scenic_quality.py
+++ tests/test_scenic_quality.py
@@ -8,6 +8,10 @@ import csv
 from osgeo import ogr
 import pygeoprocessing.testing
 from pygeoprocessing.testing import scm
+from pygeoprocessing.testing import sampledata
+from shapely.geometry import Polygon
+
+from nose.tools import nottest
 
 SAMPLE_DATA = os.path.join(
     os.path.dirname(__file__), '..', 'data', 'invest-data')
@@ -27,6 +31,50 @@ class ScenicQualityUnitTests(unittest.TestCase):
     def tearDown(self):
         """Overriding tearDown function to remove temporary directory."""
         shutil.rmtree(self.workspace_dir)
+
+
+    def test_add_percent_overlap(self):
+        """SQ: test 'add_percent_overlap' function."""
+        from natcap.invest.scenic_quality import scenic_quality
+
+        temp_dir = self.workspace_dir
+        srs = sampledata.SRS_WILLAMETTE
+
+        pos_x = srs.origin[0]
+        pos_y = srs.origin[1]
+        fields = {'myid': 'int'}
+        attrs = [{'myid': 1}, {'myid': 2}]
+
+        # Create geometry for the polygons
+        geom = [
+            Polygon(
+                [(pos_x, pos_y), (pos_x + 60, pos_y), (pos_x + 60, pos_y - 60),
+                 (pos_x, pos_y - 60), (pos_x, pos_y)]),
+            Polygon(
+                [(pos_x + 80, pos_y - 80), (pos_x + 140, pos_y - 80),
+                 (pos_x + 140, pos_y - 140), (pos_x + 80, pos_y - 140),
+                 (pos_x + 80, pos_y - 80)])]
+
+        shape_path = os.path.join(temp_dir, 'shape.shp')
+        # Create the point shapefile
+        shape_path = pygeoprocessing.testing.create_vector_on_disk(
+            geom, srs.projection, fields, attrs,
+            vector_format='ESRI Shapefile', filename=shape_path)
+
+        pixel_counts = {1: 2, 2: 1}
+        pixel_size = 30
+
+        scenic_quality.add_percent_overlap(
+            shape_path, 'myid', '%_overlap', pixel_counts, pixel_size)
+
+        exp_result = {1: 50.0, 2: 25.0}
+
+        shape = ogr.Open(shape_path)
+        layer = shape.GetLayer()
+        for feat in layer:
+            myid = feat.GetFieldAsInteger('myid')
+            perc_overlap = feat.GetFieldAsDouble('%_overlap')
+            self.assertEqual(exp_result[myid], perc_overlap)
 
 
 class ScenicQualityRegressionTests(unittest.TestCase):
@@ -63,9 +111,18 @@ class ScenicQualityRegressionTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
+    @nottest
+    def test_scenic_quality_no_refraction(self):
+        """SQ: testing stuff."""
+        from natcap.invest.scenic_quality import scenic_quality
+
+        args = ScenicQualityTests.generate_base_args(self.workspace_dir)
+
+        scenic_quality.execute(args)
+
     def test_scenic_quality(self):
         """SQ: testing stuff."""
-        from natcap.invest.hydropower import scenic_quality
+        from natcap.invest.scenic_quality import scenic_quality
 
         args = ScenicQualityTests.generate_base_args(self.workspace_dir)
 
