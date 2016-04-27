8551860e5f1dd5e5a6edaaabeb47f2f8c296de50
Will Bierbower <wbierbow@stanford.edu>
2016-04-27 16:14:07 -0700

    Fix string formatting of set object in Crop model logger

--- src/natcap/invest/crop_production/crop_production.py
+++ src/natcap/invest/crop_production/crop_production.py
@@ -266,7 +266,8 @@ def get_lookup_dict(aoi_raster, lookup_table):
     t = set(lookup_dict.keys())
 
     if set() != s-t:
-        LOGGER.warn("raster contains values not in lookup table: %s", s-t)
+        u = s-t
+        LOGGER.warn("raster contains values not in lookup table: %s" % u)
 
     # delete lookup items not in raster
     for i in (t-s):
