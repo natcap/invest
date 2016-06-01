296f13404cb08131ecfb8994b8fb863ca9e49e8c
James Douglass <jdouglass@stanford.edu>
2016-06-01 16:19:24 -0700

    Detecting type of inputs and required state where possible.
    RE:#BITBUCKET-3532

--- scripts/get_args.py
+++ scripts/get_args.py
@@ -3,7 +3,7 @@
 import json
 import sys
 
-LINE_TEMPLATE = '        {args_id} (): {description}'
+LINE_TEMPLATE = '        {args_id} ({type}): {description}'
 
 def main(filename):
     iui_dict = json.load(open(filename))
@@ -24,18 +24,39 @@ def main(filename):
         try:
             helptext = args_dict['helpText']
         except KeyError:
-            helptext = "See the User's Guide for more information."
+            try:
+                helptext = args_dict['label']
+            except KeyError:
+                helptext = "See the User's Guide for more information."
 
-        # 8 for initial indent, 10 for type, 1 for colon
-        param_name_len = len(args_id) + 8 + 10 + 1
-        remaining_firstline_len = 79 - param_name_len
+        try:
+            required = bool(args_dict['required'])
+        except KeyError:
+            required = False
 
-        formatted_helptext = '        '
+        return_type = ''
+        if (args_dict['type'] in ['file', 'folder']):
+            return_type = 'string'
+        else:
+            try:
+                if args_dict['dataType'] in ['string']:
+                    return_type = 'string'
+            except KeyError:
+                pass
+
+        helptext += ' (%s)' % 'required' if required else 'optional'
+
+        # 8 for initial indent, 10 for type, 1 for colon, 8 more for good
+        # measure
+        param_name_len = len(args_id) + 8 + 10 + 1 + 8
+        max_firstline_len = 79 - param_name_len
+
+        formatted_helptext = ''
         first_line_satisfied = False
         current_line_counter = 12  # indent for new lines.
         for word in helptext.split(' '):
             if not first_line_satisfied:
-                if len(formatted_helptext) > remaining_firstline_len and remaining_firstline_len < word:
+                if (len(formatted_helptext) > max_firstline_len):
                     formatted_helptext += '\n           '
                     first_line_satisfied = True
                 else:
@@ -48,7 +69,8 @@ def main(filename):
                     formatted_helptext += (' ' + word)
                     current_line_counter += len(word) + 1
 
-        formatted_template = LINE_TEMPLATE.format(args_id=args_id, description=formatted_helptext)
+        formatted_template = LINE_TEMPLATE.format(
+            args_id=args_id, description=formatted_helptext, type=return_type)
         print formatted_template
 
 
