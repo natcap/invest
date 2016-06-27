335d4e628b67c8e5855db71f0e73735fc0f77ca1
Rich Sharp <richpsharp@gmail.com>
2016-06-27 15:42:56 -0700

    fixes #BITBUCKET-3555 full coverage for exponential decay function via regression test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,51 @@
 """Module for testing the natcap.invest.utils module.."""
 import unittest
 import os
+import tempfile
+import shutil
+
+from pygeoprocessing.testing import scm
+import pygeoprocessing.testing
 
 
-class UtilsTests(unittest.TestCase):
-    """Tests for natcap.invest.utils."""
+class SuffixUtilsTests(unittest.TestCase):
+    """Tests for natcap.invest.utils.make_suffix_string."""
 
     def test_suffix_string(self):
         """Utils: test suffix_string."""
-        pass
+        from natcap.invest import utils
+
+        args = {'foo': 'bar', 'file_suffix': 'suff'}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '_suff')
+
+    def test_suffix_string_underscore(self):
+        """Utils: test suffix_string underscore."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar', 'file_suffix': '_suff'}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '_suff')
+
+    def test_suffix_string_empty(self):
+        """Utils: test empty suffix_string."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar', 'file_suffix': ''}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '')
+
+    def test_suffix_string_no_entry(self):
+        """Utils: test no suffix entry in args."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar'}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '')
+
+
+class FileRegistryUtilsTests(unittest.TestCase):
+    """Tests for natcap.invest.utils.file_registry."""
 
     def test_exp_decay_kernel_raster(self):
         """Utils: test exponential_decay_kernel_raster."""
@@ -22,8 +59,8 @@ class UtilsTests(unittest.TestCase):
         file_registry = utils.build_file_registry([(base_dict, '')], '')
 
         self.assertEqual(
-            UtilsTests._norm_dict(base_dict),
-            UtilsTests._norm_dict(file_registry))
+            FileRegistryUtilsTests._norm_dict(base_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
 
     def test_build_file_registry_suffix(self):
         """Utils: test build_file_registry on suffix."""
@@ -38,8 +75,8 @@ class UtilsTests(unittest.TestCase):
         }
 
         self.assertEqual(
-            UtilsTests._norm_dict(expected_dict),
-            UtilsTests._norm_dict(file_registry))
+            FileRegistryUtilsTests._norm_dict(expected_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
 
     def test_build_file_registry_list_suffix(self):
         """Utils: test build_file_registry on list of files w/ suffix."""
@@ -55,8 +92,8 @@ class UtilsTests(unittest.TestCase):
         }
 
         self.assertEqual(
-            UtilsTests._norm_dict(expected_dict),
-            UtilsTests._norm_dict(file_registry))
+            FileRegistryUtilsTests._norm_dict(expected_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
 
     def test_build_file_registry_path(self):
         """Utils: test build_file_registry on path."""
@@ -76,8 +113,8 @@ class UtilsTests(unittest.TestCase):
         }
 
         self.assertEqual(
-            UtilsTests._norm_dict(expected_dict),
-            UtilsTests._norm_dict(file_registry))
+            FileRegistryUtilsTests._norm_dict(expected_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
 
     def test_build_file_registry_duppath(self):
         """Utils: test build_file_registry ValueError on duplicate paths."""
@@ -129,3 +166,33 @@ class UtilsTests(unittest.TestCase):
             else:
                 raise ValueError("Unexpected path value: %s", path)
         return result_dict
+
+
+class ExponentialDecayUtilsTests(unittest.TestCase):
+    """Tests for natcap.invest.utils.exponential_decay_kernel_raster."""
+
+    _REGRESSION_PATH = os.path.join(
+        os.path.dirname(__file__), '..', 'data', 'invest-test-data',
+        'exp_decay_kernel')
+
+    def setUp(self):
+        """Setup workspace."""
+        self.workspace_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        """Delete workspace."""
+        shutil.rmtree(self.workspace_dir)
+
+    @scm.skip_if_data_missing(_REGRESSION_PATH)
+    def test_exp_decay_kernel_raster(self):
+        """Utils: test exponential_decay_kernel_raster."""
+        from natcap.invest import utils
+        expected_distance = 100  # 10 pixels
+        kernel_filepath = os.path.join(self.workspace_dir, 'kernel_100.tif')
+        utils.exponential_decay_kernel_raster(
+            expected_distance, kernel_filepath)
+
+        pygeoprocessing.testing.assert_rasters_equal(
+            os.path.join(
+                ExponentialDecayUtilsTests._REGRESSION_PATH,
+                'kernel_100.tif'), kernel_filepath, 1e-6)
