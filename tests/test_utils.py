6bc824ac612ef9322554ecacd02066c242cb4b00
Rich Sharp <richpsharp@gmail.com>
2016-06-27 14:32:06 -0700

    re #BITBUCKET-3553 adding tests to cover file_registry creation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 """Module for testing the natcap.invest.utils module.."""
 import unittest
+import os
 
 
 class UtilsTests(unittest.TestCase):
@@ -14,10 +15,106 @@ class UtilsTests(unittest.TestCase):
         pass
 
     def test_build_file_registry(self):
-        """Utils: test build_file_registry."""
+        """Utils: test build_file_registry on simple case."""
         from natcap.invest import utils
 
         base_dict = {'foo': 'bar', 'baz': '/bart/bam.txt'}
         file_registry = utils.build_file_registry([(base_dict, '')], '')
 
-        self.assertEqual(base_dict, file_registry)
+        self.assertEqual(
+            UtilsTests._norm_dict(base_dict),
+            UtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_suffix(self):
+        """Utils: test build_file_registry on suffix."""
+        from natcap.invest import utils
+
+        base_dict = {'foo': 'bar', 'baz': '/bart/bam.txt'}
+        file_registry = utils.build_file_registry([
+            (base_dict, '')], '_suff')
+        expected_dict = {
+            'foo': 'bar_suff',
+            'baz': '/bart/bam_suff.txt'
+        }
+
+        self.assertEqual(
+            UtilsTests._norm_dict(expected_dict),
+            UtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_list_suffix(self):
+        """Utils: test build_file_registry on list of files w/ suffix."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': ['bar', '/bart/bam.txt']
+        }
+        file_registry = utils.build_file_registry([
+            (base_dict, '')], '_suff')
+        expected_dict = {
+            'foo': ['bar_suff', '/bart/bam_suff.txt']
+        }
+
+        self.assertEqual(
+            UtilsTests._norm_dict(expected_dict),
+            UtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_path(self):
+        """Utils: test build_file_registry on path."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': 'bar',
+            'baz': '/bart/bam.txt',
+            'jab': 'jim'
+        }
+        file_registry = utils.build_file_registry([
+            (base_dict, 'newpath')], '')
+        expected_dict = {
+            'foo': 'newpath/bar',
+            'jab': 'newpath/jim',
+            'baz': '/bart/bam.txt',
+        }
+
+        self.assertEqual(
+            UtilsTests._norm_dict(expected_dict),
+            UtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_duppath(self):
+        """Utils: test build_file_registry ValueError on duplicate paths."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': 'bar',
+            'jab': 'bar'
+        }
+        with self.assertRaises(ValueError):
+            _ = utils.build_file_registry([
+                (base_dict, 'newpath')], '')
+
+    def test_build_file_registry_dupkeys(self):
+        """Utils: test build_file_registry ValueError on duplicate keys."""
+        from natcap.invest import utils
+
+        base_dict1 = {
+            'foo': 'bar',
+        }
+        base_dict2 = {
+            'foo': 'bar2',
+        }
+        with self.assertRaises(ValueError):
+            _ = utils.build_file_registry([
+                (base_dict1, ''), (base_dict2, '')], '')
+
+    @staticmethod
+    def _norm_dict(path_dict):
+        """Take a dictionary of paths and normalize the paths."""
+        result_dict = {}
+        for key, path in path_dict.iteritems():
+            if isinstance(path, str):
+                result_dict[key] = os.path.normpath(path)
+            elif isinstance(path, list):
+                result_dict[key] = [
+                    os.path.normpath(list_path) for list_path in path]
+            else:
+                raise ValueError("Unexpected path value: %s", path)
+        return result_dict
