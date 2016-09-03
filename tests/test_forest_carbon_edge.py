a7979e5bafb9118217a4fade8832b6b709f1a762
Rich Sharp <richpsharp@gmail.com>
2016-09-03 12:16:11 -0700

    re #BITBUCKET-3576 tracking aggregate base data and simplifying test case to check for file list separately from agg results

--- tests/test_forest_carbon_edge.py
+++ tests/test_forest_carbon_edge.py
@@ -7,15 +7,16 @@ import os
 
 import pygeoprocessing.testing
 from pygeoprocessing.testing import scm
-from osgeo import gdal
 from osgeo import ogr
 import numpy
 
 
 SAMPLE_DATA = os.path.join(
-    os.path.dirname(__file__), '..', 'data', 'invest-data', 'forest_carbon_edge_effect')
+    os.path.dirname(__file__), '..', 'data', 'invest-data',
+    'forest_carbon_edge_effect')
 REGRESSION_DATA = os.path.join(
-    os.path.dirname(__file__), '..', 'data', 'invest-test-data', 'forest_carbon_edge_effect')
+    os.path.dirname(__file__), '..', 'data', 'invest-test-data',
+    'forest_carbon_edge_effect')
 
 
 class ForestCarbonEdgeTests(unittest.TestCase):
@@ -59,6 +60,12 @@ class ForestCarbonEdgeTests(unittest.TestCase):
             os.path.join(REGRESSION_DATA, 'file_list.txt'),
             args['workspace_dir'])
 
+        ForestCarbonEdgeTests._assert_regression_results_eq(
+            args['workspace_dir'],
+            os.path.join(
+                args['workspace_dir'], 'aggregated_carbon_stocks.shp'),
+            os.path.join(REGRESSION_DATA, 'agg_results_base.csv'))
+
     @staticmethod
     def _test_same_files(base_list_path, directory_path):
         """Assert files in `base_list_path` are in `directory_path`.
@@ -88,3 +95,51 @@ class ForestCarbonEdgeTests(unittest.TestCase):
             raise AssertionError(
                 "The following files were expected but not found: " +
                 '\n'.join(missing_files))
+
+    @staticmethod
+    def _assert_regression_results_eq(
+            workspace_dir, result_vector_path, agg_results_path):
+        """Test workspace state against expected aggregate results.
+
+        Parameters:
+            workspace_dir (string): path to the completed model workspace
+            result_vector_path (string): path to the summary shapefile
+                produced by the Forest Carbon Edge model.
+            agg_results_path (string): path to a csv file that has the
+                expected aggregated_results.shp table in the form of
+                c_sum,c_ha_mean per line
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError if any files are missing or results are out of
+            range by `tolerance_places`
+        """
+        result_vector = ogr.Open(result_vector_path)
+        result_layer = result_vector.GetLayer()
+
+        # The tolerance of 3 digits after the decimal was determined by
+        # experimentation on the application with the given range of numbers.
+        # This is an apparently reasonable approach as described by ChrisF:
+        # http://stackoverflow.com/a/3281371/42897
+        # and even more reading about picking numerical tolerance (it's hard):
+        # https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+        tolerance_places = 3
+
+        with open(agg_results_path, 'rb') as agg_result_file:
+            for line in agg_result_file:
+                fid, c_sum, c_ha_mean = [float(x) for x in line.split(',')]
+                feature = result_layer.GetFeature(int(fid))
+                for field, value in [
+                        ('c_sum', c_sum),
+                        ('c_ha_mean', c_ha_mean)]:
+                    numpy.testing.assert_almost_equal(
+                        feature.GetField(field), value,
+                        decimal=tolerance_places)
+                ogr.Feature.__swig_destroy__(feature)
+                feature = None
+
+        result_layer = None
+        ogr.DataSource.__swig_destroy__(result_vector)
+        result_vector = None
