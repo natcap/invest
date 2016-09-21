8611b507748644ee90a089ea9d970f21b610fcde
James Douglass <jdouglass@stanford.edu>
2016-09-21 11:47:44 -0700

    Reducing redundancy through copies of args dicts.
    RE:#BITBUCKET-3585

--- tests/test_fisheries.py
+++ tests/test_fisheries.py
@@ -179,10 +179,8 @@ class FisheriesSampleDataTests(unittest.TestCase):
         self.assertEqual(final_timestep_data['spawners'], 4053119.08)
         self.assertEqual(final_timestep_data['harvest'], 527192.41)
 
-    @scm.skip_if_data_missing(SAMPLE_DATA)
-    def test_sampledata_fecundity(self):
-        # Based on the lobster inputs, but need coverage for fecundity.
-        from natcap.invest.fisheries import fisheries
+    @staticmethod
+    def fecundity_args(workspace):
         args = {
             u'alpha': 5.77e6,
             u'aoi_uri': os.path.join(SAMPLE_DATA, 'input',
@@ -202,8 +200,31 @@ class FisheriesSampleDataTests(unittest.TestCase):
             u'total_recur_recruits': 2.16e11,
             u'total_timesteps': 100,
             u'val_cont': False,
-            u'workspace_dir': self.workspace_dir,
+            u'workspace_dir': workspace,
         }
+        return args
+
+    @staticmethod
+    def galveston_args(workspace):
+        args = FisheriesSampleDataTests.fecundity_args(workspace)
+        args.update({
+            u'alpha': 6050000.0,  # TODO: supposedly ignored w/Fixed, keyerror
+            u'aoi_uri': os.path.join(SAMPLE_DATA, 'input',
+                                     'shapefile_galveston',
+                                     'Galveston_Subregion.shp'),
+            u'beta': 4.14e-08,  # TODO: supposedly ignored w/Fixed, keyerror
+            u'harvest_units': 'Individuals',  # TODO: supposedly ignored w/Fixed, keyerror
+            u'spawn_units': 'Individuals',
+            u'total_timesteps': 300,
+        })
+        return args
+
+
+    @scm.skip_if_data_missing(SAMPLE_DATA)
+    def test_sampledata_fecundity(self):
+        # Based on the lobster inputs, but need coverage for fecundity.
+        from natcap.invest.fisheries import fisheries
+        args = FisheriesSampleDataTests.fecundity_args(self.workspace_dir)
         fisheries.execute(args)
 
         final_timestep_data = FisheriesSampleDataTests.get_harvest_info(self.workspace_dir)
@@ -213,32 +234,16 @@ class FisheriesSampleDataTests(unittest.TestCase):
     @scm.skip_if_data_missing(SAMPLE_DATA)
     def test_sampledata_custom_function(self):
         from natcap.invest.fisheries import fisheries
+        args = FisheriesSampleDataTests.galveston_args(self.workspace_dir)
 
-        args = {
-            u'alpha': 6050000.0,  # TODO: supposedly ignored w/Fixed, keyerror
-            u'aoi_uri': os.path.join(SAMPLE_DATA, 'input',
-                                     'shapefile_galveston',
-                                     'Galveston_Subregion.shp'),
-            u'beta': 4.14e-08,  # TODO: supposedly ignored w/Fixed, keyerror
-            u'do_batch': False,
-            u'harvest_units': 'Individuals',  # TODO: supposedly ignored w/Fixed, keyerror
-            u'migr_cont': False,
-            u'population_csv_uri': os.path.join(TEST_DATA,
-                                                'sample_fecundity_params.csv'),
-            u'population_type': 'Age-Based',
+        args.update({
             u'recruitment_type': 'Other',
-            u'sexsp': 'No',
-            u'spawn_units': 'Individuals',
-            u'total_init_recruits': 1e5,
-            u'total_recur_recruits': 2.16e11,
-            u'total_timesteps': 300,
-            u'val_cont': False,
-            u'workspace_dir': self.workspace_dir,
-            # Demonstration of a near-trivial function; does not model
-            # anything.
+            # This doesn't model anything real, but it will produce outputs as
+            # expected.
             u'recruitment_func': lambda x: (numpy.ones((9,)),
-                                            numpy.float64(100)),
-        }
+                                             numpy.float64(100))
+        })
+
         fisheries.execute(args)
         final_timestep_data = FisheriesSampleDataTests.get_harvest_info(self.workspace_dir)
         self.assertEqual(final_timestep_data['spawners'], 100.0)
@@ -247,91 +252,38 @@ class FisheriesSampleDataTests(unittest.TestCase):
     @scm.skip_if_data_missing(SAMPLE_DATA)
     def test_sampledata_invalid_custom_function(self):
         from natcap.invest.fisheries import fisheries
+        args = FisheriesSampleDataTests.galveston_args(self.workspace_dir)
 
-        args = {
-            u'alpha': 6050000.0,  # TODO: supposedly ignored w/Fixed, keyerror
-            u'aoi_uri': os.path.join(SAMPLE_DATA, 'input',
-                                     'shapefile_galveston',
-                                     'Galveston_Subregion.shp'),
-            u'beta': 4.14e-08,  # TODO: supposedly ignored w/Fixed, keyerror
-            u'do_batch': False,
-            u'harvest_units': 'Individuals',  # TODO: supposedly ignored w/Fixed, keyerror
-            u'migr_cont': False,
-            u'population_csv_uri': os.path.join(TEST_DATA,
-                                                'sample_fecundity_params.csv'),
-            u'population_type': 'Age-Based',
+        args.update({
             u'recruitment_type': 'Other',
-            u'sexsp': 'No',
-            u'spawn_units': 'Individuals',
-            u'total_init_recruits': 1e5,
-            u'total_recur_recruits': 2.16e11,
-            u'total_timesteps': 300,
-            u'val_cont': False,
-            u'workspace_dir': self.workspace_dir,
             # The custom function must return two values, so this should raise
             # an error.
             u'recruitment_func': lambda x: x,
-        }
+        })
         with self.assertRaises(ValueError):
             fisheries.execute(args)
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     def test_sampledata_invalid_recruitment(self):
         from natcap.invest.fisheries import fisheries
+        args = FisheriesSampleDataTests.galveston_args(self.workspace_dir)
 
-        args = {
-            u'alpha': 6050000.0,
-            u'aoi_uri': os.path.join(SAMPLE_DATA, 'input',
-                                     'shapefile_galveston',
-                                     'Galveston_Subregion.shp'),
-            u'beta': 4.14e-08,
-            u'do_batch': False,
-            u'harvest_units': 'Individuals',
-            u'migr_cont': False,
-            u'population_csv_uri': os.path.join(TEST_DATA,
-                                                'sample_fecundity_params.csv'),
-            u'population_type': 'Age-Based',
+        args.update({
             u'recruitment_type': 'foo',
-            u'sexsp': 'No',
-            u'spawn_units': 'Individuals',
-            u'total_init_recruits': 1e5,
-            u'total_recur_recruits': 2.16e11,
-            u'total_timesteps': 300,
-            u'val_cont': False,
-            u'workspace_dir': self.workspace_dir,
-            # Demonstration of a near-trivial function; does not model
-            # anything.
-            u'recruitment_func': lambda x: (numpy.ones((9,)),
-                                            numpy.float64(100)),
-        }
+        })
+
         with self.assertRaises(ValueError):
             fisheries.execute(args)
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     def test_sampledata_invalid_population_type(self):
         from natcap.invest.fisheries import fisheries
+        args = FisheriesSampleDataTests.galveston_args(self.workspace_dir)
 
-        args = {
-            u'alpha': 6050000.0,
-            u'aoi_uri': os.path.join(SAMPLE_DATA, 'input',
-                                     'shapefile_galveston',
-                                     'Galveston_Subregion.shp'),
-            u'beta': 4.14e-08,
-            u'do_batch': False,
-            u'harvest_units': 'Individuals',
-            u'migr_cont': False,
-            u'population_csv_uri': os.path.join(TEST_DATA,
-                                                'sample_fecundity_params.csv'),
+        args.update({
             u'population_type': 'INVALID TYPE',
-            u'recruitment_type': 'Ricker',
-            u'sexsp': 'No',
-            u'spawn_units': 'Individuals',
-            u'total_init_recruits': 1e5,
-            u'total_recur_recruits': 2.16e11,
-            u'total_timesteps': 300,
-            u'val_cont': False,
-            u'workspace_dir': self.workspace_dir,
-        }
+        })
+
         with self.assertRaises(ValueError):
             fisheries.execute(args)
 
