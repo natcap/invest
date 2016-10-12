7b629dbcb4aca13a7b223c2ed903876a2a5debeb
Rich Sharp <richpsharp@gmail.com>
2016-10-11 18:18:52 -0700

    re #BITBUCKET-3593 adding test cases to cover all the switch options

--- tests/test_overlap_analysis.py
+++ tests/test_overlap_analysis.py
@@ -61,8 +61,97 @@ class OverlapAnalysisTests(unittest.TestCase):
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
-    def test_overlap_analysis_simple(self):
-        """Overlap Analysis: regression test with msot options disabled."""
+    def test_overlap_analysis_human_intra(self):
+        """Overlap Analysis: regression test with hubs and intra."""
+        import natcap.invest.overlap_analysis.overlap_analysis
+
+        args = {
+            'decay_amt': '0.0001',
+            'do_hubs': True,
+            'do_inter': False,
+            'do_intra': True,
+            'grid_size': '1000',
+            'hubs_uri': os.path.join(SAMPLE_DATA, 'PopulatedPlaces_WCVI.shx'),
+            'intra_name': 'RI',
+            'overlap_data_dir_uri': os.path.join(
+                SAMPLE_DATA, 'FisheriesLayers_RI'),
+            'workspace_dir': self.workspace_dir,
+            'zone_layer_uri': os.path.join(SAMPLE_DATA, 'AOI_WCVI.shp'),
+        }
+        natcap.invest.overlap_analysis.overlap_analysis.execute(args)
+        OverlapAnalysisTests._test_same_files(
+            os.path.join(REGRESSION_DATA, 'expected_file_list.txt'),
+            args['workspace_dir'])
+        pygeoprocessing.testing.assert_rasters_equal(
+            os.path.join(REGRESSION_DATA, 'hu_impscore_human_intra.tif'),
+            os.path.join(self.workspace_dir, 'output', 'hu_impscore.tif'),
+            1e-6)
+
+    @scm.skip_if_data_missing(SAMPLE_DATA)
+    @scm.skip_if_data_missing(REGRESSION_DATA)
+    def test_overlap_analysis_human_inter(self):
+        """Overlap Analysis: regression test with hubs and inter."""
+        import natcap.invest.overlap_analysis.overlap_analysis
+
+        args = {
+            'decay_amt': '0.0001',
+            'do_hubs': True,
+            'do_inter': True,
+            'do_intra': False,
+            'grid_size': '1000',
+            'hubs_uri': os.path.join(SAMPLE_DATA, 'PopulatedPlaces_WCVI.shx'),
+            'intra_name': 'RI',
+            'overlap_layer_tbl': os.path.join(
+                SAMPLE_DATA, 'Fisheries_Inputs.csv'),
+            'overlap_data_dir_uri': os.path.join(
+                SAMPLE_DATA, 'FisheriesLayers_RI'),
+            'workspace_dir': self.workspace_dir,
+            'zone_layer_uri': os.path.join(SAMPLE_DATA, 'AOI_WCVI.shp'),
+        }
+        natcap.invest.overlap_analysis.overlap_analysis.execute(args)
+        OverlapAnalysisTests._test_same_files(
+            os.path.join(
+                REGRESSION_DATA,
+                'expected_file_list_inter_only.txt'),
+            args['workspace_dir'])
+        pygeoprocessing.testing.assert_rasters_equal(
+            os.path.join(REGRESSION_DATA, 'hu_impscore_human_inter.tif'),
+            os.path.join(self.workspace_dir, 'output', 'hu_impscore.tif'),
+            1e-6)
+
+
+    @scm.skip_if_data_missing(SAMPLE_DATA)
+    @scm.skip_if_data_missing(REGRESSION_DATA)
+    def test_overlap_analysis_intra_only(self):
+        """Overlap Analysis: regression test with intra only enabled."""
+        import natcap.invest.overlap_analysis.overlap_analysis
+
+        args = {
+            'do_hubs': False,
+            'do_inter': False,
+            'do_intra': True,
+            'grid_size': '1000',
+            'intra_name': 'RI',
+            'overlap_data_dir_uri': os.path.join(
+                SAMPLE_DATA, 'FisheriesLayers_RI'),
+            'overlap_layer_tbl': os.path.join(
+                SAMPLE_DATA, 'Fisheries_Inputs.csv'),
+            'workspace_dir': self.workspace_dir,
+            'zone_layer_uri': os.path.join(SAMPLE_DATA, 'AOI_WCVI.shp'),
+        }
+        natcap.invest.overlap_analysis.overlap_analysis.execute(args)
+        OverlapAnalysisTests._test_same_files(
+            os.path.join(REGRESSION_DATA, 'expected_file_list_intra_only.txt'),
+            args['workspace_dir'])
+        pygeoprocessing.testing.assert_rasters_equal(
+            os.path.join(REGRESSION_DATA, 'hu_impscore_intra_only.tif'),
+            os.path.join(self.workspace_dir, 'output', 'hu_impscore.tif'),
+            1e-6)
+
+    @scm.skip_if_data_missing(SAMPLE_DATA)
+    @scm.skip_if_data_missing(REGRESSION_DATA)
+    def test_overlap_analysis_inter_only(self):
+        """Overlap Analysis: regression test with inter only enabled."""
         import natcap.invest.overlap_analysis.overlap_analysis
 
         args = {
@@ -79,11 +168,37 @@ class OverlapAnalysisTests(unittest.TestCase):
         }
         natcap.invest.overlap_analysis.overlap_analysis.execute(args)
         OverlapAnalysisTests._test_same_files(
-            os.path.join(REGRESSION_DATA, 'expected_file_list_simple.txt'),
+            os.path.join(REGRESSION_DATA, 'expected_file_list_inter_only.txt'),
             args['workspace_dir'])
         pygeoprocessing.testing.assert_rasters_equal(
-            os.path.join(REGRESSION_DATA, 'hu_impscore_simple.tif'),
+            os.path.join(REGRESSION_DATA, 'hu_impscore_inter_only.tif'),
             os.path.join(self.workspace_dir, 'output', 'hu_impscore.tif'),
+            1e-6)
+
+    @scm.skip_if_data_missing(SAMPLE_DATA)
+    @scm.skip_if_data_missing(REGRESSION_DATA)
+    def test_overlap_analysis_all_disabled(self):
+        """Overlap Analysis: regression test with all options disabled."""
+        import natcap.invest.overlap_analysis.overlap_analysis
+
+        args = {
+            u'do_hubs': False,
+            u'do_inter': False,
+            u'do_intra': False,
+            u'grid_size': u'1000',
+            u'overlap_data_dir_uri': os.path.join(
+                SAMPLE_DATA, 'FisheriesLayers_RI'),
+            u'workspace_dir': self.workspace_dir,
+            u'zone_layer_uri': os.path.join(SAMPLE_DATA, 'AOI_WCVI.shp'),
+        }
+        natcap.invest.overlap_analysis.overlap_analysis.execute(args)
+        OverlapAnalysisTests._test_same_files(
+            os.path.join(
+                REGRESSION_DATA, 'expected_file_list_all_disabled.txt'),
+            args['workspace_dir'])
+        pygeoprocessing.testing.assert_rasters_equal(
+            os.path.join(REGRESSION_DATA, 'hu_freq_all_disabled.tif'),
+            os.path.join(self.workspace_dir, 'output', 'hu_freq.tif'),
             1e-6)
 
     @staticmethod
