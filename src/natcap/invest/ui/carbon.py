544caba19255aa6943b90ea41fdb72dc7bff3162
James Douglass <jdouglass@stanford.edu>
2017-02-16 18:25:45 -0800

    Adding carbon file.  Still needs a little work.  RE:#BITBUCKET-3610

---
+++ src/natcap/invest/ui/carbon.py
@@ -0,0 +1,174 @@
+# coding=UTF-8
+
+from natcap.invest.ui import model
+from natcap.ui import inputs
+import natcap.invest.carbon
+
+class Carbon(model.Model):
+    label = u'InVEST Carbon Model'
+    target = staticmethod(natcap.invest.carbon.execute)
+    validator = staticmethod(natcap.invest.carbon.validate)
+    localdoc = u'../documentation/carbonstorage.html'
+
+    def __init__(self):
+        model.Model.__init__(self)
+
+        self.cur_lulc_raster = inputs.File(
+            args_key=u'lulc_cur_path',
+            helptext=(
+                u"A GDAL-supported raster representing the land-cover "
+                u"of the current scenario."),
+            label=u'Current Land Use/Land Cover (Raster)',
+            required=True,
+            validator=self.validator)
+        self.add_input(self.cur_lulc_raster)
+        self.carbon_pools_path = inputs.File(
+            args_key=u'carbon_pools_path',
+            helptext=(
+                u"A table that maps the land-cover IDs to carbon "
+                u"pools.  The table must contain columns of 'LULC', "
+                u"'C_above', 'C_Below', 'C_Soil', 'C_Dead' as described "
+                u"in the User's Guide.  The values in LULC must at "
+                u"least include the LULC IDs in the land cover maps."),
+            label=u'Carbon Pools',
+            required=True,
+            validator=self.validator)
+        self.add_input(self.carbon_pools_path)
+        self.cur_lulc_year = inputs.Text(
+            args_key=u'lulc_cur_year',
+            helptext=u'The calendar year of the current scenario.',
+            interactive=False,
+            label=u'Current Landcover Calendar Year',
+            required=False,
+            validator=self.validator)
+        self.add_input(self.cur_lulc_year)
+        self.calc_sequestration = inputs.Checkbox(
+            helptext=(
+                u"Check to enable sequestration analysis.  This "
+                u"requires inputs of Land Use/Land Cover maps for both "
+                u"current and future scenarios."),
+            label=u'Calculate Sequestration')
+        self.add_input(self.calc_sequestration)
+        self.fut_lulc_raster = inputs.File(
+            args_key=u'lulc_fut_path',
+            helptext=(
+                u"A GDAL-supported raster representing the land-cover "
+                u"of the future scenario.  <br><br>If REDD scenario "
+                u"analysis is enabled, this should be the reference, or "
+                u"baseline, future scenario against which to compare "
+                u"the REDD policy scenario."),
+            interactive=False,
+            label=u'Future Landcover (Raster)',
+            required=False,
+            validator=self.validator)
+        self.add_input(self.fut_lulc_raster)
+        self.fut_lulc_year = inputs.Text(
+            args_key=u'lulc_fut_year',
+            helptext=u'The calendar year of the future scenario.',
+            interactive=False,
+            label=u'Future Landcover Calendar Year',
+            required=False,
+            validator=self.validator)
+        self.add_input(self.fut_lulc_year)
+        self.redd = inputs.Checkbox(
+            helptext=(
+                u"Check to enable REDD scenario analysis.  This "
+                u"requires three Land Use/Land Cover maps: one for the "
+                u"current scenario, one for the future baseline "
+                u"scenario, and one for the future REDD policy "
+                u"scenario."),
+            interactive=False,
+            label=u'REDD Scenario Analysis')
+        self.add_input(self.redd)
+        self.redd_lulc_raster = inputs.File(
+            args_key=u'lulc_redd_path',
+            helptext=(
+                u"A GDAL-supported raster representing the land-cover "
+                u"of the REDD policy future scenario.  This scenario "
+                u"will be compared to the baseline future scenario."),
+            interactive=False,
+            label=u'REDD Policy (Raster)',
+            required=False,
+            validator=self.validator)
+        self.add_input(self.redd_lulc_raster)
+        self.valuation_container = inputs.Container(
+            args_key=u'do_valuation',
+            expandable=True,
+            expanded=False,
+            interactive=False,
+            label=u'Run Valuation Model')
+        self.add_input(self.valuation_container)
+        self.price_per_metric_ton_of_c = inputs.Text(
+            args_key=u'price_per_metric_ton_of_c',
+            interactive=False,
+            label=u'Price/Metric ton of carbon',
+            required=True,
+            validator=self.validator)
+        self.valuation_container.add_input(self.price_per_metric_ton_of_c)
+        self.discount_rate = inputs.Text(
+            args_key=u'discount_rate',
+            helptext=u'The discount rate as a floating point percent.',
+            interactive=False,
+            label=u'Market Discount in Price of Carbon (%)',
+            required=True,
+            validator=self.validator)
+        self.valuation_container.add_input(self.discount_rate)
+        self.rate_change = inputs.Text(
+            args_key=u'rate_change',
+            helptext=(
+                u"The floating point percent increase of the price of "
+                u"carbon per year."),
+            interactive=False,
+            label=u'Annual Rate of Change in Price of Carbon (%)',
+            required=True,
+            validator=self.validator)
+        self.valuation_container.add_input(self.rate_change)
+
+        # Set interactivity, requirement as input sufficiency changes
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.cur_lulc_year.set_interactive)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.fut_lulc_raster.set_interactive)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.fut_lulc_raster.set_required)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.fut_lulc_year.set_interactive)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.redd.set_interactive)
+        self.redd.sufficiency_changed.connect(
+            self.redd_lulc_raster.set_interactive)
+        self.redd.sufficiency_changed.connect(
+            self.redd_lulc_raster.set_required)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.price_per_metric_ton_of_c.set_interactive)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.discount_rate.set_interactive)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.rate_change.set_interactive)
+        self.calc_sequestration.sufficiency_changed.connect(
+            self.valuation_container.set_interactive)
+
+    def assemble_args(self):
+        args = {
+            self.workspace.args_key: self.workspace.value(),
+            self.suffix.args_key: self.suffix.value(),
+            self.cur_lulc_raster.args_key: self.cur_lulc_raster.value(),
+            self.carbon_pools_path.args_key: self.carbon_pools_path.value(),
+            self.valuation_container.args_key: self.valuation_container.value(),
+        }
+
+        if self.calc_sequestration.value():
+            args[self.redd_lulc_raster.args_key] = self.redd_lulc_raster.value()
+            args[self.cur_lulc_year.args_key] = int(self.cur_lulc_year.value())
+            args[self.fut_lulc_year.args_key] = int(self.fut_lulc_year.value())
+            args[self.fut_lulc_raster.args_key] = self.fut_lulc_raster.value()
+
+            if self.valuation_container.value():
+                args[self.price_per_metric_ton_of_c.args_key] = float(
+                    self.price_per_metric_ton_of_c.value())
+                args[self.discount_rate.args_key] = float(
+                    self.discount_rate.value())
+                args[self.rate_change.args_key] = float(
+                    self.rate_change.value())
+
+        return args
