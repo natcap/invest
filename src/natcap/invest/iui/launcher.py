eff84f705c805483ec8cad574068f9afe51c06fd
James Douglass <jdouglass@stanford.edu>
2017-05-09 11:34:25 -0700

    Launcher now launches models.  RE:#BITBUCKET-3634

--- src/natcap/invest/iui/launcher.py
+++ src/natcap/invest/iui/launcher.py
@@ -10,6 +10,18 @@ if APP is None:
     APP = QtGui.QApplication(sys.argv)
 
 
+class ModelLaunchButton(QtGui.QPushButton):
+    def __init__(self, text, model):
+        QtGui.QPushButton.__init__(self, text)
+        self._model = model
+        self.clicked.connect(self.launch)
+
+    def launch(self, attr):
+        print 'Launching %s' % self._model
+        import natcap.invest.iui.modelui
+        natcap.invest.iui.modelui.main(self._model + '.json')
+
+
 def main():
     launcher_window = QtGui.QMainWindow()
     layout = QtGui.QGridLayout()
@@ -21,11 +33,11 @@ def main():
     for model in cli.list_models():
         row = layout.rowCount()
         label = QtGui.QLabel()
-        button = QtGui.QPushButton()
+        button = ModelLaunchButton('Launch', model)
         labels_and_buttons.append((label, button))
 
         layout.addWidget(QtGui.QLabel(model), row, 0)
-        layout.addWidget(QtGui.QPushButton('Launch'), row, 1)
+        layout.addWidget(button, row, 1)
 
     launcher_window.show()
     APP.exec_()
