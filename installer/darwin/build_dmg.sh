22f91203ee193a6a202faff318ce41079847d8bf
James Douglass <jdouglass@stanford.edu>
2017-05-09 13:29:25 -0700

    Application is now a single .app bundle, not a bundle per model.
    RE:#BITBUCKET-3634

--- installer/darwin/build_dmg.sh
+++ installer/darwin/build_dmg.sh
@@ -21,36 +21,30 @@ fi
 
 # prepare a local temp dir for a filesystem
 mkdir -p "$tempdir"
-invest_bindir="$tempdir"/.`basename $2`  # preceeding . hides the folder from users
-cp -r "$2" "$invest_bindir"
 
 # copy out all the invest shell files and fixup the paths.
 # .command extension makes the scripts runnable by the user.
 # Shell files without the `invest_` prefix will be left alone.
-find "$invest_bindir" -iname "invest_*.sh" | while read sh_file
-do
-    new_name=`echo "$sh_file" | sed 's/\.sh$//g'`
-    new_basename=`basename $new_name`
-    _APPDIR="$tempdir/$new_basename.app"
-    _MACOSDIR="$_APPDIR/Contents/MacOS"
-    _RESOURCEDIR="$_APPDIR/Contents/Resources"
-    mkdir -p "${_MACOSDIR}"
-    mkdir -p "${_RESOURCEDIR}"
-    new_command_file="$_MACOSDIR/$new_basename"
-    mv "$sh_file" "$new_command_file"
-    cp invest.icns "$_RESOURCEDIR/invest.icns"
+new_basename=`InVEST`
+_APPDIR="$tempdir/$new_basename.app"
+_MACOSDIR="$_APPDIR/Contents/MacOS"
+cp -r "$2" "$_MACOSDIR/invest_dist"
+_RESOURCEDIR="$_APPDIR/Contents/Resources"
+mkdir -p "${_MACOSDIR}"
+mkdir -p "${_RESOURCEDIR}"
+new_command_file="$_MACOSDIR/$new_basename"
+cp invest.icns "$_RESOURCEDIR/invest.icns"
 
-    new_plist_file="$_APPDIR/Contents/Info.plist"
-    cp Info.plist "$new_plist_file"
+new_plist_file="$_APPDIR/Contents/Info.plist"
+cp Info.plist "$new_plist_file"
 
-    # replace the version and application name strings in the Info.plist file
-    sed -i '' "s|++NAME++|$new_basename|g" "$new_plist_file"
-    sed -i '' "s|++VERSION++|${1}|g" "$new_plist_file"
+# replace the version and application name strings in the Info.plist file
+sed -i '' "s|++NAME++|$new_basename|g" "$new_plist_file"
+sed -i '' "s|++VERSION++|${1}|g" "$new_plist_file"
 
-    # Three directories up from `dirname $0` is the InVEST installation folder,
-    # which contains invest_dist.
-    sed -i '' 's|./invest|`dirname $0`/../../../.invest_dist/invest|g' "$new_command_file"
-done
+# This is the command that will launch the application.
+echo '#!/bin/bash' > $new_command_file
+echo '`dirname $0`/invest_dist/invest launcher' >> $new_command_file
 
 # copy the docs into the dmg
 cp -r ../../doc/users-guide/build/html $tempdir/documentation
