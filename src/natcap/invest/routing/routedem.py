567daef95a8a81ca65b74e1b523ffccd26ee1c9b
Rich Sharp <richpsharp@gmail.com>
2017-05-31 15:15:25 -0700

    re #BITBUCKET-3646 refactored docstring and two internal calls to use filepatterns

--- src/natcap/invest/routing/routedem.py
+++ src/natcap/invest/routing/routedem.py
@@ -3,10 +3,17 @@ import os
 import logging
 
 import pygeoprocessing
+import pygeoprocessing.routing
 import natcap.invest.pygeoprocessing_0_3_3.routing
 import natcap.invest.pygeoprocessing_0_3_3.routing.routing_core
 
+from .. import utils
+
 LOGGER = logging.getLogger('natcap.invest.routing.routedem')
+
+# replace %s with file suffix
+_TARGET_SLOPE_FILE_PATTERN = 'slope%s.tif'
+_TARGET_FLOW_DIRECTION_FILE_PATTERN = 'flow_direction%s.tif'
 
 
 def execute(args):
@@ -16,74 +23,53 @@ def execute(args):
     the InVEST model API.
 
     Parameters:
-        workspace_dir (string):  The selected folder is used as the workspace
-            where all intermediate and output files will be written. If the
-            selected folder does not exist, it will be created. If
-            datasets already exist in the selected folder, they will be
-            overwritten. (required)
-        dem_uri (string):  A GDAL-supported raster file containing a base
-            Digital Elevation Model to execute the routing functionality
-            across. (required)
-        pit_filled_filename (string):  The filename of the output raster
-            with pits filled in. It will go in the project workspace.
-            (required)
-        flow_direction_filename (string):  The filename of the flow direction
-            raster. It will go in the project workspace. (required)
-        flow_accumulation_filename (string):  The filename of the flow
-            accumulation raster. It will go in the project workspace.
-            (required)
-        threshold_flow_accumulation (int):  The number of upstream cells
-            that must flow into a cell before it's classified as a stream.
-            (required)
-        multiple_stream_thresholds (bool):  Set to ``True`` to calculate
-            multiple maps. If enabled, set stream threshold to the lowest
-            amount, then set upper and step size thresholds. (optional)
-        threshold_flow_accumulation_upper (int):  The number of upstream
-            pixels that must flow into a cell before it's classified as a
-            stream. (required)
-        threshold_flow_accumulation_stepsize (int):  The number of cells
-            to step up from lower to upper threshold range. (required)
-        calculate_slope (bool):  Set to ``True`` to output a slope raster.
-            (optional)
-        slope_filename (string):  The filename of the output slope raster.
-            This will go in the project workspace. (required)
-        calculate_downstream_distance (bool):  Select to calculate a distance
-            stream raster, based on uppper threshold limit. (optional)
-        downstream_distance_filename (string):  The filename of the output
-            raster. It will go in the project workspace. (required)
+        args['workspace_dir'] (string): output directory for intermediate,
+            temporary, and final files
+        args['results_suffix'] (string): (optional) string to append to any
+            output file names
+        args['dem_path'] (string): path to a digital elevation raster
+        args['calculate_stream_threshold'] (bool): if True, model will
+            calculate a stream classification layer by thresholding flow
+            accumulation to the provided value in
+            args['threshold_flow_accumulation'].
+        args['threshold_flow_accumulation'] (int): The number of upstream
+            cells that must flow into a cell before it's classified as a
+            stream.
+        args['calculate_downstream_distance'] (bool): If True, model will
+        args['calculate_slope'] (bool):  Set to ``True`` to output a slope
+            raster.
 
     Returns:
         ``None``
     """
-    output_directory = args['workspace_dir']
-    LOGGER.info('creating directory %s', output_directory)
-    natcap.invest.pygeoprocessing_0_3_3.create_directories([output_directory])
-    dem_uri = args['dem_uri']
+    file_suffix = utils.make_suffix_string(args, 'results_suffix')
+    utils.make_directories([args['workspace_dir']])
 
-    LOGGER.info('resolving filling pits')
-
-    dem_pit_filled_uri = os.path.join(
-        output_directory, args['pit_filled_filename'])
-    natcap.invest.pygeoprocessing_0_3_3.routing.fill_pits(dem_uri, dem_pit_filled_uri)
-    dem_uri = dem_pit_filled_uri
+    dem_raster_path_band = (args['dem_path'], 1)
 
     # Calculate slope
-    if args['calculate_slope']:
+    if 'calculate_slope' in args and bool(args['calculate_slope']):
         LOGGER.info("Calculating slope")
-        slope_uri = os.path.join(output_directory, args['slope_filename'])
-        natcap.invest.pygeoprocessing_0_3_3.geoprocessing.calculate_slope(dem_uri, slope_uri)
+        target_slope_path = os.path.join(
+            args['workspace_dir'], _TARGET_SLOPE_FILE_PATTERN % file_suffix)
+        pygeoprocessing.calculate_slope(
+            dem_raster_path_band, target_slope_path)
 
     # Calculate flow accumulation
     LOGGER.info("calculating flow direction")
-    flow_direction_uri = os.path.join(
-        output_directory, args['flow_direction_filename'])
-    natcap.invest.pygeoprocessing_0_3_3.routing.flow_direction_d_inf(dem_uri, flow_direction_uri)
+    target_flow_direction_path = os.path.join(
+        args['workspace_dir'],
+        _TARGET_FLOW_DIRECTION_FILE_PATTERN % file_suffix)
+    pygeoprocessing.routing.flow_direction_d_inf(
+        dem_raster_path_band, target_flow_direction_path)
+
+    sys.exit()
 
     LOGGER.info("calculating flow accumulation")
-    flow_accumulation_uri = os.path.join(
+    flow_accumulation_path = os.path.join(
         output_directory, args['flow_accumulation_filename'])
     natcap.invest.pygeoprocessing_0_3_3.routing.flow_accumulation(
-        flow_direction_uri, dem_uri, flow_accumulation_uri)
+        flow_direction_path, dem_path, flow_accumulation_path)
 
     # classify streams from the flow accumulation raster
     LOGGER.info("Classifying streams from flow accumulation raster")
@@ -98,20 +84,20 @@ def execute(args):
             LOGGER.info(
                 "Calculating stream threshold at %s pixels",
                 threshold_amount)
-            v_stream_uri = os.path.join(
+            v_stream_path = os.path.join(
                 output_directory, 'v_stream_%s.tif' %
                 (str(threshold_amount),))
 
             natcap.invest.pygeoprocessing_0_3_3.routing.stream_threshold(
-                flow_accumulation_uri, threshold_amount, v_stream_uri)
+                flow_accumulation_path, threshold_amount, v_stream_path)
     else:
-        v_stream_uri = os.path.join(output_directory, 'v_stream.tif')
+        v_stream_path = os.path.join(output_directory, 'v_stream.tif')
         natcap.invest.pygeoprocessing_0_3_3.routing.stream_threshold(
-            flow_accumulation_uri, float(args['threshold_flow_accumulation']),
-            v_stream_uri)
+            flow_accumulation_path, float(args['threshold_flow_accumulation']),
+            v_stream_path)
 
     if args['calculate_downstream_distance']:
-        distance_uri = os.path.join(
+        distance_path = os.path.join(
             output_directory, args['downstream_distance_filename'])
         natcap.invest.pygeoprocessing_0_3_3.routing.distance_to_stream(
-            flow_direction_uri, v_stream_uri, distance_uri)
+            flow_direction_path, v_stream_path, distance_path)
