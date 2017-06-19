e7975787d2abc5d3f33ac6cace4dac7b925bef21
James Douglass <jdouglass@stanford.edu>
2017-06-19 15:33:14 -0700

    Correcting call to CLI._format_args().  RE:#BITBUCKET-3610

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -7,15 +7,12 @@ class UtilitiesTest(unittest.TestCase):
     def test_print_args(self):
         from natcap.invest.cli import _format_args
 
-        args_iterable = ('foo', 'bar', 'baz')
-
         args_dict = {
             'some_arg': [1, 2, 3, 4],
             'foo': 'bar',
         }
 
-        args_string = _format_args(args_iterable=args_iterable,
-                                   args_dict=args_dict)
+        args_string = _format_args(args_dict=args_dict)
         expected_string = six.text_type(
             'Arguments:\n'
             'foo      bar\n'
