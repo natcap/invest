e2ab044c1df86c81d7428a8aa064f14d4b2139c4
James Douglass <jdouglass@stanford.edu>
2017-07-05 16:41:01 -0700

    Trying to update hooks to no longer use get_package_paths()
    RE:#BITBUCKET-3610

--- exe/hooks/hook-shapely.py
+++ exe/hooks/hook-shapely.py
@@ -3,14 +3,14 @@ import os
 
 if is_darwin:
     from PyInstaller.utils.hooks import \
-        (collect_submodules, collect_data_files, get_package_paths)
+        (collect_submodules, collect_data_files)
 else:
     from hookutils import \
-    (collect_submodules, collect_data_files, get_package_paths)
+        (collect_submodules, collect_data_files)
 
+import shapely
 hiddenimports = collect_submodules('shapely')
-pkg_base, pkg_dir = get_package_paths('shapely')
 datas = collect_data_files('shapely')
 
 if is_win:
-    datas += [(os.path.join(pkg_dir, 'DLLs/geos_c.dll'), '')]
+    datas += [(os.path.join(shapely.__path__[0], 'DLLs/geos_c.dll'), '')]
