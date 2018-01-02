572d184c20f4b0da396678cca6f02e5de0715901
Rich Sharp <richpsharp@gmail.com>
2018-01-02 10:14:23 -0800

    Re #BITBUCKET-3719 no more update=1

--- src/natcap/invest/fisheries/fisheries_io.py
+++ src/natcap/invest/fisheries/fisheries_io.py
@@ -999,7 +999,7 @@ def _create_results_aoi(vars_dict):
     natcap.invest.pygeoprocessing_0_3_3.geoprocessing.copy_datasource_uri(aoi_uri, output_aoi_uri)
 
     # Append attributes to Shapefile
-    ds = gdal.OpenEx(output_aoi_uri, update=1)
+    ds = gdal.OpenEx(output_aoi_uri, gdal.GA_Update)
     layer = ds.GetLayer()
 
     # Set Harvest
