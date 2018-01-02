2a5c8cc881b940a9302a08993a3d2135690896f0
Rich Sharp <richpsharp@gmail.com>
2018-01-02 10:19:43 -0800

    re #BITBUCKET-3719, integers now have numbers of bit associated with them

--- src/natcap/invest/pygeoprocessing_0_3_3/geoprocessing.py
+++ src/natcap/invest/pygeoprocessing_0_3_3/geoprocessing.py
@@ -798,7 +798,7 @@ def aggregate_raster_values_uri(
                 (shapefile_uri, shapefile_field))
 
         field_def = layer_d.GetFieldDefn(field_index)
-        if field_def.GetTypeName() != 'Integer':
+        if  'Integer' not in field_def.GetTypeName():
             raise TypeError(
                 'Can only aggregate by integer based fields, requested '
                 'field is of type  %s' % field_def.GetTypeName())
