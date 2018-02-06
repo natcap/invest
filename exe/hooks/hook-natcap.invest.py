5d8838f308b2b470679ccb43eda319e7268e28c9
James Douglass <jdouglass@stanford.edu>
2018-02-06 15:35:02 -0800

    Updating natcap.invest hook; forgot an import.  RE:#BITBUCKET-3714

--- exe/hooks/hook-natcap.invest.py
+++ exe/hooks/hook-natcap.invest.py
@@ -1,4 +1,6 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import (collect_data_files,
+                                     collect_submodules,
+                                     copy_metadata)
 
 hiddenimports = collect_submodules('natcap.invest')
 datas = copy_metadata("natcap.invest") + collect_data_files('natcap.invest')
