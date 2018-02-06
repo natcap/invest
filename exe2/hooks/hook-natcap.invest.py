4a764690e7574cffd356bdf5f7d2cebdfa139faa
James Douglass <jdouglass@stanford.edu>
2018-02-06 15:19:43 -0800

    Modifications to the exe2 hooks RE:#BITBUCKET-3714

--- exe2/hooks/hook-natcap.invest.py
+++ exe2/hooks/hook-natcap.invest.py
@@ -1,3 +1,4 @@
 from PyInstaller.utils.hooks import copy_metadata
 
 datas = copy_metadata("natcap.invest")
+hiddenimports = ['faulthandler', 'natcap.invest.ui.launcher']
--- exe2/hooks/hook-osgeo.py
+++ exe2/hooks/hook-osgeo.py
@@ -8,5 +8,4 @@ if is_darwin:
     from PyInstaller.utils.hooks import collect_system_data_files
 
     datas = collect_system_data_files(
-        path=os.path.join(get_homebrew_path('gdal'), 'share', 'gdal'),
-        destdir='gdal-data')
+        path=os.path.join(get_homebrew_path('gdal'), 'share', 'gdal'))
