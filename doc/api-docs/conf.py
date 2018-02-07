a6a884467a465a45b4f76df278449243baaeeef8
James Douglass <jdouglass@stanford.edu>
2018-02-07 11:47:58 -0800

    Updating API docs conf.py.
    Removes natcap.versioner dependency and mocks out the pygeoprocessing
    __version__ attribute so we can properly import the natcap.invest python
    source.  RE:#BITBUCKET-3714

--- doc/api-docs/conf.py
+++ doc/api-docs/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import imp
+import ast
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -57,9 +58,12 @@ copyright = u'2015, The Natural Capital Project'
 # built documents.
 #
 # The short X.Y version.
-import natcap.versioner
-_version = natcap.versioner.parse_version(
-    root=os.path.join(DOCS_SOURCE_DIR, '..', '..'))
+import setuptools_scm
+_version = setuptools_scm.get_version(
+    root= os.path.join(DOCS_SOURCE_DIR, '..', '..'),
+    version_scheme='post-release',
+    local_scheme='node-and-date'
+)
 version = _version.split('+')[0]
 # The full version, including alpha/beta/rc tags.
 release = _version
@@ -295,7 +299,7 @@ autodoc_mock_imports = [
     'shapely.geometry', 'osgeo.osr', 'osgeo.gdal', 'gdal',
     'shapely.wkt', 'shapely.ops', 'shapely.speedups',
     'shapely.prepared', 'qgis.utils', 'grass.script.setup', 'PyQt4.QtTest',
-    'PyQt4.QtCore', 'geoprocessing_core'
+    'PyQt4.QtCore', 'geoprocessing_core', 'pygeoprocessing',
 ]
 
 # Mock class with attribute handling.  As suggested by:
@@ -310,6 +314,17 @@ class Mock(mock.Mock):
 # mocked up.
 for name in autodoc_mock_imports:
     sys.modules[name] = Mock()
+
+# mock the pygeoprocessing version to be whatever is in the natcap.invest
+# __init__ file.
+_invest_init = os.path.join(
+    DOCS_SOURCE_DIR, '..', '..', 'src', 'natcap', 'invest',
+    '__init__.py')
+with open(_invest_init) as init_file:
+    for line in init_file:
+        if line.startswith('PYGEOPROCESSING_REQUIRED'):
+            pgp_version = ast.literal_eval(line.split(' ')[-1].strip())
+            sys.modules['pygeoprocessing'].__version__ = pgp_version
 
 sys.modules['osgeo'].gdal.GetDriverCount.return_value = 1
 sys.modules['osgeo.gdal'].GetDriverCount.return_value = 1
