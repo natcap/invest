78d5d58b28f81fe1e87cebd3559b5d8fcc029394
James Douglass <jdouglass@stanford.edu>
2018-02-20 14:32:47 -0800

    Adding pip and setuptools_scm to the userguide deps list.  RE:#BITBUCKET-3714

--- scripts/build_install_userguide_deps.sh
+++ scripts/build_install_userguide_deps.sh
@@ -19,6 +19,9 @@ apt-get update && apt-get install -y \
     texlive-generic-extra=2016.20170123-5 \
     texlive-latex-recommended=2016.20170123-5 \
     texlive-fonts-recommended=2016.20170123-5 \
-    texlive-latex-extra=2016.20170123-5
+    texlive-latex-extra=2016.20170123-5 \
+    python-pip=9.0.1-2
+
+pip install setuptools_scm==1.15.7
     
 
