be4a8d17b217e2bd8f8b4ab9f39f1a77d71a5661
James Douglass <jdouglass@stanford.edu>
2018-02-20 12:49:25 -0800

    Adding a build script to install zipfile dependencies.
    RE:#BITBUCKET-3714

---
+++ scripts/build_install_zipfiles.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @file        : build_install_zipfiles
+# @description : Install apt packages to be able to build sampledata zipfiles. 
+#                Assumes debian:stretch.
+######################################################################
+
+apt-get update && apt-get install -y \
+    subversion=1.9.5-1 \
+    zip=3.0-11+b1 \
+    make=4.1-9.1 \
+    mercurial=4.0-1+deb9u1
