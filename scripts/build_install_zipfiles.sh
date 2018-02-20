452e1f4fda96e9b360c957c079be47cc405d02b3
James Douglass <jdouglass@stanford.edu>
2018-02-20 12:51:01 -0800

    Correcting subversion dependency number.  RE:#BITBUCKET-3714

--- scripts/build_install_zipfiles.sh
+++ scripts/build_install_zipfiles.sh
@@ -7,7 +7,7 @@
 ######################################################################
 
 apt-get update && apt-get install -y \
-    subversion=1.9.5-1 \
+    subversion=1.9.5-1+deb9u1 \
     zip=3.0-11+b1 \
     make=4.1-9.1 \
     mercurial=4.0-1+deb9u1
