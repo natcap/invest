abe639731f763ab18bd017e6db8c75154e39ed95
James Douglass <jdouglass@stanford.edu>
2018-03-02 10:49:06 -0800

    Supporting the parsing of multiple requirements files via argparse.
    RE:#BITBUCKET-3714

--- scripts/convert-requirements-to-conda-yml.py
+++ scripts/convert-requirements-to-conda-yml.py
@@ -2,6 +2,7 @@
 """convert-requirements-to-conda-yml.py"""
 
 import sys
+import argparse
 import requests
 import pkg_resources
 
@@ -20,28 +21,44 @@ SCM_MAP = {
     'git': 'git',
 }
 
+def ui(args=None):
+    if args is None:
+        args = sys.argv[1:]
 
-def main():
+    parser = argparse.ArgumentParser(description=(
+        'Convert a ser of pip requirements.txt files into an environment '
+        'file for use by `conda create`.'
+    ), prog=__file__)
+
+    parser.add_argument('req', nargs='+',
+                        help='A requirements.txt file to analyze')
+
+    args = parser.parse_args(args)
+    main(*args.req)
+
+
+def main(*requirements_files):
     pip_requirements = set([])
     conda_requirements = set(['python=2.7'])
-    for line in open('requirements.txt'):
-        line = line.strip()
-        if len(line) == 0 or line.startswith('#'):
-            continue
+    for requirement_file in requirements_files:
+        for line in open(requirement_file):
+            line = line.strip()
+            if len(line) == 0 or line.startswith('#'):
+                continue
 
-        if line.startswith(tuple(SCM_MAP.keys())):
-            pip_requirements.add(line)
-            conda_requirements.add(SCM_MAP[line.split('+')[0]])
-            continue
+            if line.startswith(tuple(SCM_MAP.keys())):
+                pip_requirements.add(line)
+                conda_requirements.add(SCM_MAP[line.split('+')[0]])
+                continue
 
-        requirement = pkg_resources.Requirement.parse(line)
-        conda_forge_url = FEEDSTOCK_URL.format(
-            package=requirement.project_name.lower())
-        if (requests.get(conda_forge_url).status_code == 200 and not
-                line.endswith('# pip-only')):
-            conda_requirements.add(line)
-        else:
-            pip_requirements.add(line)
+            requirement = pkg_resources.Requirement.parse(line)
+            conda_forge_url = FEEDSTOCK_URL.format(
+                package=requirement.project_name.lower())
+            if (requests.get(conda_forge_url).status_code == 200 and not
+                    line.endswith('# pip-only')):
+                conda_requirements.add(line)
+            else:
+                pip_requirements.add(line)
 
     conda_deps_string = '\n'.join(['- %s' % dep for dep in
                                    sorted(conda_requirements,
@@ -55,8 +72,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    ui()
 
 # TODO: document
-# TODO: add argparse UI
 # TODO: resolve dependencies by calling conda?
