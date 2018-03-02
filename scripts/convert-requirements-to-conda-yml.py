6154e8f75802daaba9cb979273ea116af3533bb2
James Douglass <jdouglass@stanford.edu>
2018-03-02 11:00:40 -0800

    Consolidating the UI and main functions and documenting.  RE:#BITBUCKET-3714

--- scripts/convert-requirements-to-conda-yml.py
+++ scripts/convert-requirements-to-conda-yml.py
@@ -21,10 +21,28 @@ SCM_MAP = {
     'git': 'git',
 }
 
-def ui(args=None):
-    if args is None:
-        args = sys.argv[1:]
 
+def build_environment_from_requirements(cli_args):
+    """Build a conda environment.yml from requirements.txt files.
+
+    This script assumes a couple of rules about what should be installed by
+    conda and what should be installed by pip:
+
+        1. If the requirement is installed from git or hg, it should be
+           installed by pip.
+        2. If the requirement is followed by the commend '# pip-only', it
+           should be installed by pip.
+        3. If the requirement is available on conda-forge, it should be
+           installed by conda.
+        4. Otherwise, it should be installed by pip.
+
+
+    Arguments:
+        cli_args (list): A list of command-line arguments.
+
+    Returns:
+        ``None``
+    """
     parser = argparse.ArgumentParser(description=(
         'Convert a ser of pip requirements.txt files into an environment '
         'file for use by `conda create`.'
@@ -33,19 +51,20 @@ def ui(args=None):
     parser.add_argument('req', nargs='+',
                         help='A requirements.txt file to analyze')
 
-    args = parser.parse_args(args)
-    main(*args.req)
+    args = parser.parse_args(cli_args)
+    requirements_files = args.req
 
-
-def main(*requirements_files):
     pip_requirements = set([])
     conda_requirements = set(['python=2.7'])
     for requirement_file in requirements_files:
         for line in open(requirement_file):
             line = line.strip()
+
+            # Blank line or comment
             if len(line) == 0 or line.startswith('#'):
                 continue
 
+            # Checked out from scm
             if line.startswith(tuple(SCM_MAP.keys())):
                 pip_requirements.add(line)
                 conda_requirements.add(SCM_MAP[line.split('+')[0]])
@@ -72,7 +91,6 @@ def main(*requirements_files):
 
 
 if __name__ == '__main__':
-    ui()
+    build_environment_from_requirements(sys.argv[1:])
 
-# TODO: document
 # TODO: resolve dependencies by calling conda?
