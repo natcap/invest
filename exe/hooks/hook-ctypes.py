ed42a29a891715caac89894b22cfe441c08b82bd
James Douglass <jdouglass@stanford.edu>
2018-04-05 13:23:54 -0700

    Updating conditionals so that all hooks expect pyinstaller 3.1.1.
    RE:#BITBUCKET-3714

--- exe/hooks/hook-ctypes.py
+++ exe/hooks/hook-ctypes.py
@@ -1,8 +1,3 @@
-from PyInstaller.compat import is_darwin, is_win
-
-if is_darwin or is_win:
-    from PyInstaller.utils.hooks import collect_submodules
-else:
-    from hookutils import collect_submodules
+from PyInstaller.utils.hooks import collect_submodules
 
 hiddenimports = collect_submodules('ctypes')
