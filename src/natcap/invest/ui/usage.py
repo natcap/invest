b6c2c9d93cc63272b3cd78789f5cda9920214725
James Douglass <jdouglass@stanford.edu>
2018-04-16 17:02:02 -0700

    Only merging bounding boxes when the inputs are demonstrably spatial.
    RE:#BITBUCKET-3733

--- src/natcap/invest/ui/usage.py
+++ src/natcap/invest/ui/usage.py
@@ -154,41 +154,43 @@ def _calculate_args_bounding_box(args_dict):
             # singular value, test if GIS type, if not, don't update bb's
             # this is an undefined bounding box that gets returned when ogr
             # opens a table only
-            local_bb = [0., 0., 0., 0.]
             if _is_gdal(arg):
-                raster_info = pygeoprocessing.get_raster_info(arg)
-                local_bb = raster_info['bounding_box']
-                projection_wkt = raster_info['projection']
-                spatial_ref = osr.SpatialReference()
-                spatial_ref.ImportFromWkt(projection_wkt)
+                spatial_info = pygeoprocessing.get_raster_info(arg)
             elif _is_ogr(arg):
-                vector_info = pygeoprocessing.get_vector_info(arg)
-                local_bb = vector_info['bounding_box']
-                projection_wkt = vector_info['projection']
+                spatial_info = pygeoprocessing.get_vector_info(arg)
+            else:
+                # File is not geospatial!
+                spatial_info = None
+
+            if spatial_info is not None:
+                local_bb = [0., 0., 0., 0.]
+                local_bb = spatial_info['bounding_box']
+                projection_wkt = spatial_info['projection']
                 spatial_ref = osr.SpatialReference()
                 spatial_ref.ImportFromWkt(projection_wkt)
-            try:
-                # means there's a GIS type with a well defined bounding box
-                # create transform, and reproject local bounding box to lat/lng
-                lat_lng_ref = osr.SpatialReference()
-                lat_lng_ref.ImportFromEPSG(4326)  # EPSG 4326 is lat/lng
-                to_lat_trans = osr.CoordinateTransformation(
-                    spatial_ref, lat_lng_ref)
-                for point_index in [0, 2]:
-                    local_bb[point_index], local_bb[point_index + 1], _ = (
-                        to_lat_trans.TransformPoint(
-                            local_bb[point_index], local_bb[point_index + 1]))
 
-                bb_intersection = _merge_bounding_boxes(
-                    local_bb, bb_intersection, 'intersection')
-                bb_union = _merge_bounding_boxes(
-                    local_bb, bb_union, 'union')
-            except Exception as transform_error:
-                # All kinds of exceptions from bad transforms or CSV files
-                # or dbf files could get us to this point, just don't bother
-                # with the local_bb at all
-                LOGGER.exception('Error when transforming coordinates: %s',
-                                 transform_error)
+                try:
+                    # means there's a GIS type with a well defined bounding box
+                    # create transform, and reproject local bounding box to lat/lng
+                    lat_lng_ref = osr.SpatialReference()
+                    lat_lng_ref.ImportFromEPSG(4326)  # EPSG 4326 is lat/lng
+                    to_lat_trans = osr.CoordinateTransformation(
+                        spatial_ref, lat_lng_ref)
+                    for point_index in [0, 2]:
+                        local_bb[point_index], local_bb[point_index + 1], _ = (
+                            to_lat_trans.TransformPoint(
+                                local_bb[point_index], local_bb[point_index + 1]))
+
+                    bb_intersection = _merge_bounding_boxes(
+                        local_bb, bb_intersection, 'intersection')
+                    bb_union = _merge_bounding_boxes(
+                        local_bb, bb_union, 'union')
+                except Exception as transform_error:
+                    # All kinds of exceptions from bad transforms or CSV files
+                    # or dbf files could get us to this point, just don't bother
+                    # with the local_bb at all
+                    LOGGER.exception('Error when transforming coordinates: %s',
+                                     transform_error)
 
         return bb_intersection, bb_union
 
