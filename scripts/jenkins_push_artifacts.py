7b66437eeeeb76a4d06261d64413e355a6886642
James Douglass <jdouglass@stanford.edu>
2018-04-25 15:45:00 -0700

    Trimming down the push function.  RE:#BITBUCKET-3714

--- scripts/jenkins_push_artifacts.py
+++ scripts/jenkins_push_artifacts.py
@@ -2,11 +2,10 @@
 """jenkins_push_artifacts.py"""
 import os
 import platform
-import getpass
-import socket
 import time
 import subprocess
 import logging
+import glob
 
 import paramiko
 from paramiko import SSHClient
@@ -19,6 +18,7 @@ JENKINS_PRIVATE_KEY_PATH = os.path.expanduser(
     os.path.join('~', '.ssh', 'dataportal-id_rsa'))
 DATAPORTAL_USER = 'dataportal'
 DATAPORTAL_HOST = 'data.naturalcapitalproject.org'
+DIST_DIR = 'dist'
 
 
 def _fix_path(path):
@@ -45,81 +45,54 @@ def _sftp_callback(bytes_transferred, total_bytes):
         _sftp_callback.last_time = time.time()
 
 
-
-def push(private_key_path, target_dir, files_to_push, files_to_unzip):
+def push(target_dir, files_to_push, files_to_unzip=None):
     """Push (and optionally unzip) files on dataportal.
 
     Parameters:
-        private_key_path (string): The path to a private key on disk that
-            should be used in addition to any system keys.
         target_dir (string): The directory on dataportal where files should be
             uploaded.
         files_to_push (list of strings): A list of string paths to files that
             should all be uploaded to ``target_dir``.
-        files_to_unzip (list of string): A list of string paths that are in
+        files_to_unzip (list of strings): A list of string paths that are in
             ``files_to_push`` that should be unzipped after upload.
 
     Returns:
         None."""
-    hg_path = subprocess.check_output('hg showconfig paths.default',
-                                      shell=True).rstrip()
 
-    username, reponame = hg_path.split('/')[-2:]
-    version_string = setuptools_scm.get_version(
-        root=os.path.join(os.path.dirname(__file__), '..'),
-        version_scheme='post-release',
-        local_scheme='node-and-date')
-
-    if username == 'natcap' and reponame == 'invest':
-        if 'post' in version_string:
-            data_dirname = 'develop'
-        else:
-            data_dirname = version_string
-
-        data_dir = os.path.join('invest-data', data_dirname)
-        release_dir = os.path.join('invest-releases', version_string)
-
-    else:
-        release_dir = os.path.join('nightly-build', 'invest-forks', username)
-        data_dir = os.path.join(release_dir, 'data')
-
-    LOGGER.debug('Release dir: %s', release_dir)
-    LOGGER.debug('Data dir: %s', data_dir)
+    if files_to_unzip is None:
+        files_to_unzip = []
 
     LOGGER.info('Writing paramiko logging to paramiko-log.txt')
     paramiko.util.log_to_file('paramiko-log.txt')
 
-    ssh = SSHClient()
-    ssh.load_system_host_keys()
+    #ssh = SSHClient()
+    #ssh.load_system_host_keys()
 
     # Automatically add host key if needed
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+    #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #private_key = paramiko.RSAKey.from_private_key_file(
+    #    JENKINS_PRIVATE_KEY_PATH)
 
-    target_dir = 'public_html'
-
-    ssh.connect(DATAPORTAL_HOST, 22, username=DATAPORTAL_USER, password=None,
-                pkey=private_key)
+    #ssh.connect(DATAPORTAL_HOST, 22, username=DATAPORTAL_USER, password=None,
+    #            pkey=private_key)
 
     # Make folders on remote if needed.
-    if target_dir is not None:
-        ssh.exec_command(
-            'if [ ! -d "{dir}" ]\nthen\nmkdir -p -v {dir}\nfi'.format(
-                dir=target_dir))
+    #ssh.exec_command(
+    #    'if [ ! -d "{dir}" ]\nthen\nmkdir -p -v {dir}\nfi'.format(
+    #        dir=target_dir))
 
     print 'Opening SCP connection'
-    sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
+    #sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
+
     for transfer_file in files_to_push:
-        file_basename = os.path.basename(transfer_file)
-        if target_dir is not None:
-            target_filename = os.path.join(target_dir, file_basename)
-        else:
-            target_filename = file_basename
+        target_filename = os.path.join(target_dir,
+                                       os.path.basename(transfer_file))
 
         # Convert windows to linux paths
         target_filename = _fix_path(target_filename)
-        print 'Transferring %s -> %s ' % (os.path.basename(transfer_file),
+        print 'Transferring %s -> %s ' % (transfer_file,
                                           target_filename)
+        continue
         for repeat in [True, True, False]:
             try:
                 sftp.put(transfer_file, target_filename,
@@ -130,20 +103,21 @@ def push(private_key_path, target_dir, files_to_push, files_to_unzip):
                 if not repeat:
                     raise filesize_inconsistency
 
+    return
+
     # correct the filepath from Windows to Linux
     if platform.system() == 'Windows':
-        release_dir = release_dir.replace(os.sep, '/')
+        target_dir = target_dir.replace(os.sep, '/')
 
-    if release_dir.startswith('public_html/'):
-        release_dir = release_dir.replace('public_html/', '')
+    if target_dir.startswith('public_html/'):
+        target_dir = target_dir.replace('public_html/', '')
 
-    for filename in files_to_unzip:
-
+    for filename in files_to_unzip: 
         print 'Unzipping %s on remote' % filename
         ssh.exec_command(
             ('cd public_html/{releasedir}; '
              'unzip -o `ls -tr {zipfile} | tail -n 1`').format(
-                 releasedir=release_dir,
+                 releasedir=target_dir,
                  zipfile=filename
              )
         )
@@ -156,7 +130,42 @@ def push(private_key_path, target_dir, files_to_push, files_to_unzip):
 
 
 if __name__ == '__main__':
-    push()
+    hg_path = subprocess.check_output('hg showconfig paths.default',
+                                      shell=True).rstrip()
 
+    username, reponame = hg_path.split('/')[-2:]
+    version_string = setuptools_scm.get_version(
+        root=os.path.join(os.path.dirname(__file__), '..'),
+        version_scheme='post-release',
+        local_scheme='node-and-date')
+    print 'Using version string %s' % version_string
 
+    if username == 'natcap' and reponame == 'invest':
+        if 'post' in version_string:
+            data_dirname = 'develop'
+        else:
+            data_dirname = version_string
 
+        data_dir = os.path.join('public_html', 'invest-data', data_dirname)
+        release_dir = os.path.join('public_html', 'invest-releases',
+                                   version_string)
+
+    else:
+        release_dir = os.path.join('public_html', 'nightly-build',
+                                   'invest-forks', username)
+        data_dir = os.path.join(release_dir, 'data')
+
+    LOGGER.debug('Release dir: %s', release_dir)
+    LOGGER.debug('Data dir: %s', data_dir)
+
+    LOGGER.info('Uploading data files')
+    push(data_dir, glob.glob(os.path.join(DIST_DIR, 'data', '*.zip')))
+
+    files_to_upload = filter(lambda x: version_string in x,
+                             glob.glob(os.path.join(DIST_DIR, '*.*')))
+    files_to_unzip = filter(lambda x: 'userguide' in x or 'apidocs' in x,
+                            files_to_upload)
+    print files_to_upload
+    print files_to_unzip
+    LOGGER.info('Uploading non-data files, unzipping documentation')
+    push(release_dir, files_to_upload, files_to_unzip)
