a6fc96a78d52a7dc62bd39d5d89963b580c020e1
James Douglass <jdouglass@stanford.edu>
2018-04-26 17:07:16 -0700

    Correcting a pip install command in the windows jenkins build script.
    RE:#BITBUCKET-3714

--- scripts/jenkins-build.bat
+++ scripts/jenkins-build.bat
@@ -6,6 +6,6 @@
 :: ``make jenkins`` target on Windows.
 make env
 env\Scripts\python.exe -m pip install --upgrade .
-env\Scripts\python.exe install -r requirements-gui.txt
+env\Scripts\python.exe -m pip install -r requirements-gui.txt
 make PYTHON=env\Scripts\python.exe windows_installer sampledata userguide python_packages
 env\scripts\python.exe scripts\jenkins_push_artifacts.py
