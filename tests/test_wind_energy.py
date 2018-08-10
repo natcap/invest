edb073e8f7b890ec202a5293952e90a37cb58c2c
Joanna Lin <chiayilin94@gmail.com>
2018-08-10 15:06:24 -0700

    removed test_val_gridpts_windsched because grid points and wind schedule have been tested in test_val_gridpts_windprice and test_time_period_exceptoin. They are also not binded with each other. see #BITBUCKET-3767.

--- tests/test_wind_energy.py
+++ tests/test_wind_energy.py
@@ -700,51 +700,6 @@ class WindEnergyRegressionTests(unittest.TestCase):
 
     # @scm.skip_if_data_missing(SAMPLE_DATA)
     # @scm.skip_if_data_missing(REGRESSION_DATA)
-    # def test_val_gridpts_windsched(self):
-    #     """WindEnergy: testing Valuation w/ grid points and wind sched."""
-    #     from natcap.invest.wind_energy import wind_energy
-
-    #     args = WindEnergyRegressionTests.generate_base_args(self.workspace_dir)
-
-    #     args['aoi_uri'] = os.path.join(
-    #         SAMPLE_DATA, 'WindEnergy', 'input', 'New_England_US_Aoi.shp')
-    #     args['land_polygon_uri'] = os.path.join(
-    #         SAMPLE_DATA, 'Base_Data', 'Marine', 'Land', 'global_polygon.shp')
-    #     args['min_distance'] = 0
-    #     args['max_distance'] = 200000
-    #     args['valuation_container'] = True
-    #     args['foundation_cost'] = 2
-    #     args['discount_rate'] = 0.07
-    #     args['grid_points_uri'] = os.path.join(
-    #         SAMPLE_DATA, 'WindEnergy', 'input', 'NE_sub_pts.csv')
-    #     args['price_table'] = True
-    #     args['wind_schedule'] = os.path.join(
-    #         SAMPLE_DATA, 'WindEnergy', 'input', 'price_table_example.csv')
-
-    #     wind_energy.execute(args)
-
-    #     raster_results = [
-    #         'carbon_emissions_tons.tif',
-    #         'levelized_cost_price_per_kWh.tif', 'npv_US_millions.tif']
-
-    #     for raster_path in raster_results:
-    #         natcap.invest.pygeoprocessing_0_3_3.testing.assert_rasters_equal(
-    #             os.path.join(args['workspace_dir'], 'output', raster_path),
-    #             os.path.join(REGRESSION_DATA, 'pricetablegridpts',
-    #                          raster_path))
-
-    #     vector_results = [
-    #         'example_size_and_orientation_of_a_possible_wind_farm.shp',
-    #         'wind_energy_points.shp']
-
-    #     for vector_path in vector_results:
-    #         natcap.invest.pygeoprocessing_0_3_3.testing.assert_vectors_equal(
-    #             os.path.join(args['workspace_dir'], 'output', vector_path),
-    #             os.path.join(
-    #                 REGRESSION_DATA, 'pricetablegridpts', vector_path))
-
-    # @scm.skip_if_data_missing(SAMPLE_DATA)
-    # @scm.skip_if_data_missing(REGRESSION_DATA)
     # def test_val_avggriddist_windprice(self):
     #     """WindEnergy: testing Valuation w/ avg grid distances and wind price."""
     #     from natcap.invest.wind_energy import wind_energy
