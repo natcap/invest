7e85fc0baddb0d9aff4bbebea78de902a49d6b91
Joanna Lin <chiayilin94@gmail.com>
2018-08-10 13:53:36 -0700

    delete test_no_valuation function because this is tested previously in test_no_distances. see #BITBUCKET-3767.

--- tests/test_wind_energy.py
+++ tests/test_wind_energy.py
@@ -690,40 +690,6 @@ class WindEnergyRegressionTests(unittest.TestCase):
                 os.path.join(tempdir, 'output', vector_path),
                 os.path.join(tests_results_dir, 'nodistances', vector_path))
 
-    @scm.skip_if_data_missing(SAMPLE_DATA)
-    @scm.skip_if_data_missing(REGRESSION_DATA)
-    def test_no_valuation(self):
-        """WindEnergy: testing case w/ AOI, land poly, and distances."""
-        from natcap.invest.wind_energy import wind_energy
-
-        args = WindEnergyRegressionTests.generate_base_args(self.workspace_dir)
-
-        args['aoi_uri'] = os.path.join(
-            SAMPLE_DATA, 'WindEnergy', 'input', 'New_England_US_Aoi.shp')
-        args['land_polygon_uri'] = os.path.join(
-            SAMPLE_DATA, 'Base_Data', 'Marine', 'Land', 'global_polygon.shp')
-        args['min_distance'] = 0
-        args['max_distance'] = 200000
-
-        wind_energy.execute(args)
-
-        raster_results = [
-            'density_W_per_m2.tif', 'harvested_energy_MWhr_per_yr.tif']
-
-        for raster_path in raster_results:
-            natcap.invest.pygeoprocessing_0_3_3.testing.assert_rasters_equal(
-                os.path.join(args['workspace_dir'], 'output', raster_path),
-                os.path.join(REGRESSION_DATA, 'novaluation', raster_path))
-
-        vector_results = [
-            'example_size_and_orientation_of_a_possible_wind_farm.shp',
-            'wind_energy_points.shp']
-
-        for vector_path in vector_results:
-            natcap.invest.pygeoprocessing_0_3_3.testing.assert_vectors_equal(
-                os.path.join(args['workspace_dir'], 'output', vector_path),
-                os.path.join(REGRESSION_DATA, 'novaluation', vector_path))
-
     # @scm.skip_if_data_missing(SAMPLE_DATA)
     # @scm.skip_if_data_missing(REGRESSION_DATA)
     # def test_val_gridpts_windsched(self):
