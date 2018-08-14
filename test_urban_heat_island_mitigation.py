fbeed47b8c733f60999f0b6ac592160c9d161b6c
Rich Sharp <richpsharp@gmail.com>
2018-08-14 13:56:55 -0700

    re #BITBUCKET-3769 tracking a tracer script for development

---
+++ test_urban_heat_island_mitigation.py
@@ -0,0 +1,34 @@
+"""Tracer script to help with development."""
+import logging
+import sys
+
+import natcap.invest.urban_heat_island_mitigation
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(pathname)s.%(funcName)s:%(lineno)d] %(message)s'),
+    stream=sys.stdout,
+    )
+LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    """Entry point."""
+    args = {
+        'workspace_dir': 'urban_heat_island_invest_workspace',
+        'results_suffix': '',
+        'aoi_watersheds_path': r"C:\Users\rpsharp\Dropbox\Urban InVEST\Stormwater data SF\Draft_Watersheds_SFEI\Draft_Watersheds_SFEI.shp",
+        'rainfall_depth': 257.0,
+        'lulc_path': r"C:\Users\rpsharp\Dropbox\Urban InVEST\Stormwater data SF\LULC_SFBA.tif",
+        'soils_hydrological_group_raster_path': r"C:\Users\rpsharp\Dropbox\Urban InVEST\Stormwater data SF\SoilHydroGroup_SFBA_reproj_0123.tif",
+        'curve_number_table_path': r"C:\Users\rpsharp\Dropbox\Urban InVEST\Stormwater data SF\Biophysical_water_SF.csv",
+        'built_infrastructure_vector_path': r"C:\Users\rpsharp\Dropbox\Urban InVEST\Stormwater data SF\Built_infra_censusblock.shp",
+        'infrastructure_damage_loss_table_path': r"C:\Users\rpsharp\Dropbox\Urban InVEST\Stormwater data SF\Damage.csv",
+        }
+    natcap.invest.urban_heat_island_mitigation.execute(args)
+
+
+if __name__ == '__main__':
+    main()
