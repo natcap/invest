ecc5d396752dafa38cacfa5bffe3f00e67ad9399
Joanna Lin <chiayilin94@gmail.com>
2018-08-16 14:28:05 -0700

    replace pygeoprocessing 0.3.3 imports with 1.0. see #BITBUCKET-3768.

--- tests/test_hra.py
+++ tests/test_hra.py
@@ -4,7 +4,8 @@ import tempfile
 import shutil
 import os
 
-from natcap.invest.pygeoprocessing_0_3_3.testing import scm
+import pygeoprocessing
+from pygeoprocessing.testing import scm
 
 SAMPLE_DATA = os.path.join(
     os.path.dirname(__file__), '..', 'data', 'invest-test-data',
@@ -50,7 +51,7 @@ class HRATests(unittest.TestCase):
         HRATests._test_same_files(
             os.path.join(REGRESSION_DATA, 'expected_file_list_euc_none.txt'),
             args['workspace_dir'])
-        natcap.invest.pygeoprocessing_0_3_3.testing.assert_rasters_equal(
+        pygeoprocessing.testing.assert_rasters_equal(
             os.path.join(REGRESSION_DATA, 'ecosys_risk_euc_none.tif'),
             os.path.join(
                 self.workspace_dir, 'output', 'Maps', 'ecosys_risk.tif'),
@@ -79,7 +80,7 @@ class HRATests(unittest.TestCase):
         HRATests._test_same_files(
             os.path.join(REGRESSION_DATA, 'expected_file_list_mult_none.txt'),
             args['workspace_dir'])
-        natcap.invest.pygeoprocessing_0_3_3.testing.assert_rasters_equal(
+        pygeoprocessing.testing.assert_rasters_equal(
             os.path.join(REGRESSION_DATA, 'ecosys_risk_mult_none.tif'),
             os.path.join(
                 self.workspace_dir, 'output', 'Maps', 'ecosys_risk.tif'),
@@ -108,10 +109,10 @@ class HRATests(unittest.TestCase):
         HRATests._test_same_files(
             os.path.join(REGRESSION_DATA, 'expected_file_list_euc_lin.txt'),
             args['workspace_dir'])
-        natcap.invest.pygeoprocessing_0_3_3.testing.assert_rasters_equal(
+        pygeoprocessing.testing.assert_rasters_equal(
             os.path.join(REGRESSION_DATA, 'ecosys_risk_euc_lin.tif'),
             os.path.join(
-                self.workspace_dir, 'output', 'Maps', 'ecosys_risk.tif'))
+                self.workspace_dir, 'output', 'Maps', 'ecosys_risk.tif'), 1e-6)
 
     @scm.skip_if_data_missing(SAMPLE_DATA)
     @scm.skip_if_data_missing(REGRESSION_DATA)
@@ -136,7 +137,7 @@ class HRATests(unittest.TestCase):
         HRATests._test_same_files(
             os.path.join(REGRESSION_DATA, 'expected_file_list_euc_exp.txt'),
             args['workspace_dir'])
-        natcap.invest.pygeoprocessing_0_3_3.testing.assert_rasters_equal(
+        pygeoprocessing.testing.assert_rasters_equal(
             os.path.join(REGRESSION_DATA, 'ecosys_risk_euc_exp.tif'),
             os.path.join(
                 self.workspace_dir, 'output', 'Maps', 'ecosys_risk.tif'),
