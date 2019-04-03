074f267389c25d733e89c36bfab8243359bafe03
Rich Sharp <richpsharp@gmail.com>
2019-04-03 13:22:41 -0700

    re #BITBUCKET-3788 removing old code and deleting the to-process flow raster

--- src/natcap/invest/ndr/ndr_core.pyx
+++ src/natcap/invest/ndr/ndr_core.pyx
@@ -387,7 +387,7 @@ def ndr_eff_calculation(
 
     cdef int n_cols, n_rows
     flow_dir_info = pygeoprocessing.get_raster_info(mfd_flow_direction_path)
-    n_rows, n_cols = flow_dir_info['raster_size']
+    n_cols, n_rows = flow_dir_info['raster_size']
 
     cdef stack[int] processing_stack
     stream_info = pygeoprocessing.get_raster_info(stream_path)
@@ -483,15 +483,18 @@ def ndr_eff_calculation(
                 global_col, global_row)
             flow_dir = <int>mfd_flow_direction_raster.get(
                     global_col, global_row)
-            if stream_raster.get(global_col, global_row) == 1 or (
-                    is_close(crit_len_nodata, crit_len) or
-                    is_close(retention_eff_lulc, retention_eff_nodata) or
-                    flow_dir == 0):
+            if stream_raster.get(global_col, global_row) == 1:
+                # if it's a stream effective retention is 0.
                 effective_retention_raster.set(global_col, global_row, 0)
+            elif (is_close(crit_len, crit_len_nodata) or
+                  is_close(retention_eff_lulc, retention_eff_nodata) or
+                  flow_dir == 0):
+                # if it's nodata, effective retention is nodata.
+                effective_retention_raster.set(
+                    global_col, global_row, effective_retention_nodata)
             else:
                 working_retention_eff = 0.0
                 outflow_weight_sum = 0
-                LOGGER.info('start loop')
                 for i in range(8):
                     outflow_weight = (flow_dir >> (i*4)) & 0xF
                     if outflow_weight == 0:
@@ -499,11 +502,9 @@ def ndr_eff_calculation(
                     outflow_weight_sum += outflow_weight
                     ds_col = col_offsets[i] + global_col
                     if ds_col < 0 or ds_col >= n_cols:
-                        LOGGER.info('skip because ds_col < 0 or >= n_cols %s %s', ds_col, n_cols)
                         continue
                     ds_row = row_offsets[i] + global_row
                     if ds_row < 0 or ds_row >= n_rows:
-                        LOGGER.info('skip because ds_row < 0 or >= n_rows %s %s', ds_row, n_rows)
                         continue
                     if i % 2 == 1:
                         step_size = cell_size*1.41421356237
@@ -526,13 +527,9 @@ def ndr_eff_calculation(
                             intermediate_retention = retention_eff_lulc
                         working_retention_eff += (
                             intermediate_retention * outflow_weight)
-                    LOGGER.debug('working_retention_eff %s %s', working_retention_eff, i)
-                LOGGER.info('end loop %s %s %s', outflow_weight_sum, global_col, global_row)
                 if outflow_weight_sum > 0:
                     old_eff = working_retention_eff
                     working_retention_eff /= float(outflow_weight_sum)
-                    if working_retention_eff == 0:
-                        LOGGER.error('setting %s %s %s %s %s %s %s %s', retention_eff_lulc, intermediate_retention, outflow_weight, outflow_weight_sum, old_eff, retention_eff_lulc, neighbor_effective_retention, current_step_factor)
                     effective_retention_raster.set(
                         global_col, global_row, working_retention_eff)
                 else:
@@ -566,330 +563,5 @@ def ndr_eff_calculation(
                     # push on stack, otherwise another downstream pixel will
                     # pick it up
                     processing_stack.push(neighbor_row*n_cols + neighbor_col)
-
     to_process_flow_directions_raster.close()
-    #os.remove(to_process_flow_directions_path)
-    return
-    ####################
-    """
-    outflow_weights_path = natcap.invest.pygeoprocessing_0_3_3.temporary_filename()
-    outflow_direction_path = natcap.invest.pygeoprocessing_0_3_3.temporary_filename()
-    natcap.invest.pygeoprocessing_0_3_3.routing.routing_core.calculate_flow_weights(
-        flow_direction_path, outflow_weights_path, outflow_direction_path)
-    outflow_weights_ds = gdal.OpenEx(outflow_weights_path)
-    outflow_weights_band = outflow_weights_ds.GetRasterBand(1)
-    cdef float outflow_weights_nodata = natcap.invest.pygeoprocessing_0_3_3.get_nodata_from_uri(
-        outflow_weights_path)
-    outflow_direction_ds = gdal.OpenEx(outflow_direction_path)
-    outflow_direction_band = outflow_direction_ds.GetRasterBand(1)
-    cdef int outflow_direction_nodata = natcap.invest.pygeoprocessing_0_3_3.get_nodata_from_uri(
-        outflow_direction_path)
-    cdef int block_col_size, block_row_size
-    block_col_size, block_row_size = stream_band.GetBlockSize()
-    cdef int n_global_block_rows = int(ceil(float(n_rows) / block_row_size))
-    cdef int n_global_block_cols = int(ceil(float(n_cols) / block_col_size))
-
-    cdef numpy.ndarray[numpy.npy_float32, ndim=4] stream_block = numpy.zeros(
-        (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-        dtype=numpy.float32)
-    cdef numpy.ndarray[numpy.npy_int8, ndim=4] outflow_direction_block = (
-        numpy.zeros(
-            (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-            dtype=numpy.int8))
-    cdef numpy.ndarray[numpy.npy_float32, ndim=4] outflow_weights_block = (
-        numpy.zeros(
-            (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-            dtype=numpy.float32))
-    cdef numpy.ndarray[numpy.npy_float32, ndim=4] effective_retention_block = (
-        numpy.zeros(
-            (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-            dtype=numpy.float32))
-    cdef numpy.ndarray[numpy.npy_float32, ndim=4] retention_eff_lulc_block = (
-        numpy.zeros(
-            (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-            dtype=numpy.float32))
-    cdef numpy.ndarray[numpy.npy_float32, ndim=4] crit_len_block = numpy.zeros(
-        (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-        dtype=numpy.float32)
-
-
-    cdef numpy.ndarray[numpy.npy_int8, ndim=4] processed_cell_block = (
-        numpy.zeros(
-            (N_BLOCK_ROWS, N_BLOCK_COLS, block_row_size, block_col_size),
-            dtype=numpy.int8))
-
-    band_list = [stream_band, outflow_direction_band, outflow_weights_band,
-                 effective_retention_band, processed_cell_band,
-                 retention_eff_lulc_band, crit_len_band]
-    block_list = [stream_block, outflow_direction_block, outflow_weights_block,
-                  effective_retention_block, processed_cell_block,
-                  retention_eff_lulc_block, crit_len_block]
-    update_list = [False, False, False, True, True, False, False]
-
-    cdef numpy.ndarray[numpy.npy_byte, ndim=2] cache_dirty = (
-        numpy.zeros((N_BLOCK_ROWS, N_BLOCK_COLS), dtype=numpy.byte))
-
-    cdef BlockCache block_cache = BlockCache(
-        N_BLOCK_ROWS, N_BLOCK_COLS, n_rows, n_cols, block_row_size,
-        block_col_size, band_list, block_list, update_list, cache_dirty)
-
-    #center point of global index
-    cdef int global_row, global_col
-    cdef int row_index, col_index
-    cdef int row_block_offset, col_block_offset
-    cdef int global_block_row, global_block_col
-
-    #neighbor sections of global index
-    cdef int neighbor_row, neighbor_col
-    cdef int neighbor_row_index, neighbor_col_index
-    cdef int neighbor_row_block_offset, neighbor_col_block_offset
-    cdef int flat_index
-
-
-    cdef c_set[int] cells_in_queue
-
-    #build up the stream pixel indexes as starting seed points for the search
-    cdef time_t last_time, current_time
-    time(&last_time)
-    for global_block_row in xrange(n_global_block_rows):
-        time(&current_time)
-        if current_time - last_time > 5.0:
-            LOGGER.info(
-                "find_sinks %.1f%% complete",
-                (global_block_row + 1.0) / n_global_block_rows * 100)
-            last_time = current_time
-        for global_block_col in xrange(n_global_block_cols):
-            for global_row in xrange(
-                    global_block_row*block_row_size,
-                    min((global_block_row+1)*block_row_size, n_rows)):
-                for global_col in xrange(
-                        global_block_col*block_col_size,
-                        min((global_block_col+1)*block_col_size, n_cols)):
-                    block_cache.update_cache(
-                        global_row, global_col, &row_index, &col_index,
-                        &row_block_offset, &col_block_offset)
-                    if stream_block[
-                            row_index, col_index, row_block_offset,
-                            col_block_offset] == 1:
-                        flat_index = global_row * n_cols + global_col
-                        visit_stack.push_front(global_row * n_cols + global_col)
-                        cells_in_queue.insert(flat_index)
-
-                        effective_retention_block[row_index, col_index,
-                            row_block_offset, col_block_offset] = 0
-                        processed_cell_block[row_index, col_index,
-                            row_block_offset, col_block_offset] = 1
-                        cache_dirty[row_index, col_index] = 1
-
-    cdef int neighbor_outflow_direction, neighbor_index, outflow_direction
-    cdef float neighbor_outflow_weight, current_effective_retention
-    cdef float outflow_weight, neighbor_effective_retention, step_size
-    cdef float downstream_effective_retention, current_stream
-    cdef float original_effective_retention
-    cdef float retention_eff_lulc, crit_len, intermediate_retention
-    cdef float current_step_factor
-    cdef int it_flows_here
-
-    while visit_stack.size() > 0:
-        flat_index = visit_stack.front()
-        visit_stack.pop_front()
-        cells_in_queue.erase(flat_index)
-        global_row = flat_index / n_cols
-        global_col = flat_index % n_cols
-
-        block_cache.update_cache(
-            global_row, global_col, &row_index, &col_index,
-            &row_block_offset, &col_block_offset)
-
-        update_downstream = False
-        current_effective_retention = 0.0
-
-        time(&current_time)
-        if current_time - last_time > 0.0001:
-            last_time = current_time
-            LOGGER.info(
-                'visit_stack on stream effective_retention size: %d ', visit_stack.size())
-
-        current_stream = stream_block[
-            row_index, col_index, row_block_offset, col_block_offset]
-        outflow_direction = outflow_direction_block[
-            row_index, col_index, row_block_offset,
-            col_block_offset]
-        if current_stream == 1:
-            effective_retention_block[row_index, col_index,
-                row_block_offset, col_block_offset] = 0
-            processed_cell_block[row_index, col_index,
-                row_block_offset, col_block_offset] = 1
-            cache_dirty[row_index, col_index] = 1
-        elif outflow_direction == outflow_direction_nodata:
-            current_effective_retention = 1.0
-        elif processed_cell_block[row_index, col_index, row_block_offset,
-                col_block_offset] == 0:
-            #add downstream effective_retention to current effective_retention
-
-            outflow_weight = outflow_weights_block[
-                row_index, col_index, row_block_offset,
-                col_block_offset]
-
-            retention_eff_lulc = retention_eff_lulc_block[
-                row_index, col_index, row_block_offset,
-                col_block_offset]
-
-            crit_len = crit_len_block[
-                row_index, col_index, row_block_offset,
-                col_block_offset]
-
-            for neighbor_index in xrange(2):
-                #check if downstream neighbors are calcualted
-                if neighbor_index == 1:
-                    outflow_direction = (outflow_direction + 1) % 8
-                    outflow_weight = (1.0 - outflow_weight)
-
-                if outflow_weight <= 0.0:
-                    continue
-
-                neighbor_row = global_row + row_offsets[outflow_direction]
-                neighbor_col = global_col + col_offsets[outflow_direction]
-                if (neighbor_row < 0 or neighbor_row >= n_rows or
-                        neighbor_col < 0 or neighbor_col >= n_cols):
-                    #out of bounds
-                    continue
-
-                block_cache.update_cache(
-                    neighbor_row, neighbor_col, &neighbor_row_index,
-                    &neighbor_col_index, &neighbor_row_block_offset,
-                    &neighbor_col_block_offset)
-
-                if stream_block[neighbor_row_index,
-                        neighbor_col_index, neighbor_row_block_offset,
-                        neighbor_col_block_offset] == stream_nodata:
-                    #out of the valid raster entirely
-                    continue
-
-                neighbor_effective_retention = effective_retention_block[
-                    neighbor_row_index, neighbor_col_index,
-                    neighbor_row_block_offset, neighbor_col_block_offset]
-
-                neighbor_outflow_direction = outflow_direction_block[
-                    neighbor_row_index, neighbor_col_index,
-                    neighbor_row_block_offset, neighbor_col_block_offset]
-
-                neighbor_outflow_weight = outflow_weights_block[
-                    neighbor_row_index, neighbor_col_index,
-                    neighbor_row_block_offset, neighbor_col_block_offset]
-
-                if processed_cell_block[neighbor_row_index, neighbor_col_index,
-                        neighbor_row_block_offset,
-                        neighbor_col_block_offset] == 0:
-                    neighbor_flat_index = neighbor_row * n_cols + neighbor_col
-                    #insert into the processing queue if it's not already there
-                    if (cells_in_queue.find(flat_index) ==
-                            cells_in_queue.end()):
-                        visit_stack.push_back(flat_index)
-                        cells_in_queue.insert(flat_index)
-
-                    if (cells_in_queue.find(neighbor_flat_index) ==
-                            cells_in_queue.end()):
-                        visit_stack.push_front(neighbor_flat_index)
-                        cells_in_queue.insert(neighbor_flat_index)
-
-                    update_downstream = True
-                    neighbor_effective_retention = 0.0
-
-                if outflow_direction % 2 == 1:
-                    #increase effective_retention by a square root of 2 for diagonal
-                    step_size = cell_size * 1.41421356237
-                else:
-                    step_size = cell_size
-
-                current_step_factor = exp(-5 * step_size / crit_len)
-                if neighbor_effective_retention >= retention_eff_lulc:
-                    current_effective_retention += (
-                        neighbor_effective_retention) * outflow_weight
-                else:
-                    intermediate_retention =  (
-                        neighbor_effective_retention * current_step_factor +
-                        retention_eff_lulc * (1 - current_step_factor))
-                    if intermediate_retention > retention_eff_lulc:
-                        intermediate_retention = retention_eff_lulc
-                    current_effective_retention += (
-                        intermediate_retention * outflow_weight)
-
-        if not update_downstream:
-            #mark flat_index as processed
-            block_cache.update_cache(
-                global_row, global_col, &row_index, &col_index,
-                &row_block_offset, &col_block_offset)
-            processed_cell_block[row_index, col_index,
-                row_block_offset, col_block_offset] = 1
-            effective_retention_block[row_index, col_index,
-                row_block_offset, col_block_offset] = (
-                    current_effective_retention)
-            cache_dirty[row_index, col_index] = 1
-
-            #update any upstream neighbors with this effective_retention
-            for neighbor_index in range(8):
-                neighbor_row = global_row + row_offsets[neighbor_index]
-                neighbor_col = global_col + col_offsets[neighbor_index]
-                if (neighbor_row < 0 or neighbor_row >= n_rows or
-                        neighbor_col < 0 or neighbor_col >= n_cols):
-                    #out of bounds
-                    continue
-
-                block_cache.update_cache(
-                    neighbor_row, neighbor_col, &neighbor_row_index,
-                    &neighbor_col_index, &neighbor_row_block_offset,
-                    &neighbor_col_block_offset)
-
-                #streams were already added, skip if they are in the queue
-                if (stream_block[neighbor_row_index, neighbor_col_index,
-                        neighbor_row_block_offset,
-                        neighbor_col_block_offset] == 1 or
-                    stream_block[neighbor_row_index, neighbor_col_index,
-                        neighbor_row_block_offset,
-                        neighbor_col_block_offset] == stream_nodata):
-                    continue
-
-                if processed_cell_block[
-                        neighbor_row_index,
-                        neighbor_col_index,
-                        neighbor_row_block_offset,
-                        neighbor_col_block_offset] == 1:
-                    #don't reprocess it, it's already been updated by two valid
-                    #children
-                    continue
-
-                neighbor_outflow_direction = outflow_direction_block[
-                    neighbor_row_index, neighbor_col_index,
-                    neighbor_row_block_offset, neighbor_col_block_offset]
-                if neighbor_outflow_direction == outflow_direction_nodata:
-                    #if the neighbor has no flow, we can't flow here
-                    continue
-
-                neighbor_outflow_weight = outflow_weights_block[
-                    neighbor_row_index, neighbor_col_index,
-                    neighbor_row_block_offset, neighbor_col_block_offset]
-
-                it_flows_here = False
-                if (neighbor_outflow_direction ==
-                        inflow_offsets[neighbor_index]):
-                    it_flows_here = True
-                elif ((neighbor_outflow_direction + 1) % 8 ==
-                        inflow_offsets[neighbor_index]):
-                    it_flows_here = True
-                    neighbor_outflow_weight = 1.0 - neighbor_outflow_weight
-
-                neighbor_flat_index = neighbor_row * n_cols + neighbor_col
-                if (it_flows_here and neighbor_outflow_weight > 0.0 and
-                    cells_in_queue.find(neighbor_flat_index) ==
-                        cells_in_queue.end()):
-                    visit_stack.push_back(neighbor_flat_index)
-                    cells_in_queue.insert(neighbor_flat_index)
-
-    block_cache.flush_cache()
-
-    for dataset in [outflow_weights_ds, outflow_direction_ds]:
-        gdal.Dataset.__swig_destroy__(dataset)
-    for dataset_uri in [outflow_weights_path, outflow_direction_path]:
-        os.remove(dataset_uri)
-    """
+    os.remove(to_process_flow_directions_path)
