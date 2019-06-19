42ea6eaad7b7c9405c1ff90ace208ca77a8826c2
David Fisher <davemfish@gmail.com>
2019-06-19 14:12:16 -0700

    mandating that workspace_dir is not in sample parametersets, and testing that functionality. #BITBUCKET-3886.

--- scripts/invest-autovalidate.py
+++ scripts/invest-autovalidate.py
@@ -22,11 +22,6 @@ class ValidateExceptionTests(unittest.TestCase):
     def setUp(self):
         """Overriding setUp function to create temp file."""
         self.workspace = tempfile.mkdtemp()
-        datastack_path = os.path.join(
-            self.workspace, 'dummy.invs.json')
-        with open(datastack_path, 'wb') as file:
-            file.write('"args": {"something":"else"},')
-            file.write('"model_name": natcap.invest.carbon')
 
     def tearDown(self):
         """Overriding tearDown function to remove temporary dir."""
@@ -34,17 +29,51 @@ class ValidateExceptionTests(unittest.TestCase):
 
     def test_exception_on_invalid_data(self):
         """Test ValueError is raised on invalid datastack."""
+        datastack_path = os.path.join(
+            self.workspace, 'dummy.invs.json')
+        with open(datastack_path, 'wb') as file:
+            file.write('"args": {"something": "else"},')
+            file.write('"model_name": natcap.invest.carbon')
+        with self.assertRaises(ValueError):
+            main(self.workspace)
+
+    def test_exception_on_workspace_dir(self):
+        """Test ValueError is raised if workspace_dir is defined in datastack."""
+        datastack_path = os.path.join(
+            self.workspace, 'dummy.invs.json')
+        with open(datastack_path, 'wb') as file:
+            file.write('"args": {"workspace_dir": "/home/foo"},')
+            file.write('"model_name": natcap.invest.carbon')
         with self.assertRaises(ValueError):
             main(self.workspace)
 
 
 def main(sampledatadir):
-    """Do validation for each datastack and store error messages."""
+    """Do validation for each datastack and store error messages.
+    
+    Parameters:
+        sampledatadir (string): path to the invest-sample-data repository,
+            where '*invs.json' datastack files are expected to be in the root.
+
+    Returns:
+        None
+
+    Raises:
+        ValueError if any module's `validate` function issued warnings.
+    """
     validation_messages = ''
     for datastack_path in glob.glob(os.path.join(sampledatadir, '*invs.json')):
 
         paramset = datastack.extract_parameter_set(datastack_path)
-        paramset.args['workspace_dir'] = tempfile.mkdtemp()  # missing from some sample datastacks
+        if 'workspace_dir' in paramset.args and \
+                paramset.args['workspace_dir'] != '':
+            msg = (
+                '%s : workspace_dir should not be defined '
+                'for sample datastacks' % datastack_path)
+            validation_messages += os.linesep + msg
+            LOGGER.error(msg)
+        else:
+            paramset.args['workspace_dir'] = tempfile.mkdtemp()
         model_module = importlib.import_module(name=paramset.model_name)
 
         try:
@@ -61,6 +90,8 @@ def main(sampledatadir):
                 validation_messages += (
                     os.linesep + datastack_path + ': ' +
                     pprint.pformat(model_warnings))
+            if os.path.exists(paramset.args['workspace_dir']):
+                os.rmdir(paramset.args['workspace_dir'])
 
     if validation_messages:
         raise ValueError(validation_messages)
