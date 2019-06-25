a4c5cf7ca2f19e02b9b66a89553440aeefedfc81
James Douglass <jdouglass@stanford.edu>
2019-06-25 13:19:55 -0700

    Removing deprecated test function.  RE:#BITBUCKET-3809

--- tests/test_delineateitpy.py
+++ tests/test_delineateitpy.py
@@ -137,33 +137,3 @@ class DelineateItTests(unittest.TestCase):
 
             self.assertTrue(shapely_feature.equals(expected_geom))
             self.assertEqual(expected_fields, feature.items())
-
-    @staticmethod
-    def _test_same_files(base_list_path, directory_path):
-        """Assert files in `base_list_path` are in `directory_path`.
-
-        Parameters:
-            base_list_path (string): a path to a file that has one relative
-                file path per line.
-            directory_path (string): a path to a directory whose contents will
-                be checked against the files listed in `base_list_file`
-
-        Returns:
-            None
-
-        Raises:
-            AssertionError when there are files listed in `base_list_file`
-                that don't exist in the directory indicated by `path`
-        """
-        missing_files = []
-        with open(base_list_path, 'r') as file_list:
-            for file_path in file_list:
-                full_path = os.path.join(directory_path, file_path.rstrip())
-                if full_path == '':
-                    continue
-                if not os.path.isfile(full_path):
-                    missing_files.append(full_path)
-        if len(missing_files) > 0:
-            raise AssertionError(
-                "The following files were expected but not found: " +
-                '\n'.join(missing_files))
