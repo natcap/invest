fb4a765d012c73838cb33fb662b5ef15adc3d7cb
Dave Fisher <4071255+davemfish@users.noreply.github.com>
2019-08-16 13:18:25 -0700

    initial use of bootstrap tabs.

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {InvestJob} from './components';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+
+function ControlledTabs() {
+  const [tabkey, setTab] = useState('setup');
+
+  return (
+    <Tabs id="controlled-tab-example" activeKey={tabkey} onSelect={k => setTab(k)}>
+      <Tab eventKey="setup" title="Setup">
+        <InvestJob />
+      </Tab>
+      <Tab eventKey="log" title="Log">
+        <InvestJob />
+      </Tab>
+      <Tab eventKey="viz" title="Viz" disabled>
+        <InvestJob />
+      </Tab>
+    </Tabs>
+  );
+}
 
 export default class App extends React.Component {
   render() {
   	return (
-  	  <div>
-  	   	<InvestJob />
-    	</div>
+			<ControlledTabs />
     );
 	};
 }
