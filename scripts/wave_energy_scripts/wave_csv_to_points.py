4c10b6bea69aba08aa21aa3f68cf557ca5015dad
James Douglass <jdouglass@stanford.edu>
2019-09-16 12:13:20 -0700

    Updates to various scripts for python3 compatibility.  RE:#BITBUCKET-3905

--- scripts/wave_energy_scripts/wave_csv_to_points.py
+++ scripts/wave_energy_scripts/wave_csv_to_points.py
@@ -1,38 +1,40 @@
 """Python module that creates a point shapefile from a CSV
 
     Example from command line:
-    >> python wave_csv_to_points.py wave_formatted_csv_data.txt my_layer shape_out.shp 
+    >> python wave_csv_to_points.py wave_formatted_csv_data.txt my_layer shape_out.shp
 
     Example two:
-    >> python wave_csv_to_points.py wave_formatted_csv_data.csv WCVI WCVI_points.shp 
+    >> python wave_csv_to_points.py wave_formatted_csv_data.csv WCVI WCVI_points.shp
 """
 
-# Imports
 import csv
-from osgeo import ogr
-from osgeo import osr
 import os
 import sys
+import shutil
+
+from osgeo import ogr
+from osgeo import osr
+
 
 def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
-    """Creates a point shapefile from a wave energy data csv file that is 
+    """Creates a point shapefile from a wave energy data csv file that is
         properly formatted. The point shapefile created is not projected
         and uses latitude and longitude for its geometry.
-        
+
         wave_data_csv_uri - a URI to a comma separated file of wave point data
             that has been properly formatted (required) Example format:
             ID,I,J,LONG,LATI,HSAVG,TPAVG
             1,102,370,24.3,54.3,10.2,11.1
             2,102,370,24.3,54.3,10.2,11.1
-        
+
         layer_name - a string for the name of the point shapefile
             layer (required)
-        
+
         output_uri - a URI for the output path of the point shapefile (required)
 
         return - Nothing"""
-   
-    # Initiate a dictionary to build up data from the csv file 
+
+    # Initiate a dictionary to build up data from the csv file
     dict_data = {}
 
     # Open the csv file
@@ -41,7 +43,7 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
     # Get a handle on the csv file by using dictReader which handles each line
     # as a dictionary where the column headers are the keys
     reader = csv.DictReader(point_file)
-    
+
     # A list of column headers that we want to remain integers all other column
     # headers will become floats
     int_list = ['ID', 'I', 'J']
@@ -49,7 +51,7 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
     # Iterate over the file by line
     for row in reader:
         # For each line's dictionary, iterate over the key-value pairs
-        for k,v in row.iteritems():
+        for k,v in row.items():
             # If the key represents a value that should be an integer, cast to
             # an int, else cast to a float
             if k in int_list:
@@ -58,7 +60,7 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
                 row[k] = float(v)
         # Build up the new dictionary
         dict_data[row['ID']] = row
-    
+
     # If the output_uri exists delete it
     if os.path.isfile(output_uri):
         os.remove(output_uri)
@@ -73,19 +75,19 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
     # Set the spatial reference to WGS84 (lat/long)
     source_sr = osr.SpatialReference()
     source_sr.SetWellKnownGeogCS("WGS84")
-  
+
     # Create the new point shapefile layer
     output_layer = output_datasource.CreateLayer(
             layer_name, source_sr, ogr.wkbPoint)
 
-    # Get the keys of the dictionary which are the 'ID' values 
+    # Get the keys of the dictionary which are the 'ID' values
     outer_keys = dict_data.keys()
-    
+
     # Using the list of keys from above, get the first keys sub dictionary and
     # get it's keys. These 'inner' keys are the column headers from the file
     # and will be added to the point shapefile as fields
     field_list = dict_data[outer_keys[0]].keys()
-    
+
     # Create a dictionary to store what variable types the fields are
     type_dict = {}
 
@@ -107,14 +109,14 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
             field_type = ogr.OFTString
         else:
             field_type = ogr.OFTReal
-       
+
         # Create a new field in the point shapefile
-        output_field = ogr.FieldDefn(field, field_type)   
+        output_field = ogr.FieldDefn(field, field_type)
         output_layer.CreateField(output_field)
 
     # For each inner dictionary (for each point) create a point and set its
     # fields
-    for point_dict in dict_data.itervalues():
+    for point_dict in dict_data.values():
         # Get latitude / longitude values
         latitude = float(point_dict['LATI'])
         longitude = float(point_dict['LONG'])
@@ -125,12 +127,12 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
 
         # Create a new point feature
         output_feature = ogr.Feature(output_layer.GetLayerDefn())
-        
+
         for field_name in point_dict:
             field_index = output_feature.GetFieldIndex(field_name)
             # Set the value for each field for this particular point
             output_feature.SetField(field_index, point_dict[field_name])
-       
+
         # Set geometry and create / set the feature
         output_feature.SetGeometryDirectly(geom)
         output_layer.CreateFeature(output_feature)
@@ -138,12 +140,16 @@ def create_wave_point_ds(wave_data_csv_uri, layer_name, output_uri):
 
     output_layer.SyncToDisk()
 
-# Argument 1 from the command line, the wave energy csv data
-wave_data_csv_uri = sys.argv[1]
-# Argument 2 from the command line, a string for the layer name
-layer_name = sys.argv[2]
-# Argument 3 from the command line, the output URI for the point shapefile
-out_uri = sys.argv[3]
-# Call the function to create our point shapefile from CSV
-create_wave_point_ds(wave_data_csv_uri, layer_name, out_uri)
 
+if __name__ == '__main__':
+    # Argument 1 from the command line, the wave energy csv data
+    wave_data_csv_uri = sys.argv[1]
+
+    # Argument 2 from the command line, a string for the layer name
+    layer_name = sys.argv[2]
+
+    # Argument 3 from the command line, the output URI for the point shapefile
+    out_uri = sys.argv[3]
+
+    # Call the function to create our point shapefile from CSV
+    create_wave_point_ds(wave_data_csv_uri, layer_name, out_uri)
