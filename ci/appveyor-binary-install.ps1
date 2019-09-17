40aa3516e433fb1ebaa26275b142f890b87a4227
James Douglass <jdouglass@stanford.edu>
2019-09-17 12:54:21 -0700

    Using cmd.exe for some of the installation executables.  RE:#BITBUCKET-3905

--- ci/appveyor-binary-install.ps1
+++ ci/appveyor-binary-install.ps1
@@ -4,20 +4,17 @@
 # and on the PATH.  It also requires that the environment variable
 #
 
-choco install make wget vcredist140 pandoc
-
-# Our Makefile depends on zip rather than 7zip, so we need to get those binaries.
-choco install zip
+choco install make wget vcredist140 pandoc zip
 
 # The binary build requires the shapely DLL to be named something specific.
 # /B copies the file as a binary file.
 cmd.exe --% /c copy /B %PYTHON%\Lib\site-packages\shapely\DLLs\geos_c.dll %PYTHON%\Lib\site-packages\shapely\DLLs\geos.dll
 
 # Download and install NSIS plugins to their correct places.
-wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/Inetc.zip
-wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/Nsisunz.zip
-wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/NsProcess.zip
-7z e NsProcess.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" Plugin\nsProcess.dll
-7z e NsProcess.zip -o"C:\Program Files (x86)\NSIS\Include" Include\nsProcess.nsh
-7z e Inetc.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" Plugins\x86-ansi\INetC.dll
-7z e Nsisunz.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" nsisunz\Release\nsisunz.dll
+cmd.exe --% /c wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/Inetc.zip
+cmd.exe --% /c wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/Nsisunz.zip
+cmd.exe --% /c wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/NsProcess.zip
+cmd.exe --% /c 7z e NsProcess.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" Plugin\nsProcess.dll
+cmd.exe --% /c 7z e NsProcess.zip -o"C:\Program Files (x86)\NSIS\Include" Include\nsProcess.nsh
+cmd.exe --% /c 7z e Inetc.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" Plugins\x86-ansi\INetC.dll
+cmd.exe --% /c 7z e Nsisunz.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" nsisunz\Release\nsisunz.dll
