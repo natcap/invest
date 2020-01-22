675e83eef2b39fb8af943a8eed4e7eec4b0f270e
James Douglass <jdouglass@stanford.edu>
2020-01-21 17:16:19 -0800

    Removing testing that uses the RPC usage logger.  RE:#BITBUCKET-3894

--- ui_tests/test_model_logging.py
+++ ui_tests/test_model_logging.py
@@ -18,11 +18,8 @@ except ImportError:
     from StringIO import StringIO
     from urllib import urlencode
 
-from pygeoprocessing.testing import scm
 import numpy.testing
 
-import Pyro4
-import mock
 
 SAMPLE_DATA = os.path.join(
     os.path.dirname(__file__), '..', 'data', 'invest-sample-data')
@@ -39,89 +36,6 @@ class ModelLoggingTests(unittest.TestCase):
         """Remove the workspace."""
         shutil.rmtree(self.workspace_dir)
 
-    def test_pyro_server(self):
-        """Usage logger test server as an RPC."""
-        from natcap.invest.ui import usage_logger
-        # attempt to get an open port; could result in race condition but
-        # will be okay for a test. if this test ever fails because of port
-        # in use, that's probably why
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('', 0))
-        port = sock.getsockname()[1]
-        sock.close()
-        sock = None
-
-        server_args = {
-            'hostname': 'localhost',
-            'port': port,
-        }
-
-        server_thread = threading.Thread(
-            target=usage_logger.execute, args=(server_args,))
-        server_thread.daemon = True
-        server_thread.start()
-        time.sleep(1)
-
-        logging_server = Pyro4.Proxy(
-            "PYRO:natcap.invest.remote_logging@localhost:%d" % port)
-        # this makes for an easy expected result
-        sample_data = dict(
-            (key_field, key_field) for key_field in
-            usage_logger.LoggingServer._LOG_FIELD_NAMES)
-        logging_server.log_invest_run(sample_data, 'log')
-
-    def test_add_exit_status(self):
-        """Usage logger record run and then exit and verify they are added."""
-        from natcap.invest.ui import usage_logger
-
-        logging_server = usage_logger.LoggingServer()
-
-        # set up a sample dict whose values are identical to its keys
-        # this makes for an easy expected result
-        sample_data = dict(
-            (key_field, key_field) for key_field in
-            usage_logger.LoggingServer._LOG_FIELD_NAMES)
-
-        # This mock needs only to return a valid json string with the expected
-        # key-value pairs.
-        json_string = str('{"START": "http://foo.bar", "FINISH": "http://foo.bar"}')
-        with mock.patch(
-                'natcap.invest.ui.usage_logger.urlopen',
-                return_value=StringIO(json_string)) as mock_obj:
-            logging_server.log_invest_run(sample_data, 'log')
-        self.assertEqual(mock_obj.call_count, 2)
-        sample_data['ip_address'] = 'local'
-        self.assertEqual(
-            sorted(mock_obj.call_args[0][0].data.decode('utf-8').split('&')),
-            sorted(urlencode(sample_data).split('&')))
-
-        exit_sample_data = dict(
-            (key_field, key_field) for key_field in
-            usage_logger.LoggingServer._EXIT_LOG_FIELD_NAMES)
-        with mock.patch(
-                'natcap.invest.ui.usage_logger.urlopen',
-                return_value=StringIO(json_string)) as mock_obj:
-            logging_server.log_invest_run(exit_sample_data, 'exit')
-        self.assertEqual(mock_obj.call_count, 2)
-        exit_sample_data['ip_address'] = 'local'
-        self.assertEqual(
-            sorted(mock_obj.call_args[0][0].data.decode('utf-8').split('&')),
-            sorted(urlencode(exit_sample_data).split('&')))
-
-    def test_unknown_mode(self):
-        """Usage logger test that an unknown mode raises an exception."""
-        from natcap.invest.ui import usage_logger
-
-        logging_server = usage_logger.LoggingServer()
-
-        sample_data = dict(
-            (key_field, key_field) for key_field in
-            usage_logger.LoggingServer._LOG_FIELD_NAMES)
-
-        with self.assertRaises(ValueError):
-            logging_server.log_invest_run(sample_data, 'bad_mode')
-
-    @scm.skip_if_data_missing(SAMPLE_DATA)
     def test_bounding_boxes(self):
         """Usage logger test that we can extract bounding boxes."""
         from natcap.invest import utils
