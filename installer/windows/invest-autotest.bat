5b53132c50834dd03a24860bcbbe793efd97a254
David Fisher <davemfish@gmail.com>
2020-02-07 13:26:08 -0800

    updated invest-autotest to reflect changed location of sample data installation. #BITBUCKET-3943.

--- installer/windows/invest-autotest.bat
+++ installer/windows/invest-autotest.bat
@@ -1,6 +1,6 @@
 @REM Run this script from within the invest-3-x86 directory to run the
 @REM invest-autotest program on windows.
 @REM
-@REM Assumes that python is on the PATH.
+@REM Assumes that 32-bit python is on the PATH.
 
-python "%CD%\invest-autotest.py" --cwd=".." --binary="%CD%\invest.exe
+python "%CD%\invest-autotest.py" --cwd="..\sample_data" --binary="%CD%\invest.exe
