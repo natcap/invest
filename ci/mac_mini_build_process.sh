7f43e68ac44b85c70e0d1631347e037399dd12e6
David Fisher <davemfish@gmail.com>
2020-02-10 16:32:46 -0800

    some comments in the mac build script.  #BITBUCKET-3949.

--- ci/mac_mini_build_process.sh
+++ ci/mac_mini_build_process.sh
@@ -5,9 +5,13 @@
 
 # System Pre-requisites (all should be on the PATH):
 # make
-# python3
+# python3 (with Cython)
 # conda
 # pandoc
+
+# It might be a good idea to `rm -rf` any existing python environment
+# because Make doesn't always re-make an env if there were updates to 
+# requirements files. But I'm afraid to add that `rm` to this script.
 
 # PyInstaller's exe/invest.spec expects the python environment
 # to be named 'env' so it can find binary files that need moving
@@ -24,4 +28,9 @@ source activate ./$PYTHON_ENV
 # overriding this make variable just so it includes the trailing slash,
 # which is key for cp and rsync commands in this bash shell
 make INVEST_BINARIES_DIR=dist/invest/ mac_zipfile
+
+# This script never makes the sample data, which is fine since we don't 
+# distribute sample data with the mac installer. 
+# But it means we will see this message on make deploy (and its okay to ignore):
+# CommandException: arg (dist/data) does not name a directory, bucket, or bucket subdir.
 make deploy
