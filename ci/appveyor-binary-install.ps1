5f76b26e1b1d3976921cfa85b0f61b9c263ed145
James Douglass <jdouglass@stanford.edu>
2020-02-24 15:21:52 -0800

    Adding debug logging, NSIS. RE:#26

--- ci/appveyor-binary-install.ps1
+++ ci/appveyor-binary-install.ps1
@@ -12,12 +12,14 @@ choco install make wget vcredist140 pandoc zip
 
 # The binary build requires the shapely DLL to be named something specific.
 # /B copies the file as a binary file.
+Write-Host "Copying shapely DLL"
 cmd.exe --% /c copy /B %PYTHON%\Lib\site-packages\shapely\DLLs\geos_c.dll %PYTHON%\Lib\site-packages\shapely\DLLs\geos.dll
 
 # Download and install NSIS plugins to their correct places.
-cmd.exe --% /c wget -nv --no-check-certificate https://storage.googleapis.com/natcap-build-dependencies/windows/Inetc.zip
-cmd.exe --% /c wget -nv --no-check-certificate https://storage.googleapis.com/natcap-build-dependencies/windows/Nsisunz.zip
-cmd.exe --% /c wget -nv --no-check-certificate https://storage.googleapis.com/natcap-build-dependencies/windows/NsProcess.zip
+Write-Host "Downloading and extracting NSIS"
+cmd.exe /c "wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/Inetc.zip"
+cmd.exe /c "wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/Nsisunz.zip"
+cmd.exe /c "wget -nv https://storage.googleapis.com/natcap-build-dependencies/windows/NsProcess.zip"
 cmd.exe --% /c 7z e NsProcess.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" Plugin\nsProcess.dll
 cmd.exe --% /c 7z e NsProcess.zip -o"C:\Program Files (x86)\NSIS\Include" Include\nsProcess.nsh
 cmd.exe --% /c 7z e Inetc.zip -o"C:\Program Files (x86)\NSIS\Plugins\x86-ansi" Plugins\x86-ansi\INetC.dll
