07a17ef11c08ff4ef5b894273df18db3219d6b08
davemfish <davemfish@gmail.com>
2020-03-02 12:36:29 -0800

    trying Tail instead of LazyLog. in progress.

--- src/components/LogTab/index.jsx
+++ src/components/LogTab/index.jsx
@@ -9,23 +9,55 @@ import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 
+const logStyle = {
+  whiteSpace: 'pre-line',
+  maxHeight: '700px',
+  overflowY: 'scroll',
+};
 
 export class LogTab extends React.Component {
 
   constructor(props) {
     super(props);
+    this.content = React.createRef();
+    this.state = {
+      logdata: ''
+    }
+  }
+
+  componentDidUpdate() {
+    this.content.current.scrollTop = this.content.current.scrollHeight;
+    if (this.props.logfile) {
+      let tail = new Tail(this.props.logfile, {
+        fromBeginning: true
+      });
+      tail.on('line', (data) => {
+        logdata += `${data}`
+        this.setState({ logdata: logdata })
+      })
+    }
   }
 
   render() {
+    console.log('render log')
     const current_err = this.props.logStdErr;
     // Include the stderr in the main log even though it also gets an Alert
     let renderedLog;
     let renderedAlert;
     let killButton;
+    let logdata;
 
-    if (this.props.logfile) {
-      renderedLog = <LazyLog height={600} stream={true} url={path.resolve(this.props.logfile)} />
-    }
+    // if (this.props.logfile) {
+    //   renderedLog = <LazyLog
+    //     height={600}
+    //     stream={true}
+    //     url={path.resolve(this.props.logfile)}/>
+    // }
+    
+    renderedLog =
+        <Col ref={this.content} style={logStyle}>
+          {this.state.logdata}
+        </Col>
 
     if (current_err) {
       renderedAlert = <Alert variant={'danger'}>{current_err}</Alert>
