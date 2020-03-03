04a2125cbb4a845f28b0e5e0bb202ebe7b76cc03
James Douglass <jdouglass@stanford.edu>
2020-03-03 15:50:16 -0800

    Adding a script to update Makefile to the latest UG rev. #14

---
+++ ci/release/increment-userguide-revision.py
@@ -0,0 +1,37 @@
+import shutil
+import os
+import pprint
+import requests
+
+
+def get_latest_rev_on_userguide():
+    r = requests.get('https://api.github.com/repos/natcap/invest.users-guide/commits/master')
+    return r.json()['sha']
+
+
+def update_userguide_rev_in_makefile(new_rev):
+    if not os.path.exists('build'):
+        os.makedirs('build')
+    new_makefile_path = os.path.join('build', 'Makefile')
+    makefile_path = 'Makefile'
+
+    # The Makefile uses \n newlines, so enforce that newline character in the
+    # new Makefile.
+    with open(new_makefile_path, 'w', newline='\n') as new_makefile:
+        with open(makefile_path) as makefile:
+            for line in makefile:
+                if line.startswith('GIT_UG_REPO_REV'):
+                    line_prefix, old_sha = line.split(':=')
+                    print('Replacing Makefile SHA %s with %s' % (
+                        old_sha.rstrip(), new_rev))
+                    new_makefile.write('%s:= %s\n' % (
+                        line_prefix, new_rev))
+                else:
+                    new_makefile.write(line)
+
+    shutil.copyfile(new_makefile_path, makefile_path)
+
+
+if __name__ == '__main__':
+    sha = get_latest_rev_on_userguide()
+    update_userguide_rev_in_makefile(sha)
