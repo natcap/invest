dd97528cc8fc42c6204d978d4fa51353b98df278
davemfish <davemfish@gmail.com>
2020-03-12 14:01:17 -0700

    successfully starting and stopping the app in a test, using spectron. #5

--- tests/spectron.test.js
+++ tests/spectron.test.js
@@ -2,31 +2,34 @@ import { Application } from 'spectron';
 
 const app = new Application({
   path: 'node_modules/electron/dist/electron',
-  args: ['src/main.js'],
-  // chromeDriverArgs: ["--disable-dev-shm-usage", "--no-sandbox", "--headless"],
+  args: ['-r @babel/register src/main.js'],
+  // chromeDriverArgs: ['remote-debugging-port=9222'],
   chromeDriverLogPath: 'chromeDriver.log'
-})
-
-app.client.getMainProcessLogs().then(function (logs) {
-  logs.forEach(function (log) {
-    console.log(log)
-  })
 })
 
 jest.setTimeout(10000)
 
-beforeEach(() => {
-  return app.start()
-})
+// beforeEach(() => {
+//   return app.start()
+// })
 
-afterEach(() => {
-  // if (app.isRunning()) {
-  return app.stop()
-  // }
-})
+// afterEach(() => {
+//   if (app.isRunning()) {
+//     return app.stop()
+//   }
+// })
+
 
 test('Application starts', async () => {
-  await new Promise(resolve => setTimeout(resolve, 8000)); // sleep while app starts
-  console.log(app.client.getTitle())
-  expect(true)
-}, 10000)
+  console.log('1')
+  await app.start()
+  // app.client.getMainProcessLogs().then(function (logs) {
+  //   logs.forEach(function (log) {
+  //     console.log(log)
+  //   })
+  // })
+  console.log('2')
+  const title = app.client.getTitle()  
+  console.log(title)
+  await app.stop()
+})
