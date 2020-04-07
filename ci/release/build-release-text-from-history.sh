d1f216d454c5efd0201dbeef1043fe16bbdaa8ef
James Douglass <jamesdouglassusa@gmail.com>
2020-04-07 10:39:34 -0700

    Correcting bug in environment variables.  RE:#14

--- ci/release/build-release-text-from-history.sh
+++ ci/release/build-release-text-from-history.sh
@@ -6,18 +6,19 @@
 # This requires the GITHUB_REF environment variable to be declared,
 # which must be in the git refs format of a tag (examples: refs/tags/3.8.0)
 
-: "${GITHUB_REF:?Need to set GITHUB_REF to a git ref string.  Example: refs/tags/3.8.0}"
+: "${VERSION:?Need to set VERSION.  Example: VERSION=3.8.0}"
 
 # Format the HISTORY of this release for the release.
 # This file represents both the title and the body of the
 # release.  A blank line separates the title from the body.
 RELEASE_MESSAGE_RST_FILE=release_message.rst
-echo "${GITHUB_REF:10}" >> $RELEASE_MESSAGE_RST_FILE
+rm -f $RELEASE_MESSAGE_RST_FILE  # remove the file if it exists
+echo "$VERSION" >> $RELEASE_MESSAGE_RST_FILE
 echo "" >> $RELEASE_MESSAGE_RST_FILE
 
 # Read HISTORY from the released tag up until the first
 # blank line
-sed -n "/${GITHUB_REF:10}/,/^$/p" HISTORY.rst | tail -n +3 >> $RELEASE_MESSAGE_RST_FILE
+sed -n "/$VERSION/,/^$/p" HISTORY.rst | tail -n +3 >> $RELEASE_MESSAGE_RST_FILE
 
 pandoc \
     --from rst \
