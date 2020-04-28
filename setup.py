61615e0d3008c5c4b1dfe03003d41598138fef2e
James Douglass <jamesdouglassusa@gmail.com>
2020-04-28 09:25:52 -0700

    Adding compiler flag for renamed libc++. RE:#98

--- setup.py
+++ setup.py
@@ -24,6 +24,13 @@ _GUI_REQUIREMENTS = [req.split(';')[0].split('#')[0].strip() for req in
 README = open('README_PYTHON.rst').read().format(
     requirements='\n'.join(['    ' + r for r in _REQUIREMENTS]))
 
+# Since OSX Mavericks, the stdlib has been renamed.  So if we're on OSX, we
+# need to be sure to define which standard c++ library to use.  I don't have
+# access to a pre-Mavericks mac, so hopefully this won't break on someone's
+# older system.  Tested and it works on Mac OSX Catalina.
+compiler_and_linker_args = []
+if platform.system() == 'Darwin':
+    compiler_and_linker_args = ['-stdlib=libc++']
 
 setup(
     name='natcap.invest',
