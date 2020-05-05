19a947644c0a710e83238c2c3e6de66122b65b6a
davemfish <davemfish@gmail.com>
2020-05-05 14:08:08 -0700

    debugging missing libspatialindex binaries. #95.

--- exe/hooks/hook-rtree.py
+++ exe/hooks/hook-rtree.py
@@ -4,7 +4,7 @@ from PyInstaller.utils.hooks import collect_data_files
 if is_darwin:
 	import glob
 	binaries = [
-		(binary, 'dylib') for binary in glob.glob('**/lib/libspatialindex*')]
+		(binary, 'dylib') for binary in glob.glob('**/lib/libspatialindex*', recursive=True)]
 
 else:
 	datas = collect_data_files('rtree')
