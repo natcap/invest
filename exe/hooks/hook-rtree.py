5a30917e539789c296a2d4a6a029ff6ab897d148
davemfish <davemfish@gmail.com>
2020-05-05 16:38:15 -0700

    debugging missing libspatialindex binaries. #95.

--- exe/hooks/hook-rtree.py
+++ exe/hooks/hook-rtree.py
@@ -4,7 +4,7 @@ from PyInstaller.utils.hooks import collect_data_files
 if is_darwin:
 	import glob
 	binaries = [
-		(binary, 'dylib') for binary in glob.glob(
+		(binary, '.') for binary in glob.glob(
 			'/usr/local/miniconda/envs/mac-env/lib/libspatialindex*')]
 
 else:
