e9ffe416b074874beea56bb23647e539d1ff68f2
davemfish <davemfish@gmail.com>
2020-05-06 11:14:56 -0700

    reverting changes to hook-rtree because they already existed in invest.spec. #95.

--- exe/hooks/hook-rtree.py
+++ exe/hooks/hook-rtree.py
@@ -1,11 +1,3 @@
-from PyInstaller.compat import is_darwin
 from PyInstaller.utils.hooks import collect_data_files
 
-if is_darwin:
-	import glob
-	binaries = [
-		(binary, '.') for binary in glob.glob(
-			'/usr/local/miniconda/envs/mac-env/lib/libspatialindex*')]
-
-else:
-	datas = collect_data_files('rtree')
+datas = collect_data_files('rtree')
