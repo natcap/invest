3908bd1fe80146671bbb91d404d49b506b21bfd6
davemfish <davemfish@gmail.com>
2020-06-04 16:04:13 -0700

    debugging the build script because it's not working on windows.

--- build.js
+++ build.js
@@ -14,12 +14,17 @@ if (!fs.existsSync(BUILD_DIR)) {
 
 // transpile jsx and es6 files to javasciprt
 const cmdArgs = [SRC_DIR, '-d', BUILD_DIR]
-const investRun = spawnSync('npx babel', cmdArgs, {
+const runBabel = spawnSync('npx babel', cmdArgs, {
         shell: true, 
       });
 
+console.log(`${runBabel.stdout}`)
+if (runBabel.stderr) {
+	console.log(`${runBabel.stderr}`)
+}
+
 // copy all other files to their same relative location in the build dir
-glob(SRC_DIR.concat('/**/*'), (err, files) => {
+glob(SRC_DIR.concat(path.sep, '**', path.sep, '*'), (err, files) => {
 	files.forEach(file => {
 		if (['.css', '.html'].includes(path.extname(file))) {
 			const dest = file.replace(SRC_DIR, BUILD_DIR)
