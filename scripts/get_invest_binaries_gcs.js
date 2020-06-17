ad48f15c23d38fa40b299d8b18aa4f094bdb3f79
davemfish <davemfish@gmail.com>
2020-06-17 09:24:39 -0700

    make sure the correct binary archive is downloaded for the given OS build.

--- scripts/get_invest_binaries_gcs.js
+++ scripts/get_invest_binaries_gcs.js
@@ -3,6 +3,21 @@ const fs = require('fs')
 const path = require('path')
 const url = require('url')
 
+const args = process.argv.slice(2)
+if (args.length > 1) {
+	console.log('expected exactly 1 argument: the current OS')
+	break
+}
+let fileSuffix;
+switch (args[0]) {
+case 'windows-latest':
+	fileSuffix = 'Windows'
+case 'macos-latest':
+	fileSuffix = 'Darwin'
+case 'ubuntu-latest':
+	fileSuffix = 'Linux'
+}
+
 // todo, move this config to package.json, other config file,
 // and/or command line args, e.g. Actions YAML should set OS suffix
 // for SRCFILE
@@ -10,7 +25,7 @@ const HOSTNAME = 'https://storage.googleapis.com/'
 const BUCKET = 'natcap-dev-build-artifacts'
 const FORK = 'invest/davemfish'
 const VERSION = '3.8.0.post631+ge925ab2b/'
-const SRCFILE = 'invest_binaries_Linux.zip'
+const SRCFILE = `invest_binaries_${fileSuffix}.zip`
 const DESTFILE = './build/binaries.zip'
 
 const urladdress = url.resolve(HOSTNAME, path.join(BUCKET, FORK, VERSION, SRCFILE))
