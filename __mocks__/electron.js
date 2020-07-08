78734ec1afb664614dd3ec3178abfc82c7a420b5
davemfish <davemfish@gmail.com>
2020-07-08 16:37:06 -0700

    adding to the electron mock so tests can run in dev mode

--- __mocks__/electron.js
+++ __mocks__/electron.js
@@ -9,5 +9,12 @@ export const remote = {
 		getPath: jest.fn().mockImplementation(() => {
 			return path.resolve('tests/data')
 		})
+	},
+	// normally we have the command-line args passed to electron
+	// via `npm start` available through the remote.process module.
+	// The first two are built into npm start (I forget what they are).
+	// The third one we use and so must mock.
+	process: {
+		argv: ['', '', '--dev']
 	}
 }
