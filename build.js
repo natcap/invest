9f6193e27e6aaa4ede265c56f1b6d643678ea088
davemfish <davemfish@gmail.com>
2020-07-16 09:15:25 -0700

    linting the build script.

--- build.js
+++ build.js
@@ -1,35 +1,34 @@
 'use strict';
 
-const spawnSync = require('child_process').spawnSync;
+const { spawnSync } = require('child_process');
 const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob');
 
-const SRC_DIR = 'src'
-const BUILD_DIR = 'build'
+const SRC_DIR = 'src';
+const BUILD_DIR = 'build';
 
 if (!fs.existsSync(BUILD_DIR)) {
-	fs.mkdirSync(BUILD_DIR)
+  fs.mkdirSync(BUILD_DIR);
 }
 
 // transpile jsx and es6 files to javasciprt
-const cmdArgs = [SRC_DIR, '-d', BUILD_DIR]
+const cmdArgs = [SRC_DIR, '-d', BUILD_DIR];
 const runBabel = spawnSync('npx babel', cmdArgs, {
-        shell: true, 
-      });
+  shell: true,
+});
 
-console.log(`${runBabel.stdout}`)
+console.log(`${runBabel.stdout}`);
 if (runBabel.stderr) {
-	console.log(`${runBabel.stderr}`)
+  console.log(`${runBabel.stderr}`);
 }
 
 // copy all other files to their same relative location in the build dir
 glob(SRC_DIR.concat(path.sep, '**', path.sep, '*'), (err, files) => {
-	files.forEach(file => {
-		if (['.css', '.html', '.json'].includes(path.extname(file))) {
-			const dest = file.replace(SRC_DIR, BUILD_DIR)
-			fs.copySync(file, dest)
-		}
-	})
-})
-
+  files.forEach((file) => {
+    if (['.css', '.html', '.json'].includes(path.extname(file))) {
+      const dest = file.replace(SRC_DIR, BUILD_DIR);
+      fs.copySync(file, dest);
+    }
+  });
+});
