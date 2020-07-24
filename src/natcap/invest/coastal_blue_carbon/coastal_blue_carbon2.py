57dc3744e25b71bc5c9380bc47775a01fae494ca
James Douglass <jamesdouglassusa@gmail.com>
2020-07-24 15:12:56 -0700

    Handling transition years from the consumed table. RE:#209

--- src/natcap/invest/coastal_blue_carbon/coastal_blue_carbon2.py
+++ src/natcap/invest/coastal_blue_carbon/coastal_blue_carbon2.py
@@ -4,10 +4,12 @@ import logging
 import taskgraph
 import pygeoprocessing
 import pandas
+import numpy
 
 from .. import utils
 
 LOGGER = logging.getLogger(__name__)
+
 
 def execute(args):
     suffix = utils.make_suffix_string(args, 'results_suffix')
@@ -25,7 +27,64 @@ def execute(args):
         # TypeError when n_workers is None.
         n_workers = -1  # Synchronous mode.
     task_graph = taskgraph.TaskGraph(
-        churn_dir, n_workers, reporting_interval=5.0)
+        taskgraph_cache_dir, n_workers, reporting_interval=5.0)
+
+    if 'transitions_csv' in args and args['transitions_csv'] not in ('', None):
+        transitions = _extract_transitions_from_table(args['transitions_csv'])
+    else:
+        transitions = {}
+
+    baseline_lulc_info = pygeoprocessing.get_raster_info(
+        args['baseline_lulc_path'])
+    target_sr_wkt = baseline_lulc_info['projection_wkt']
+    min_pixel_size = numpy.min(numpy.abs(baseline_lulc_info['pixel_size']))
+    target_pixel_size = (min_pixel_size, -min_pixel_size)
+
+    transition_years = set()
+    try:
+        transition_years.add(int(args['baseline_lulc_year']))
+    except (KeyError, ValueError, TypeError):
+        LOGGER.error('The baseline_lulc_year is required but not provided.')
+        raise ValueError('Baseline lulc year is required.')
+
+    try:
+        transition_years.add(int(args['analysis_year']))
+    except (KeyError, ValueError, TypeError):
+        pass
+
+    base_paths = [args['baseline_lulc_path']]
+    aligned_paths = [os.path.join(
+        intermediate_dir, f'aligned_baseline_lulc{suffix}'.tif)]
+    for transition_year in transitions:
+        base_paths.append(transitions[transition_year])
+        transition_years.add(transition_year)
+        aligned_paths.append(
+            os.path.join(
+                intermediate_dir,
+                f'aligned_transition_{transition_year}{suffix}.tif'))
+
+    alignment_task = task_graph.add_task(
+        func=pygeoprocessing.align_and_resize_raster_stack,
+        args=(base_paths, aligned_paths, ['nearest']*len(base_paths),
+              target_pixel_size, 'intersection'),
+        kwargs={
+            'target_projection_wkt': target_sr_wkt,
+            'raster_align_index': 0,
+        },
+        hash_algorithm='md5',
+        copy_duplicate_artifact=True,
+        target_path_list=aligned_paths,
+        task_name='Align input landcover rasters.')
+
+
+
+
+
+
+
+
+
+
 
 
 def _extract_transitions_from_table(csv_path):
@@ -36,14 +95,11 @@ def _extract_transitions_from_table(csv_path):
     table = pandas.read_csv(
         csv_path, sep=None, index_col=False, engine='python',
         encoding=encoding)
-    table.columns = table.columns.str.lower()
+    table.columns = table.columns.str.lower().strip()
 
     output_dict = {}
     table.set_index('transition_year', drop=False, inplace=True)
     for index, row in table.iterrows():
-        output_dict[index] = row['raster_path']
+        output_dict[int(index)] = row['raster_path']
 
     return output_dict
-
-
-
