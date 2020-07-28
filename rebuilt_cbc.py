bfd2dc9de8604cd2d95e61c5c35ee1ec45954ab8
James Douglass <jamesdouglassusa@gmail.com>
2020-07-28 10:55:53 -0700

    Adding files I'm using for testing the new model.
    RE:#209

---
+++ rebuilt_cbc.py
@@ -0,0 +1,23 @@
+import os
+import logging
+
+from natcap.invest.coastal_blue_carbon import coastal_blue_carbon2
+
+logging.basicConfig(level=logging.INFO)
+ARGS = {
+    'workspace_dir': 'rebuilt_cbc',
+    'results_suffix': '',
+    'transitions_csv': 'rebuilt_cbc_transitions.csv',
+    'landcover_transitions_table': os.path.join(
+        'data', 'invest-sample-data', 'CoastalBlueCarbon',
+        'outputs_preprocessor', 'transitions_sample.csv'),
+    'baseline_lulc_path': os.path.join(
+        'data', 'invest-sample-data', 'CoastalBlueCarbon', 'inputs',
+        'GBJC_2010_mean_Resample.tif'),
+    'baseline_lulc_year': 2010,
+    'analysis_year': 2060,
+    'biophysical_table_path': 'rebuilt_cbc_biophysical_table.csv',
+}
+
+if __name__ == '__main__':
+    coastal_blue_carbon2.execute(ARGS)
