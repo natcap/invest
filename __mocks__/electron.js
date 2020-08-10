e1d09f68d065d585986e341e741dbded670ad6bc
davemfish <davemfish@gmail.com>
2020-08-10 09:07:40 -0700

    various minor lintings.

--- __mocks__/electron.js
+++ __mocks__/electron.js
@@ -1,26 +1,26 @@
 import path from 'path';
 
 const mockApp = {
-	getPath: jest.fn().mockImplementation(() => {
-		return path.resolve('tests/data')
-	})
-}
+  getPath: jest.fn().mockImplementation(() => {
+    return path.resolve('tests/data');
+  })
+};
 
 // mocks for when electron API accessed via remote from renderer process
 export const remote = {
-	dialog: {
-		showOpenDialog: jest.fn(),
-		showSaveDialog: jest.fn()
-	},
-	app: mockApp,
-	// normally we have the command-line args passed to electron
-	// via `npm start` available through the remote.process module.
-	// The first two are built into npm start (I forget what they are).
-	// The third one we use and so must mock.
-	process: {
-		argv: ['', '', '--dev']
-	}
-}
+  dialog: {
+    showOpenDialog: jest.fn(),
+    showSaveDialog: jest.fn(),
+  },
+  app: mockApp,
+  // normally we have the command-line args passed to electron
+  // via `npm start` available through the remote.process module.
+  // The first two are built into npm start (I forget what they are).
+  // The third one we use and so must mock.
+  process: {
+    argv: ['', '', '--dev'],
+  },
+};
 
 // mocks for when electron API called from main process
 export const app = mockApp
