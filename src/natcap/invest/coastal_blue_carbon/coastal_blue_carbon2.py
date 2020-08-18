941cc59eb456cd2e8e22c0591784ae57b9bb7a2e
James Douglass <jamesdouglassusa@gmail.com>
2020-08-18 11:29:47 -0700

    Starting a new execute entrypoint. RE:#209

--- src/natcap/invest/coastal_blue_carbon/coastal_blue_carbon2.py
+++ src/natcap/invest/coastal_blue_carbon/coastal_blue_carbon2.py
@@ -466,6 +466,113 @@ def execute(args):
     biophysical_parameters = utils.build_lookup_from_csv(
         args['biophysical_table_path'], 'code')
 
+    # The analysis between the baseline landcover raster and the first
+    # transition (or the analysis year, whichever comes first) is much simpler
+    # than the transition analysis.
+
+
+def execute_old(args):
+    suffix = utils.make_suffix_string(args, 'results_suffix')
+    output_dir = os.path.join(args['workspace_dir'], 'output')
+    intermediate_dir = os.path.join(args['workspace_dir'], 'intermediate')
+    taskgraph_cache_dir = os.path.join(intermediate_dir, 'task_cache')
+
+    utils.make_directories([output_dir, intermediate_dir, taskgraph_cache_dir])
+
+    try:
+        n_workers = int(args['n_workers'])
+    except (KeyError, ValueError, TypeError):
+        # KeyError when n_workers is not present in args
+        # ValueError when n_workers is an empty string.
+        # TypeError when n_workers is None.
+        n_workers = -1  # Synchronous mode.
+    task_graph = taskgraph.TaskGraph(
+        taskgraph_cache_dir, n_workers, reporting_interval=5.0)
+
+    if 'transitions_csv' in args and args['transitions_csv'] not in ('', None):
+        transitions = _extract_transitions_from_table(args['transitions_csv'])
+    else:
+        transitions = {}
+
+    # Phase 1: alignment and preparation of inputs
+    baseline_lulc_info = pygeoprocessing.get_raster_info(
+        args['baseline_lulc_path'])
+    target_sr_wkt = baseline_lulc_info['projection_wkt']
+    min_pixel_size = numpy.min(numpy.abs(baseline_lulc_info['pixel_size']))
+    target_pixel_size = (min_pixel_size, -min_pixel_size)
+
+    transition_years = set()
+    try:
+        baseline_lulc_year = int(args['baseline_lulc_year'])
+    except (KeyError, ValueError, TypeError):
+        LOGGER.error('The baseline_lulc_year is required but not provided.')
+        raise ValueError('Baseline lulc year is required.')
+
+    try:
+        # TODO: validate that args['analysis_year'] > max(transition_years)
+        analysis_year = int(args['analysis_year'])
+    except (KeyError, ValueError, TypeError):
+        analysis_year = None
+
+    # TODO: check that the years in the price table match the years in the
+    # range of the timesteps we're running.
+    if ('do_economic_analysis' in args and
+            args['do_economic_analysis'] not in ('', None)):
+        if args.get('do_price_table', False):
+            prices = {
+                year: values['price'] for (year, values) in
+                utils.build_lookup_from_csv(
+                    args['price_table'], 'year').items()}
+        else:
+            inflation_rate = float(args['inflation_rate']) * 0.01
+            annual_price = float(args['price'])
+            max_year = max(transition_years.keys()).union(set([analysis_year]))
+
+            prices = {}
+            for timestep_index, year in enumerate(
+                    range(baseline_lulc_year, max_year + 1)):
+                prices[year] = (
+                    ((1 + inflation_rate) ** timestep_index) *
+                    annual_price)
+
+        discount_rate = float(args['discount_rate']) * 0.01
+        for year, price in prices.items():
+            n_years_elapsed = year - baseline_lulc_year
+            prices[year] /= (1 + discount_rate) ** n_years_elapsed
+
+    base_paths = [args['baseline_lulc_path']]
+    aligned_lulc_paths = {}
+    aligned_paths = [os.path.join(
+        intermediate_dir,
+        f'aligned_lulc_baseline_{baseline_lulc_year}{suffix}.tif')]
+    aligned_lulc_paths[int(args['baseline_lulc_year'])] = aligned_paths[0]
+    for transition_year in transitions:
+        base_paths.append(transitions[transition_year])
+        transition_years.add(transition_year)
+        aligned_paths.append(
+            os.path.join(
+                intermediate_dir,
+                f'aligned_lulc_transition_{transition_year}{suffix}.tif'))
+        aligned_lulc_paths[transition_year] = aligned_paths[-1]
+
+    alignment_task = task_graph.add_task(
+        func=pygeoprocessing.align_and_resize_raster_stack,
+        args=(base_paths, aligned_paths, ['nearest']*len(base_paths),
+              target_pixel_size, 'intersection'),
+        kwargs={
+            'target_projection_wkt': target_sr_wkt,
+            'raster_align_index': 0,
+        },
+        hash_algorithm='md5',
+        copy_duplicate_artifact=True,
+        target_path_list=aligned_paths,
+        task_name='Align input landcover rasters.')
+
+    # We're assuming that the LULC initial variables and the carbon pool
+    # transient table are combined into a single lookup table.
+    biophysical_parameters = utils.build_lookup_from_csv(
+        args['biophysical_table_path'], 'code')
+
     stock_rasters = {}
     disturbance_rasters = {}
     halflife_rasters = {}
