7c8c909d7c459629e0e50606505f23c0dd2b8539
davemfish <davemfish@gmail.com>
2020-09-08 11:38:10 -0700

    improve scoping of App and InvestJob component tests.

--- tests/app.test.js
+++ tests/app.test.js
@@ -18,52 +18,14 @@ import {
 jest.mock('../src/server_requests');
 import { fileRegistry } from '../src/constants';
 import { cleanupDir } from '../src/utils'
-
 import SAMPLE_SPEC from './data/carbon_args_spec.json';
+
 const MOCK_INVEST_LIST = {
   Carbon: { 
     internal_name: 'carbon'
   }
 }
 const MOCK_VALIDATION_VALUE = [[['workspace_dir'], 'invalid because']];
-const MOCK_JOB_DATABASE_PATH = path.join(fileRegistry.CACHE_DIR, 'mock_job_database.json')
-const MOCK_JOB_DATA_PATH = path.join(fileRegistry.CACHE_DIR, 'mock_job_data.json');
-const MOCK_JOB_ID = 'job1hash';
-const MOCK_RECENT_JOBS_DB = { 
-  [MOCK_JOB_ID]: {
-    model: "carbon",
-    workspace: { "directory": "carbon_workspace", "suffix": null },
-    jobDataPath: MOCK_JOB_DATA_PATH,
-    status: "success",
-    humanTime: "3/5/2020, 10:43:14 AM",
-    systemTime: 1583259376573.759,
-  },
-};
-
-beforeAll(() => {
-  // Setting up the files that would exist if there are saved jobs.
-  const job = {
-    jobID: MOCK_JOB_ID,
-    modelRunName: MOCK_RECENT_JOBS_DB[MOCK_JOB_ID].model,
-    argsValues: {
-      workspace_dir: MOCK_RECENT_JOBS_DB[MOCK_JOB_ID].workspace.directory
-    },
-    workspace: MOCK_RECENT_JOBS_DB[MOCK_JOB_ID].workspace,
-    logfile: 'foo-log.txt',
-    status: MOCK_RECENT_JOBS_DB[MOCK_JOB_ID].status,
-  };
-  fs.writeFileSync(MOCK_JOB_DATA_PATH, JSON.stringify(job), 'utf8');
-  fs.writeFileSync(MOCK_JOB_DATABASE_PATH, JSON.stringify(MOCK_RECENT_JOBS_DB), 'utf8');
-})
-
-beforeEach(() => {
-  // needed for the fetchDatastack call monitoring. TODO: scope this better
-  jest.resetAllMocks();
-  // Careful with reset because "resetting a spy results
-  // in a function with no return value". I had been using spies to observe
-  // function calls, but not to mock return values. Spies used for that 
-  // purpose should be 'restored' not 'reset'. Do that inside the test as-needed.
-})
 
 afterAll(() => {
   // cache dir accumulates files when Execute is clicked
@@ -73,218 +35,246 @@ afterAll(() => {
   jest.resetAllMocks()
 })
 
-test('Clicking an invest model button renders SetupTab', async () => {
-  getInvestList.mockResolvedValue(MOCK_INVEST_LIST);
-  getSpec.mockResolvedValue(SAMPLE_SPEC);
-  fetchValidation.mockResolvedValue(MOCK_VALIDATION_VALUE);
+describe('Various ways to open and close InVEST models', () => {
+  beforeAll(() => {
+    getInvestList.mockResolvedValue(MOCK_INVEST_LIST);
+    getSpec.mockResolvedValue(SAMPLE_SPEC);
+    fetchValidation.mockResolvedValue(MOCK_VALIDATION_VALUE);
+  })
+  beforeEach(() => {
+    jest.clearAllMocks() // clears usage data, does not reset/restore
+  })
 
-  const { findByText } = render(
-    <App
-      jobDatabase={'foodb.json'}
-      investExe='foo'
-    />
-  );
+  test('Clicking an invest model button renders SetupTab', async () => {
+    const { findByText } = render(
+      <App
+        jobDatabase={'foodb.json'}
+        investExe='foo'
+      />
+    );
 
-  const carbon = await findByText('Carbon');
-  fireEvent.click(carbon);
-  const executeButton = await findByText('Execute');
-  const setupTab = await findByText('Setup');
-  expect(executeButton).toBeTruthy();
-  expect(executeButton).toBeDisabled();  // depends on the mocked fetchValidation
-  expect(setupTab.classList.contains('active')).toBeTruthy();  
-  expect(getSpec).toHaveBeenCalledTimes(1);
+    const carbon = await findByText('Carbon');
+    fireEvent.click(carbon);
+    const executeButton = await findByText('Execute');
+    const setupTab = await findByText('Setup');
+    expect(executeButton).toBeTruthy();
+    expect(executeButton).toBeDisabled();  // depends on the mocked fetchValidation
+    expect(setupTab.classList.contains('active')).toBeTruthy();  
+    expect(getSpec).toHaveBeenCalledTimes(1);
+  })
+
+  test('Clicking a recent job renders SetupTab', async () => {
+    const mockJobDatabasePath = path.join(fileRegistry.CACHE_DIR, 'mock_job_database.json')
+    const mockJobDataPath = path.join(fileRegistry.CACHE_DIR, 'mock_job_data.json');
+    const mockJobId = 'job1hash';
+    const mockRecentJobsDB = { 
+      [mockJobId]: {
+        model: "carbon",
+        workspace: { "directory": "carbon_workspace", "suffix": null },
+        jobDataPath: mockJobDataPath,
+        status: "success",
+        humanTime: "3/5/2020, 10:43:14 AM",
+        systemTime: 1583259376573.759,
+      },
+    };
+    // Setting up the files that would exist if there are saved jobs.
+    const job = {
+      jobID: mockJobId,
+      modelRunName: mockRecentJobsDB[mockJobId].model,
+      argsValues: {
+        workspace_dir: mockRecentJobsDB[mockJobId].workspace.directory
+      },
+      workspace: mockRecentJobsDB[mockJobId].workspace,
+      logfile: 'foo-log.txt',
+      status: mockRecentJobsDB[mockJobId].status,
+    };
+    fs.writeFileSync(mockJobDataPath, JSON.stringify(job), 'utf8');
+    fs.writeFileSync(mockJobDatabasePath, JSON.stringify(mockRecentJobsDB), 'utf8');
+
+    const { findByText, findByLabelText } = render(
+      <App
+        jobDatabase={mockJobDatabasePath}
+        investExe='foo'
+      />
+    );
+
+    const recentJobCard = await findByText(
+      mockRecentJobsDB[mockJobId].workspace.directory
+    );
+    fireEvent.click(recentJobCard);
+    const executeButton = await findByText('Execute');
+    const setupTab = await findByText('Setup');
+    expect(executeButton).toBeTruthy();
+    expect(executeButton).toBeDisabled(); // depends on the mocked fetchValidation
+    expect(setupTab.classList.contains('active')).toBeTruthy();
+
+    // Expect some arg values that were loaded from the saved job:
+    const input = await findByLabelText(/Workspace/);
+    expect(input).toHaveValue(
+      mockRecentJobsDB[mockJobId].workspace.directory
+    );
+  })
+
+  test('LoadParameters: Dialog callback renders SetupTab', async () => {
+    const mockDialogData = {
+      filePaths: ['foo.json']
+    }
+    const mockDatastack = {
+      module_name: 'natcap.invest.carbon',
+      args: {
+        carbon_pools_path: "Carbon/carbon_pools_willamette.csv", 
+      }
+    }
+    remote.dialog.showOpenDialog.mockResolvedValue(mockDialogData)
+    fetchDatastackFromFile.mockResolvedValue(mockDatastack)
+    
+    const { findByText, findByLabelText } = render(
+      <App
+        jobDatabase={'foodb.json'}
+        investExe='foo'
+      />
+    );
+
+    const loadButton = await findByText('Load Parameters');
+    fireEvent.click(loadButton);
+    const executeButton = await findByText('Execute');
+    const setupTab = await findByText('Setup');
+    const input = await findByLabelText(/Carbon Pools/);
+    expect(executeButton).toBeDisabled();  // depends on the mocked fetchValidation
+    expect(setupTab.classList.contains('active')).toBeTruthy();
+    expect(input).toHaveValue(mockDatastack.args.carbon_pools_path)
+  })
+
+  test('LoadParameters: Dialog callback does nothing when canceled', async () => {
+    // this resembles the callback data if the dialog is canceled instead of 
+    // a file selected.
+    const mockDialogData = {
+      filePaths: []
+    }
+    remote.dialog.showOpenDialog.mockResolvedValue(mockDialogData)
+    
+    const { findByText, findByLabelText } = render(
+      <App
+        jobDatabase={'foodb.json'}
+        investExe='foo'
+      />
+    );
+
+    const loadButton = await findByText('Load Parameters');
+    const homeTab = await findByText('Home');
+    fireEvent.click(loadButton);
+    // expect we're on the same tab we started on instead of switching to Setup
+    expect(homeTab.classList.contains('active')).toBeTruthy();
+    // These are the calls that would have triggered if a file was selected
+    expect(fetchDatastackFromFile).toHaveBeenCalledTimes(0)
+    expect(getSpec).toHaveBeenCalledTimes(0)
+  })
 })
 
-test('Clicking a recent job renders SetupTab', async () => {
-  getInvestList.mockResolvedValue(MOCK_INVEST_LIST);
-  fetchValidation.mockResolvedValue(MOCK_VALIDATION_VALUE);
-  getSpec.mockResolvedValue(SAMPLE_SPEC);
 
-  const { findByText, findByLabelText } = render(
-    <App
-      jobDatabase={MOCK_JOB_DATABASE_PATH}
-      investExe='foo'
-    />
-  );
-
-  const recentJobCard = await findByText(
-    MOCK_RECENT_JOBS_DB[MOCK_JOB_ID].workspace.directory
-  );
-  fireEvent.click(recentJobCard);
-  const executeButton = await findByText('Execute');
-  const setupTab = await findByText('Setup');
-  expect(executeButton).toBeTruthy();
-  expect(executeButton).toBeDisabled(); // depends on the mocked fetchValidation
-  expect(setupTab.classList.contains('active')).toBeTruthy();
-
-  // Expect some arg values that were loaded from the saved job:
-  const input = await findByLabelText(/Workspace/);
-  expect(input).toHaveValue(
-    MOCK_RECENT_JOBS_DB[MOCK_JOB_ID].workspace.directory
-  );
-})
-
-test('Recent Jobs: each has a button', async () => {
+describe('Display recently executed InVEST jobs', () => {
   getInvestList.mockResolvedValue({});
-  // This is a special json containing data used for testing
-  const testJobsDatabase = path.join(__dirname, './data/jobdb.json');
-  const { getByText } = render(
-    <App
-      jobDatabase={testJobsDatabase}
-      investExe='foo'/>);
-  const db = JSON.parse(fs.readFileSync(testJobsDatabase));
+  
+  test('Recent Jobs: each has a button', async () => {
+    // This json is tracked in the repo for purposes of this test.
+    const testJobsDatabase = path.join(__dirname, './data/jobdb.json');
+    const { getByText } = render(
+      <App
+        jobDatabase={testJobsDatabase}
+        investExe='foo'/>);
+    const db = JSON.parse(fs.readFileSync(testJobsDatabase));
 
-  await waitFor(() => {
-    Object.keys(db).forEach(job => {
-      expect(getByText(db[job].workspace.directory))
-        .toBeTruthy();
+    await waitFor(() => {
+      Object.keys(db).forEach(job => {
+        expect(getByText(db[job].workspace.directory))
+          .toBeTruthy();
+      })
     })
   })
+
+  test('Recent Jobs: Some text if the database does not exist', async () => {
+    const testJobsDatabase = 'foo.json';
+    const { findByText } = render(
+      <App
+        jobDatabase={testJobsDatabase}
+        investExe='foo'/>);
+
+    const node = await findByText(/No recent InVEST runs/)
+    expect(node).toBeInTheDocument()
+  })
 })
 
-test('Recent Jobs: database is missing', async () => {
+describe('InVEST global settings: dialog interactions', () => {
   getInvestList.mockResolvedValue({});
-  const testJobsDatabase = 'foo.json';
-  const { findByText } = render(
-    <App
-      jobDatabase={testJobsDatabase}
-      investExe='foo'/>);
+  test('Set the python logging level to pass to the invest CLI', async () => {
+    const DEFAULT = 'INFO';
+    const { getByText, getByLabelText } = render(
+      <App
+        jobDatabase={fileRegistry.JOBS_DATABASE}
+        investExe='foo'/>);
 
-  const node = await findByText(/No recent InVEST runs/)
-  expect(node).toBeInTheDocument()
-})
+    // Check the default settings
+    fireEvent.click(getByText('Settings'));
+    await waitFor(() => { 
+      // waiting because the selected value depends on passed props
+      expect(getByText(DEFAULT).selected).toBeTruthy();
+    })
 
-test('LoadParameters: Dialog callback renders SetupTab', async () => {
-  const mockDialogData = {
-    filePaths: ['foo.json']
-  }
-  const mockDatastack = {
-    module_name: 'natcap.invest.carbon',
-    args: {
-      carbon_pools_path: "Carbon/carbon_pools_willamette.csv", 
-    }
-  }
-
-  remote.dialog.showOpenDialog.mockResolvedValue(mockDialogData)
-  fetchDatastackFromFile.mockResolvedValue(mockDatastack)
-  getInvestList.mockResolvedValue(MOCK_INVEST_LIST);
-  getSpec.mockResolvedValue(SAMPLE_SPEC);
-  fetchValidation.mockResolvedValue(MOCK_VALIDATION_VALUE);
-
-  const { findByText, findByLabelText } = render(
-    <App
-      jobDatabase={'foodb.json'}
-      investExe='foo'
-    />
-  );
-
-  const loadButton = await findByText('Load Parameters');
-  fireEvent.click(loadButton);
-  const executeButton = await findByText('Execute');
-  const setupTab = await findByText('Setup');
-  const input = await findByLabelText(/Carbon Pools/);
-  expect(executeButton).toBeDisabled();  // depends on the mocked fetchValidation
-  expect(setupTab.classList.contains('active')).toBeTruthy();
-  expect(input).toHaveValue(mockDatastack.args.carbon_pools_path)
-})
-
-test('LoadParameters: Dialog callback does nothing when canceled', async () => {
-  // this resembles the callback data if the dialog is canceled instead of 
-  // a file selected.
-  const mockDialogData = {
-    filePaths: []
-  }
-  remote.dialog.showOpenDialog.mockResolvedValue(mockDialogData)
-  getInvestList.mockResolvedValue(MOCK_INVEST_LIST);
-  
-  const { findByText, findByLabelText } = render(
-    <App
-      jobDatabase={'foodb.json'}
-      investExe='foo'
-    />
-  );
-
-  const loadButton = await findByText('Load Parameters');
-  const homeTab = await findByText('Home');
-  fireEvent.click(loadButton);
-  // expect we're on the same tab we started on instead of switching to Setup
-  expect(homeTab.classList.contains('active')).toBeTruthy();
-  // These are the calls that would have triggered if a file was selected
-  expect(fetchDatastackFromFile).toHaveBeenCalledTimes(0)
-  expect(getSpec).toHaveBeenCalledTimes(0)
-})
-
-test('Settings dialog interactions: logging level', async () => {
-  getInvestList.mockResolvedValue({});
-  const DEFAULT = 'INFO';
-
-  const { getByText, getByLabelText } = render(
-    <App
-      jobDatabase={fileRegistry.JOBS_DATABASE}
-      investExe='foo'/>);
-
-  // Check the default settings
-  fireEvent.click(getByText('Settings'));
-  await waitFor(() => { 
-    // waiting because the selected value depends on passed props
+    // Change the select input and cancel -- expect default selected
+    fireEvent.change(getByLabelText('Logging threshold'),
+      { target: { value: 'DEBUG' } })
+    fireEvent.click(getByText('Cancel'));
+    // fireEvent.click(getByText('Settings'));  // why is this unecessary?
     expect(getByText(DEFAULT).selected).toBeTruthy();
+
+    // Change the select input and save -- expect new value selected
+    fireEvent.change(getByLabelText('Logging threshold'),
+      { target: { value: 'DEBUG' } })
+    fireEvent.click(getByText('Save Changes'));
+    // fireEvent.click(getByText('Settings'));  // why is this unecessary?
+    expect(getByText('DEBUG').selected).toBeTruthy();
   })
 
-  // Change the select input and cancel -- expect default selected
-  fireEvent.change(getByLabelText('Logging threshold'),
-    { target: { value: 'DEBUG' } })
-  fireEvent.click(getByText('Cancel'));
-  // fireEvent.click(getByText('Settings'));  // why is this unecessary?
-  expect(getByText(DEFAULT).selected).toBeTruthy();
+  test('Set the invest n_workers parameter', async () => {
+    const defaultValue = '-1';
+    const newValue = '2'
+    const badValue = 'a'
+    const labelText = 'Taskgraph n_workers parameter'
 
-  // Change the select input and save -- expect new value selected
-  fireEvent.change(getByLabelText('Logging threshold'),
-    { target: { value: 'DEBUG' } })
-  fireEvent.click(getByText('Save Changes'));
-  // fireEvent.click(getByText('Settings'));  // why is this unecessary?
-  expect(getByText('DEBUG').selected).toBeTruthy();
-})
+    const { getByText, getByLabelText } = render(
+      <App
+        jobDatabase={fileRegistry.JOBS_DATABASE}
+        investExe='foo'/>);
 
-test('Settings dialog interactions: n workers', async () => {
-  getInvestList.mockResolvedValue({});
-  const defaultValue = '-1';
-  const newValue = '2'
-  const badValue = 'a'
-  const labelText = 'Taskgraph n_workers parameter'
+    fireEvent.click(getByText('Settings'));
+    const input = getByLabelText(labelText, { exact: false })
+    
+    // Check the default settings
+    await waitFor(() => { 
+      // waiting because the text value depends on passed props
+      expect(input).toHaveValue(defaultValue);
+    })
 
-  const { getByText, getByLabelText } = render(
-    <App
-      jobDatabase={fileRegistry.JOBS_DATABASE}
-      investExe='foo'/>);
-
-  fireEvent.click(getByText('Settings'));
-  const input = getByLabelText(labelText, { exact: false })
-  
-  // Check the default settings
-  await waitFor(() => { 
-    // waiting because the text value depends on passed props
+    // Change the value and cancel -- expect default value
+    fireEvent.change(input, { target: { value: newValue } })
+    fireEvent.click(getByText('Cancel'));
     expect(input).toHaveValue(defaultValue);
+
+    // Change the value and save -- expect new value selected
+    fireEvent.change(input, { target: { value: newValue } })
+    expect(input).toHaveValue(newValue); // of course, we just set it
+    // The real test: still newValue after saving and re-opening
+    fireEvent.click(getByText('Save Changes'));
+    fireEvent.click(getByText('Settings'));
+    await waitFor(() => {  // the value to test is inherited through props
+      expect(input).toHaveValue(newValue);
+    })
+
+    // Change to bad value -- expect invalid signal
+    fireEvent.change(input, { target: { value: badValue } })
+    expect(input.classList.contains('is-invalid')).toBeTruthy();
+    expect(getByText('Save Changes')).toBeDisabled();
   })
-
-  // Change the value and cancel -- expect default value
-  fireEvent.change(input, { target: { value: newValue } })
-  fireEvent.click(getByText('Cancel'));
-  expect(input).toHaveValue(defaultValue);
-
-  // Change the value and save -- expect new value selected
-  fireEvent.change(input, { target: { value: newValue } })
-  expect(input).toHaveValue(newValue); // of course, we just set it
-  // The real test: still newValue after saving and re-opening
-  fireEvent.click(getByText('Save Changes'));
-  fireEvent.click(getByText('Settings'));
-  await waitFor(() => {  // the value to test is inherited through props
-    expect(input).toHaveValue(newValue);
-  })
-
-  // Change to bad value -- expect invalid signal
-  fireEvent.change(input, { target: { value: badValue } })
-  expect(input.classList.contains('is-invalid')).toBeTruthy();
-  expect(getByText('Save Changes')).toBeDisabled();
 })
 
 describe('InVEST subprocess testing', () => {
