2e8aa8a128b80d06e0786a0562a1beeb3cb93174
davemfish <davemfish@gmail.com>
2020-09-15 10:38:16 -0700

    preparing to use spectron to launch app and run an invest model.

--- tests/binary_tests/spectron.test.js
+++ tests/binary_tests/spectron.test.js
@@ -1,19 +1,25 @@
 import { Application } from 'spectron';
+import glob from 'glob';
 
-const app = new Application({
-  path: 'node_modules/electron/dist/electron',
-  args: ["src/specmain.js"],
-  // args: ["src/main.js"],
-  webdriverOptions: {execArgv: "-r @babel/register"},
-  chromeDriverArgs: ['--disable-dev-shm-usage', '--headless'],
-  chromeDriverLogPath: 'chromeDriver.log'
-})
+const appPath = glob.sync('dist/invest-desktop_*')
+
+beforeAll(() => {
+  const app = new Application({
+    path: appPath[0],
+    // args: ["src/specmain.js"],
+    // args: ["src/main.js"],
+    // webdriverOptions: {execArgv: "-r @babel/register"},
+    chromeDriverArgs: ['--disable-dev-shm-usage', '--headless'],
+    chromeDriverLogPath: 'chromeDriver.log'
+  });
+});
+
 
 // jest.setTimeout(10000)
 
-// beforeEach(() => {
-//   return app.start()
-// })
+beforeEach(() => {
+  return app.start()
+})
 
 afterAll(() => {
   if (app.isRunning()) {
