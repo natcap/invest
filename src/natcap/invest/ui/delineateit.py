7f10db89f27c26fe2da28d7d77e7498aa531c4cc
emlys <esoth@stanford.edu>
2020-09-28 11:12:46 -0600

    added checkbox input; connect to outlet features input

--- src/natcap/invest/ui/delineateit.py
+++ src/natcap/invest/ui/delineateit.py
@@ -22,14 +22,16 @@ class Delineateit(model.InVESTModel):
             validator=self.validator)
         self.add_input(self.dem_path)
 
-        self.outlet_vector_container = inputs.Container(
-            label=('Use your own geometries to delineate watersheds from, '
-                   'instead of automatically detected pour points.'),
-            expandable=True,
-            expanded=False,
-            interactive=False,
-            args_key='outlet_vector')
-        self.add_input(self.outlet_vector_container)
+        self.detect_pour_points = inputs.Checkbox(
+            args_key='detect_pour_points',
+            helptext=(
+                'If this box is checked, run pour point detection algorithm. '
+                'Use detected pour points instead of outlet geometries.'),
+            label='Detect pour points'
+        )
+        self.add_input(self.detect_pour_points)
+        self.detect_pour_points.sufficiency_changed.connect(
+            self.on_detect_pour_points_changed)
 
         self.outlet_vector_path = inputs.File(
             args_key='outlet_vector_path',
@@ -38,10 +40,9 @@ class Delineateit(model.InVESTModel):
                 "outlets such as municipal water intakes or lakes."),
             label='Outlet Features (Vector)',
             validator=self.validator)
-        self.outlet_vector_container.add_input(self.outlet_vector_path)
+        self.add_input(self.outlet_vector_path)
         self.outlet_vector_path.value_changed.connect(
             self._enable_point_snapping_container)
-
         self.skip_invalid_geometry = inputs.Checkbox(
             args_key='skip_invalid_geometry',
             helptext=(
@@ -83,7 +84,13 @@ class Delineateit(model.InVESTModel):
                 "snapped.  MultiPoints will also not be snapped."),
             validator=self.validator)
         self.snap_points_container.add_input(self.snap_distance)
-        
+
+    def on_detect_pour_points_changed(self, value):
+        """Change interactivity of other inputs given boolean signal value"""
+        self.outlet_vector_path.set_interactive(not value)
+        self.skip_invalid_geometry.set_interactive(not value)
+        self.snap_points_container.set_interactive(value)
+
     def _enable_point_snapping_container(self, input_valid):
         outlet_vector_path = self.outlet_vector_path.value()
         if delineateit._vector_may_contain_points(outlet_vector_path):
@@ -97,7 +104,6 @@ class Delineateit(model.InVESTModel):
             self.workspace.args_key: self.workspace.value(),
             self.suffix.args_key: self.suffix.value(),
             self.dem_path.args_key: self.dem_path.value(),
-            self.use_outlet_vector.args_key: self.use_outlet_vector.value(),
             self.outlet_vector_path.args_key: (
                 self.outlet_vector_path.value()),
             self.snap_points_container.args_key: (
