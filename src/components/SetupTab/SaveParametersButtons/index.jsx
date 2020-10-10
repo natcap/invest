4e33a81f874e1d7e39e844d72200cc0a44115612
davemfish <davemfish@gmail.com>
2020-10-10 16:06:07 -0700

    for safety, and for tests, only portal to  DOM node if it exists.

--- src/components/SetupTab/SaveParametersButtons/index.jsx
+++ src/components/SetupTab/SaveParametersButtons/index.jsx
@@ -9,26 +9,29 @@ import SaveFileButton from '../../SaveFileButton';
 export default class SaveParametersButtons extends React.Component {
   render() {
     const sidebarNode = document.getElementById('setup-sidebar-children');
-    return ReactDOM.createPortal(
-      (
-        <DropdownButton
-          id="dropdown-basic-button"
-          title="Save Parameters"
-          renderMenuOnMount // w/o this, items inaccessible in jsdom test env
-          className="mx-3 float-right"
-        >
-          <SaveFileButton
-            title="Save parameters to JSON"
-            defaultTargetPath="invest_args.json"
-            func={this.props.wrapArgsToJsonFile}
-          />
-          <SaveFileButton
-            title="Save to Python script"
-            defaultTargetPath="execute_invest.py"
-            func={this.props.savePythonScript}
-          />
-        </DropdownButton>
-      ), sidebarNode
-    );
+    if (sidebarNode) {
+      return ReactDOM.createPortal(
+        (
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Save Parameters"
+            renderMenuOnMount // w/o this, items inaccessible in jsdom test env
+            className="mx-3 float-right"
+          >
+            <SaveFileButton
+              title="Save parameters to JSON"
+              defaultTargetPath="invest_args.json"
+              func={this.props.wrapArgsToJsonFile}
+            />
+            <SaveFileButton
+              title="Save to Python script"
+              defaultTargetPath="execute_invest.py"
+              func={this.props.savePythonScript}
+            />
+          </DropdownButton>
+        ), sidebarNode
+      );
+    }
+    return (<div/>);
   }
 }
