87dd562fb84535b7c3f55f09d3d6f84976d4cc97
emlys <esoth@stanford.edu>
2020-10-21 09:12:07 -0600

    edit bundling script

--- installer/darwin/build_app_bundle.sh
+++ installer/darwin/build_app_bundle.sh
@@ -20,13 +20,37 @@ mkdir -p "$tempdir"
 # copy out all the invest shell files and fixup the paths.
 # .command extension makes the scripts runnable by the user.
 # Shell files without the `invest_` prefix will be left alone.
+
+
+# Correct macOS app bundle structure 
+# https://developer.apple.com/library/archive/technotes/tn2206/_index.html#//apple_ref/doc/uid/DTS40007919-CH1-TNTAG201
+# see https://developer.apple.com/forums/thread/114943?answerId=353975022#353975022
+#
+# Only Mach-O format files should be signed as code because they have the LC_CODE_SIGNATURE attribute.
+# Trying to sign other formats as code results in the signature being stored in an extended attribute.
+# Extended attributes can be lost when moving files around so it's not recommended.
+# Everything in Resources/ is still signed, but as a resource under the seal of the parent bundle. 
+#
+#
+# InVEST.app/
+#   Contents/
+#       Frameworks/
+#           all .dylibs
+#       MacOS/
+#           InVEST unix executable
+#       Resources/
+#           everything else!
+#       Info.plist
+
 new_basename='InVEST'
 _APPDIR="$3"
 _MACOSDIR="$_APPDIR/Contents/MacOS"
 _RESOURCEDIR="$_APPDIR/Contents/Resources"
+_FRAMEWORKSDIR="$_APPDIR/Contents/Frameworks"
 CONFIG_DIR="installer/darwin"
 mkdir -p "${_MACOSDIR}"
 mkdir -p "${_RESOURCEDIR}"
+
 cp -r "$2" "$_MACOSDIR/invest_dist"
 new_command_file="$_MACOSDIR/$new_basename"
 cp $CONFIG_DIR/invest.icns "$_RESOURCEDIR/invest.icns"
