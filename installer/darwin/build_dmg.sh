d6200d89638f82fc769b0f1da32fdc565dc0da4c
emlys <esoth@stanford.edu>
2020-11-04 12:15:39 -0700

    make sure dmg is located at env var path

--- installer/darwin/build_dmg.sh
+++ installer/darwin/build_dmg.sh
@@ -6,12 +6,12 @@
 #  $1 = the version string to use
 #  $2 = the path to the binary dir to package.
 #  $3 = the path to the directory of html documentation to include.
+#  $4 = the path to the final DMG to create
 #
 # Script adapted from http://stackoverflow.com/a/1513578/299084
 
 CONFIG_DIR="installer/darwin"
 title="InVEST ${1}"  # the name of the volume the DMG provides.
-finalDMGName="dist/InVEST-${1}.dmg"  # the name of the final DMG file.
 
 # copy the docs into the dmg
 docsdir="$3"
@@ -23,8 +23,6 @@ fi
 # Copy the release notes (HISTORY.rst) into the dmg as an HTML doc.
 pandoc HISTORY.rst -o $2/HISTORY.html
 
-dmgbuild -Dinvestdir="$2" -s $CONFIG_DIR/dmgconf.py "$title" "$finalDMGName"
+dmgbuild -Dinvestdir="$2" -s $CONFIG_DIR/dmgconf.py "$title" "$4"
 
 find . -name "InVEST-${1}.dmg"
-
-# codesign --verbose --sign "Natural Capital Project Software Team" "$finalDMG"
