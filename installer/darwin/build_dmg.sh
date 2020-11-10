34d7f902ef15fdc8b11bba6bc0e73c6eecbc0e74
emlys <esoth@stanford.edu>
2020-11-10 16:54:56 -0700

    don't put docs or history into app bundle

--- installer/darwin/build_dmg.sh
+++ installer/darwin/build_dmg.sh
@@ -4,23 +4,32 @@
 #
 # Arguments:
 #  $1 = the version string to use
-#  $2 = the path to the binary dir to package.
-#  $3 = the path to the directory of html documentation to include.
-#  $4 = the path to the final DMG to create
+#  $2 = the path to the binary dir to package
+#  $3 = the path to the directory of html documentation to include
+#  $4 = the path to a python file setting the dmgbuild configuration
+#  $5 = the path to the final DMG to create
 
 VERSION=$1
 BINARY_PATH=$2
 DOCS_PATH=$3
-DMG_PATH=$4
-DMG_CONFIG_PATH=$5
+DMG_CONFIG_PATH=$4
+DMG_PATH=$5
 
-# copy the docs into the dmg
+# create a temp dir to pass to dmgbuild
+# this will hold the app bundle, docs, and history
+TEMPDIR="tmp_dmg_dir"
+mkdir TEMPDIR
+
+# copy in the app bundle
+cp -r $BINARY_PATH $TEMPDIR
+
+# copy in the html docs
 if [ -d $DOCS_PATH ]
 then
-    cp -r $DOCS_PATH $BINARY_PATH/documentation
+    cp -r $DOCS_PATH $TEMPDIR/documentation
 fi
 
-# Copy the release notes (HISTORY.rst) into the dmg as an HTML doc.
-pandoc HISTORY.rst -o BINARY_PATH/HISTORY.html
+# Copy in the release notes (HISTORY.rst) as an HTML doc.
+pandoc HISTORY.rst -o $TEMPDIR/HISTORY.html
 
-dmgbuild -Dinvestdir=$BINARY_PATH -s $DMG_CONFIG_PATH "InVEST ${VERSION}" $DMG_PATH
+dmgbuild -Dinvestdir=$TEMPDIR -s $DMG_CONFIG_PATH "InVEST ${VERSION}" $DMG_PATH
