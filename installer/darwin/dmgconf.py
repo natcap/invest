10ca66ed9202c80282d6f66abed255f1afb4c9e2
emlys <esoth@stanford.edu>
2020-11-10 11:31:35 -0700

    change size again

--- installer/darwin/dmgconf.py
+++ installer/darwin/dmgconf.py
@@ -19,14 +19,14 @@ symlinks = {'Applications': '/Applications'}
 files = [defines['investdir']]
 
 icon_locations = {
-    _invest_dirname: (100, 120),
-    'Applications': (350, 120)
+    _invest_dirname: (200, 250),
+    'Applications': (650, 250)
 }
 icon_size = 70
 text_size = 12
 
 # Window Settings
-window_rect = ((0, 10000), (450, 375))
+window_rect = ((0, 10000), (1000, 700))
 background = os.path.join(CWD, 'background.png')
 #background = 'builtin-arrow'
 default_view = 'icon-view'
