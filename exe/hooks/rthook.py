c20f4b10d3b39194d41fd98b2fdc117c2154416d
James Douglass <jamesdouglassusa@gmail.com>
2020-11-17 10:17:16 -0800

    Adding environment variable for Qt launch on Big Sur. RE:#384

--- exe/hooks/rthook.py
+++ exe/hooks/rthook.py
@@ -6,3 +6,8 @@ import platform
 multiprocessing.freeze_support()
 
 os.environ['PROJ_LIB'] = os.path.join(sys._MEIPASS, 'proj')
+
+# This allows Qt 5.13+ to start on Big Sur.
+# See https://bugreports.qt.io/browse/QTBUG-87014
+# and https://github.com/natcap/invest/issues/384
+os.environ['QT_MAC_WANTS_LAYER'] = 1
