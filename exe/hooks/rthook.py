9ea1ff9bdacfc5041aef781a9a470b7122f580f1
James Douglass <jamesdouglassusa@gmail.com>
2020-11-17 16:37:51 -0800

    Only setting the QT_MAC_WANTS_LAYER variable on mac. RE:#384

--- exe/hooks/rthook.py
+++ exe/hooks/rthook.py
@@ -7,7 +7,8 @@ multiprocessing.freeze_support()
 
 os.environ['PROJ_LIB'] = os.path.join(sys._MEIPASS, 'proj')
 
-# This allows Qt 5.13+ to start on Big Sur.
-# See https://bugreports.qt.io/browse/QTBUG-87014
-# and https://github.com/natcap/invest/issues/384
-os.environ['QT_MAC_WANTS_LAYER'] = '1'
+if platform.system() == 'Darwin':
+    # This allows Qt 5.13+ to start on Big Sur.
+    # See https://bugreports.qt.io/browse/QTBUG-87014
+    # and https://github.com/natcap/invest/issues/384
+    os.environ['QT_MAC_WANTS_LAYER'] = '1'
