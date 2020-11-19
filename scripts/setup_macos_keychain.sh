545b0bbf9730a43a9412ae6c67fc2c464a371928
emlys <esoth@stanford.edu>
2020-11-19 16:09:12 -0700

    use env vars rather than arguments

--- scripts/setup_macos_keychain.sh
+++ scripts/setup_macos_keychain.sh
@@ -1,35 +1,25 @@
 #!/bin/bash -ve
 
-#
-# Arguments:
-#  $1 = the name to give the new keychain
-#  $2 = the keychain password to set
-#  $3 = path to .p12 certificate file to add to keychain
-#  $4 = .p12 certificate file password
-
+# The environment variables referenced should be set by the calling github actions step
 
 security list-keychains
-security create-keychain -p $2 $1
-security list-keychains -s $1
+security create-keychain -p $KEYCHAIN_PASS $KEYCHAIN_NAME
+security list-keychains -s $KEYCHAIN_NAME
 security list-keychains
 
-echo $1
-echo $2
-echo $3
-echo $4
 
 echo 'listed keychains'
 # unlock the keychain so we can import to it (stays unlocked 5 minutes by default)
-security unlock-keychain -p $2 $1
+security unlock-keychain -p $KEYCHAIN_PASS $KEYCHAIN_NAME
 echo 'unlocked keychain'
 
 # add the certificate to the keychain
 # -T option says that the codesign executable can access the keychain
 # for some reason this alone is not enough, also need the following step
-security import $3 -k $1 -P $4 -T /usr/bin/codesign
+security import $P12_FILE_PATH -k $KEYCHAIN_NAME -P $CERT_KEY_PASS -T /usr/bin/codesign
 
 echo 'imported cert'
 # this is essential to avoid the UI password prompt
-security set-key-partition-list -S apple-tool:,apple: -s -k $2 $1
+security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASS $KEYCHAIN_NAME
 
 
