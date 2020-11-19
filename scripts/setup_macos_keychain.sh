799d43cb2f5ec24a3e9b035ba0e7569f3737ab43
emlys <esoth@stanford.edu>
2020-11-19 13:24:48 -0700

    more testing

--- scripts/setup_macos_keychain.sh
+++ scripts/setup_macos_keychain.sh
@@ -7,10 +7,14 @@
 #  $3 = path to .p12 certificate file to add to keychain
 #  $4 = .p12 certificate file password
 
+
 security list-keychains
 security create-keychain -p $2 $1
 security list-keychains -s $1
 security list-keychains
+
+echo $1
+echo $3
 
 echo 'listed keychains'
 # unlock the keychain so we can import to it (stays unlocked 5 minutes by default)
@@ -21,6 +25,8 @@ echo 'unlocked keychain'
 # -T option says that the codesign executable can access the keychain
 # for some reason this alone is not enough, also need the following step
 security import $3 -k $1 -P $4 -T /usr/bin/codesign
+
+echo 'imported cert'
 # this is essential to avoid the UI password prompt
 security set-key-partition-list -S apple-tool:,apple: -s -k $2 $1
 
