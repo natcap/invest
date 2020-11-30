11bd67fc9113c7ca695ee8b277a20cf0cefd1a52
davemfish <davemfish@gmail.com>
2020-11-30 06:39:25 -0800

    linting a script

--- scripts/fetch_invest_binaries.js
+++ scripts/fetch_invest_binaries.js
@@ -1,59 +1,61 @@
-/**
- * 2020-07-30 - the workbench build process no longer depends on
- * pre-built binaries, instead building them itself, so this script
- * is not being used anymore.
- * 
- * This is a utility script for fetching prebuilt invest binaries
- * that include the server.py module and thus are compatible with the
- * workbench.
- */
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const pkg = require('../package');
 
-const https = require('https')
-const fs = require('fs')
-const path = require('path')
-const url = require('url')
-const package = require('../package')
-
-let fileSuffix;
+let filePrefix;
+// The invest build process only builds for these OS
 switch (process.platform) {
-	case 'win32':
-		filePrefix = 'windows'
-		break
-	case 'darwin':
-		filePrefix = 'macos'
-		break
-	case 'linux':
-		filePrefix = 'linux'
-		break
-	default:
-		throw new Error("expected platform to be windows, mac, or linux")
+  case 'win32':
+    filePrefix = 'windows';
+    break;
+  case 'darwin':
+    filePrefix = 'macos';
+    break;
+  // case 'linux':
+  //   filePrefix = 'linux';
+  //   break;
+  default:
+    throw new Error(
+      `No prebuilt invest binaries are available for ${process.platform}`
+    );
 }
 
-const HOSTNAME = package.invest.hostname
-const BUCKET = package.invest.bucket
-const FORK = package.invest.fork
-const VERSION = package.invest.version
-const SRCFILE = `${filePrefix}_invest_binaries.zip`
+const HOSTNAME = pkg.invest.hostname;
+const BUCKET = pkg.invest.bucket;
+const FORK = pkg.invest.fork;
+const VERSION = pkg.invest.version;
+const SRCFILE = `${filePrefix}_invest_binaries.zip`;
 const DESTFILE = path.resolve('build/binaries.zip');
 
-const urladdress = url.resolve(HOSTNAME, path.join(BUCKET, FORK, VERSION, SRCFILE))
+const urladdress = url.resolve(
+  HOSTNAME, path.join(BUCKET, FORK, VERSION, SRCFILE)
+);
 
-const download = function(url, dest) {
-	console.log(`fetching ${url}`)
-	fs.existsSync(path.dirname(dest)) || fs.mkdirSync(path.dirname(dest));
-	const fileStream = fs.createWriteStream(dest)
-	const request = https.get(url, function(response) {
-		console.log(response.statusCode)
-		if (response.statusCode != 200) {
-			fileStream.close()
-			console.log(url)
-			return
-		}
-		response.pipe(fileStream)
-		fileStream.on('finish', function() {
-			fileStream.close()
-		})
-	})
+/**
+ * Download a file from src to dest.
+ *
+ * @param  {string} src - url for a single publicly hosted file
+ * @param  {string} dest - local path for saving the file
+ */
+function download(src, dest) {
+  console.log(`downloading ${url}`);
+  fs.existsSync(path.dirname(dest)) || fs.mkdirSync(path.dirname(dest));
+  const fileStream = fs.createWriteStream(dest);
+  https.get(src, (response) => {
+    console.log(response.statusCode);
+    if (response.statusCode !== 200) {
+      fileStream.close();
+      return;
+    }
+    response.pipe(fileStream);
+    fileStream.on('finish', () => {
+      fileStream.close();
+    });
+  }).on('error', (e) => {
+    console.log(e);
+  });
 }
 
-download(urladdress, DESTFILE)
+download(urladdress, DESTFILE);
