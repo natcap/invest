6b1f426c4af5d532ad0ca8b1ed8bc68a0589a3ba
emlys <esoth@stanford.edu>
2020-12-03 17:35:08 -0700

    remove unused sphinx options

--- doc/api-docs/conf.py
+++ doc/api-docs/conf.py
@@ -16,11 +16,12 @@ import importlib
 import itertools
 import os
 import pkgutil
+import subprocess
 import sys
 import warnings
-from unittest.mock import MagicMock
+
 from sphinx.ext import apidoc
-import subprocess
+from unittest.mock import MagicMock
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -48,9 +49,6 @@ templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
-
-# The encoding of source files.
-#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -81,36 +79,12 @@ release = _version
 # Usually you set "language" from the command line for these cases.
 language = None
 
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = []
 
-# The reST default role (used for this markup: `text`) to use for all
-# documents.
-#default_role = None
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-#add_module_names = True
-
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
-
-# A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
 keep_warnings = False
@@ -124,20 +98,8 @@ import sphinx_rtd_theme
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-#html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -153,84 +115,10 @@ html_favicon = "_static/favicon.gif"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-#html_extra_path = []
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# If false, no module index is generated.
-#html_domain_indices = True
-
-# If false, no index is generated.
-#html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
-
-# Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
-
-# A dictionary with options for the search language support, empty by default.
-# Now only 'ja' uses this config value
-#html_search_options = {'type': 'default'}
-
-# The name of a javascript file (relative to the configuration directory) that
-# implements a search results scorer. If empty, the default will be used.
-#html_search_scorer = 'scorer.js'
-
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'InVEST3doc'
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
-}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
@@ -239,26 +127,6 @@ latex_documents = [
   ('index', 'InVEST3.tex', u'InVEST 3 Documentation',
    u'The Natural Capital Project', 'manual'),
 ]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
-
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
-
-# If false, no module index is generated.
-#latex_domain_indices = True
 
 
 # -- Options for manual page output ---------------------------------------
@@ -269,9 +137,6 @@ man_pages = [
     ('index', 'invest', u'InVEST Documentation',
      [u'The Natural Capital Project'], 1)
 ]
-
-# If true, show URL addresses after external links.
-#man_show_urls = False
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -285,18 +150,11 @@ texinfo_documents = [
    'Miscellaneous'),
 ]
 
-# Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
 
-# If false, no module index is generated.
-#texinfo_domain_indices = True
+# -- Prepare for sphinx build ---------------------------------------------
 
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
-
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#texinfo_no_detailmenu = False
-
+# build cython extensions in-place so that sphinx can find the .so files
+# alongside the source code
 subprocess.run(['python', 'setup.py', 'build_ext', '--inplace'], 
                cwd=os.path.join(DOCS_SOURCE_DIR, '..', '..'))
 
@@ -325,11 +183,9 @@ sys.modules.update([
   ('natcap.invest', MagicMock())
 ])
 
-# Use sphinx apidoc tool to generate documentation for invest
-# Generated rst files go into the api/ directory
-
-# Note that some apidoc options like --separate and --module-first may not work
-# the same because we aren't making use of their values in the custom templates
+# Use sphinx apidoc tool to generate documentation for invest. Generated rst 
+# files go into the api/ directory. Note that some apidoc options may not work 
+# the same because we aren't using their values in the custom templates
 apidoc.main([
     '--force',  # overwrite any files from previous run
     '-o', os.path.join(DOCS_SOURCE_DIR, 'api'),  # output to api/
@@ -339,6 +195,8 @@ apidoc.main([
     os.path.join(DOCS_SOURCE_DIR, '..', '..', 'src', 'natcap')
 ])
 
+
+# -- Generate model entrypoints file --------------------------------------
 
 MODEL_RST_TEMPLATE = """
 .. _models:
