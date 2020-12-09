3c173237b83eb7d42b5a155c7fa3985b146a37cc
Doug <dcdenu4@gmail.com>
2020-12-09 14:34:40 -0500

    Update HQ default half sat for sample data
    Update value from 0.5 to 0.05. Also update the revisions for sample data
    and users guide that now reflect this change.

--- src/natcap/invest/ui/habitat_quality.py
+++ src/natcap/invest/ui/habitat_quality.py
@@ -148,7 +148,7 @@ class HabitatQuality(model.InVESTModel):
             args_key='half_saturation_constant',
             helptext=(
                 "A positive floating point value that is defaulted at "
-                "0.5. This is the value of the parameter k in equation "
+                "0.05. This is the value of the parameter k in equation "
                 "(4). In general, set k to half of the highest grid "
                 "cell degradation value on the landscape.  To perform "
                 "this model calibration the model must be run once in "
