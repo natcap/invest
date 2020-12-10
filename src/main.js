d468d512343fb8b78560c1704bdde413f32327ba
davemfish <davemfish@gmail.com>
2020-12-10 12:13:30 -0800

    specify a port number when starting the flask subprocess

--- src/main.js
+++ src/main.js
@@ -20,7 +20,10 @@ const { getLogger } = require('./logger');
 
 const logger = getLogger(__filename.split('/').slice(-1)[0]);
 
-const PORT = (process.env.PORT || '5000').trim();
+// This could be optionally configured already in '.env'
+if (!process.env.PORT) {
+  process.env.PORT = 56789;
+}
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
