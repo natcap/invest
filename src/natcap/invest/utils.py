2af300dfdad2799d02da8659851eabf2a114496b
emlys <esoth@stanford.edu>
2021-01-05 14:32:24 -0700

    add logger name to logging format

--- src/natcap/invest/utils.py
+++ src/natcap/invest/utils.py
@@ -20,6 +20,7 @@ import pygeoprocessing
 LOGGER = logging.getLogger(__name__)
 LOG_FMT = (
     "%(asctime)s "
+    "(%(name)s) "
     "%(module)s.%(funcName)s(%(lineno)d) "
     "%(levelname)s %(message)s")
 
