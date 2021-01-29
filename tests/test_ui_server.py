9015fdad47a6d6d41a8d8d161666184efb5e61f8
emlys <esoth@stanford.edu>
2021-01-29 10:03:05 -0700

    update test to may_have_points

--- tests/test_ui_server.py
+++ tests/test_ui_server.py
@@ -27,35 +27,39 @@ class EndpointFunctionTests(unittest.TestCase):
         colnames = json.loads(response.get_data(as_text=True))
         self.assertEqual(colnames, [])
 
-    def test_get_vector_has_points(self):
+    def test_get_vector_may_have_points(self):
         from natcap.invest import ui_server
         test_client = ui_server.app.test_client()
         # an empty path
         response = test_client.post(
-            '/vector_has_points', 
+            '/vector_may_have_points', 
             json={'vector_path': ''})
-        has_points = json.loads(response.get_data(as_text=True))['has_points']
-        self.assertEqual(has_points, True)
+        may_have_points = json.loads(
+            response.get_data(as_text=True))['may_have_points']
+        self.assertEqual(may_have_points, True)
         # a vector with no point geometries
         path = os.path.join(
             TEST_DATA_PATH, 'aquaculture', 'Input', 'Finfish_Netpens.shp')
         response = test_client.post(
-            '/vector_has_points', 
+            '/vector_may_have_points', 
             json={'vector_path': path})
-        has_points = json.loads(response.get_data(as_text=True))['has_points']
-        self.assertEqual(has_points, False)
+        may_have_points = json.loads(
+            response.get_data(as_text=True))['may_have_points']
+        self.assertEqual(may_have_points, False)
         # a vector with point geometries
         path = os.path.join(
             TEST_DATA_PATH, 'delineateit', 'input', 'outlets.shp')
         response = test_client.post(
-            '/vector_has_points', 
+            '/vector_may_have_points', 
             json={'vector_path': path})
-        has_points = json.loads(response.get_data(as_text=True))['has_points']
-        self.assertEqual(has_points, True)
+        may_have_points = json.loads(
+            response.get_data(as_text=True))['may_have_points']
+        self.assertEqual(may_have_points, True)
         # a non-vector file
         path = os.path.join(TEST_DATA_PATH, 'ndr', 'input', 'dem.tif')
         response = test_client.post(
-            '/vector_has_points', 
+            '/vector_may_have_points', 
             json={'vector_path': path})
-        has_points = json.loads(response.get_data(as_text=True))['has_points']
-        self.assertEqual(has_points, False)
+        may_have_points = json.loads(
+            response.get_data(as_text=True))['may_have_points']
+        self.assertEqual(may_have_points, False)
