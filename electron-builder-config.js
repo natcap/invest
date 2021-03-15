9b96fa98eae76c1874aa27fe457080a3dee0809d
davemfish <davemfish@gmail.com>
2021-03-15 11:43:25 -0700

    debug the workbench version string maker. #77.

--- electron-builder-config.js
+++ electron-builder-config.js
@@ -11,8 +11,9 @@ const EXT = OS === 'win32' ? 'exe' : 'dmg';
 const APP_ID = `NaturalCapitalProject.Invest.Workbench.${pkg.invest.version}`;
 
 // Uniquely identify the changeset we're building & packaging.
-const workbenchVersion = execFileSync('git', ['describe', '--tags']);
-console.log(`building version ${workbenchVersion}`);
+const workbenchVersion = execFileSync('git', ['describe', '--tags'])
+  .toString().trim();
+console.log(workbenchVersion);
 
 // productName controls the install dirname & app name
 // We might want to remove the workbench version from this name
