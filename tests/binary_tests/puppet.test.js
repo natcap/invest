f8cdc7aa9afa484087f954a954856a2d23be1a5e
emlys <esoth@stanford.edu>
2021-04-14 15:43:18 -0600

    no changes to test

--- tests/binary_tests/puppet.test.js
+++ tests/binary_tests/puppet.test.js
@@ -80,7 +80,7 @@ beforeAll(async () => {
 
   // get data about the remote debugging endpoint
   // so we don't make the next fetch too early
-  await new Promise((resolve) => setTimeout(resolve, 60000));
+  await new Promise((resolve) => setTimeout(resolve, 20000));
   const res = await fetch(`http://localhost:${PORT}/json/version`);
   const data = JSON.parse(await res.text());
   BROWSER = await puppeteer.connect({
