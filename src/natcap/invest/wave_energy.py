80de899ebcc3de0b43d285aae8c0bd85c7aabeec
emlys <esoth@stanford.edu>
2021-04-28 16:49:02 -0600

    fix wave energy and tests

--- src/natcap/invest/wave_energy.py
+++ src/natcap/invest/wave_energy.py
@@ -248,9 +248,6 @@ ARGS_SPEC = {
             "name": "Grid Connection Points Table"
         },
         "machine_econ_path": {
-            "validation_options": {
-                'required_fields': ['name', 'value', 'note'],
-            },
             "type": "csv",
             "rows": {
                 "CapMax": {
