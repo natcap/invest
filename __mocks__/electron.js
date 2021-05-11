03c59e50e9ca95f97a263363ce001150b40a5f2c
davemfish <davemfish@gmail.com>
2021-05-11 14:21:53 -0400

    more mocks of the electron main process APIs. #136.

--- __mocks__/electron.js
+++ __mocks__/electron.js
@@ -1,3 +1,14 @@
+/* Mocks of all parts of the electron API that we use.
+
+None of the electron APIs are available in a node env, even though
+electron is a node module. (Note that node_modules/electron/index.js only
+exports the filepath to the electron exe.)
+
+APIs are only available in an electron browser environment, but our tests
+run in node where all electron's exports are undefined. So we must mock,
+even for APIs that would otherwise seem okay to call during a test.
+*/
+
 import path from 'path';
 
 export const app = {
@@ -9,8 +20,16 @@ export const app = {
 export class BrowserWindow {
   constructor() {
     this.loadURL = jest.fn();
+    this.once = jest.fn();
+    this.on = jest.fn();
+    this.webContents = {
+      on: jest.fn(),
+      session: {
+        on: jest.fn()
+      }
+    };
   }
-};
+}
 
 export const dialog = {
   showOpenDialog: jest.fn(),
@@ -28,6 +47,11 @@ export const ipcRenderer = {
   on: jest.fn(),
   send: jest.fn(),
   invoke: jest.fn().mockImplementation(() => Promise.resolve()),
+};
+
+export const Menu = {
+  buildFromTemplate: jest.fn(),
+  setApplicationMenu: jest.fn(),
 };
 
 export const nativeTheme = {};
