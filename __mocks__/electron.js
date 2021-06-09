48c0ac54ead3221d044b7b04bd03b24149ee7e39
davemfish <davemfish@gmail.com>
2021-06-09 10:47:36 -0400

    cleaned up the electron IPC mocks. #136.

--- __mocks__/electron.js
+++ __mocks__/electron.js
@@ -16,29 +16,25 @@ import events from 'events';
 // TODO: given this functional IPC, some tests can probably
 // mock return values/implementations at a lower-level
 // e.g. electron shell API, etc.
-// class mockIPC extends events.EventEmitter {
-//   invoke(channel, ...args) {
-//     return new Promise((resolve, reject) => {
-//       this.once('send-to-main')
-
-//       this.emit('send-to-main', channel, ...args)
-//     });
-//   }
-// }
-const mockIPC = new events.EventEmitter();
-mockIPC.handle = jest.fn().mockReturnThis();
-mockIPC.handleOnce = jest.fn().mockReturnThis();
-mockIPC.send = (channel, ...args) => {
-  const event = {
-    reply: (channel, response) => {
-      mockIPC.emit(channel, {}, response);
-    }
-  };
-  mockIPC.emit(channel, event, ...args);
-};
-mockIPC.invoke = jest.fn().mockImplementation(() => Promise.resolve());
-export const ipcMain = mockIPC;
-export const ipcRenderer = mockIPC;
+class MockIPC extends events.EventEmitter {
+  constructor() {
+    super();
+    this.handle = jest.fn().mockReturnThis();
+    this.handleOnce = jest.fn().mockReturnThis();
+    this.invoke = jest.fn().mockImplementation(() => Promise.resolve());
+    this.send = (channel, ...args) => {
+      const event = {
+        reply: (channel, response) => {
+          this.emit(channel, {}, response);
+        }
+      };
+      this.emit(channel, event, ...args);
+    };
+  }
+}
+const electronMockIPC = new MockIPC();
+export const ipcMain = electronMockIPC;
+export const ipcRenderer = electronMockIPC;
 
 const mockApp = new events.EventEmitter();
 mockApp.getPath = jest.fn().mockImplementation(
@@ -57,7 +53,7 @@ export class BrowserWindow {
         on: jest.fn(),
       },
       send: (channel, ...args) => {
-        mockIPC.emit(channel, {}, ...args);
+        electronMockIPC.emit(channel, {}, ...args);
       },
       downloadURL: jest.fn(),
     };
