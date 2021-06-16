339d0584869727fa544ea7558b96c8eb2dfd306c
davemfish <davemfish@gmail.com>
2021-06-16 17:54:40 -0400

    running build code during devMode and using babel --watch to compile src on save. allows jsx and es6 features in index.js. #146.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-const { ipcRenderer } = require('electron'); // eslint-disable-line import/no-extraneous-dependencies
-
 const logger = window.Workbench.getLogger(__filename.split('/').slice(-1)[0]);
 const isDevMode = process.argv.includes('--dev');
 if (isDevMode) {
-  // in dev mode we can have babel transpile modules on import
-  require('@babel/register'); // eslint-disable-line import/no-extraneous-dependencies
   require('react-devtools');
 }
 
-const _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
-const react = _interopRequireDefault(require('react'));
-const reactDom = _interopRequireDefault(require('react-dom'));
-const app = require('./app');
+import { ipcRenderer } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './app';
 
 // Create a right-click menu
 // TODO: Not sure if Inspect Element should be available in production
@@ -23,18 +20,14 @@ window.addEventListener('contextmenu', (e) => {
   ipcRenderer.send('show-context-menu', rightClickPosition);
 });
 
-const render = async function render(isFirstRun) {
-  reactDom.default.render(
-    react.default.createElement(
-      app.default, {
-        isFirstRun: isFirstRun,
-      }
-    ),
+function render(isFirstRun) {
+  ReactDom.render(
+    <App isFirstRun/>,
     document.getElementById('App')
-  );
+  )
 };
 
 ipcRenderer.invoke('is-first-run')
   .then((response) => {
     render(response.isFirstRun);
-  });
+  })
