52ce4571528ae6b3755e2923526e051fd847b4e1
davemfish <davemfish@gmail.com>
2021-06-17 16:22:33 -0400

    wired up react-devtools as a chrome devtools extension instead of a standalone service. #146.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-const logger = window.Workbench.getLogger(__filename.split('/').slice(-1)[0]);
-const isDevMode = process.argv.includes('--dev');
-if (isDevMode) {
-  require('react-devtools');
-}
-
 import { ipcRenderer } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
 import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './app';
+
+const logger = window.Workbench.getLogger(__filename.split('/').slice(-1)[0]);
 
 // Create a right-click menu
 // TODO: Not sure if Inspect Element should be available in production
