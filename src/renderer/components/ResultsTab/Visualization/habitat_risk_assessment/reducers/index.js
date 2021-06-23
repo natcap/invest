0fdff85976c1781e602d465887d83c8092d02207
davemfish <davemfish@gmail.com>
2021-06-23 11:50:55 -0400

    removed files in the ResultsTab component that we are not using. #145 and #15.

--- src/renderer/components/ResultsTab/Visualization/habitat_risk_assessment/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import csvUrl from './reducer_csv_url';
-import vectorsOnMap from './reducer_vectors_on_map';
-import fileSuffix from './reducer_file_suffix';
-
-const rootReducer = combineReducers({ csvUrl, vectorsOnMap, fileSuffix });
-
-export default rootReducer;
