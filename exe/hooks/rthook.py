1b09927b969d184374d81ca3606e00581119a823
emlys <esoth@stanford.edu>
2021-07-26 11:52:30 -0600

    remove workaround for bug in gdal 3.3.0 now that it's fixed

--- exe/hooks/rthook.py
+++ exe/hooks/rthook.py
@@ -12,14 +12,3 @@ if platform.system() == 'Darwin':
     # See https://bugreports.qt.io/browse/QTBUG-87014
     # and https://github.com/natcap/invest/issues/384
     os.environ['QT_MAC_WANTS_LAYER'] = '1'
-
-elif platform.system() == 'Windows':
-    # Remove any paths from the PATH that don't exist
-    # This is necessary to prevent a FileNotFoundError when importing gdal
-    # with USE_PATH_FOR_GDAL_PYTHON=YES, which is needed to import gdal >=3.3.0
-    # https://github.com/OSGeo/gdal/issues/3898
-    filtered_paths = []
-    for path in os.environ['PATH'].split(';'):
-        if os.path.exists(path):
-            filtered_paths.append(path)
-    os.environ['PATH'] = ';'.join(filtered_paths)
