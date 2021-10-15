a21a4c7a1dba758be02dbf800c96a114b6b055f1
James Douglass <jdouglass@stanford.edu>
2021-10-15 00:08:47 -0500

    Manually adding pyd files from the osgeo package.
    RE:#665

--- exe/hooks/hook-osgeo.py
+++ exe/hooks/hook-osgeo.py
@@ -1,5 +1,8 @@
+import os.path
+import glob
+
 from PyInstaller.compat import is_win
-from PyInstaller.utils.hooks import collect_dynamic_libs
+from PyInstaller.utils.hooks import collect_dynamic_libs, get_package_paths
 
 if is_win:
     # GDAL appears to need `_gdal.cp38-win_amd64.pyd` located specifically in
@@ -10,3 +13,7 @@ if is_win:
     # This will take the dynamic libraries in osgeo and put them into osgeo,
     # relative to the binaries directory.
     binaries = collect_dynamic_libs('osgeo', 'osgeo')
+    pkg_base, pkg_dir = get_package_paths('osgeo')
+    for pyd_file in glob.glob(os.path.join(pkg_dir, '*.pyd')):
+        binaries.append(
+            (pyd_file, os.path.join('osgeo', os.path.basename(pyd_file))))
