fab2954d33f99d2ef01bc7b0076dab7cf7fc0bb6
davemfish <davemfish@gmail.com>
2021-10-28 16:03:03 -0400

    have to turn off the jest override in order to see a proper stracktrace.

--- tests/jest.overrides.js
+++ tests/jest.overrides.js
@@ -2,11 +2,11 @@ const crypto = require('crypto');
 
 // Cause tests to fail on console.error messages
 // Taken from https://stackoverflow.com/questions/28615293/is-there-a-jest-config-that-will-fail-tests-on-console-warn/50584643#50584643
-let error = console.error;
-console.error = function (message) {
-  error.apply(console, arguments); // keep default behaviour
-  throw (message instanceof Error ? message : new Error(message));
-};
+// let error = console.error;
+// console.error = function (message) {
+//   error.apply(console, arguments); // keep default behaviour
+//   throw (message instanceof Error ? message : new Error(message));
+// };
 
 // I've found this obfuscates error messages sometimes:
 /* With this override:
