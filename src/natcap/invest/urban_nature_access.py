e381c1f9bcdbb57b19eeda15af3e41a42ef7064f
James Douglass <jdouglass@stanford.edu>
2021-11-15 11:09:26 -0800

    Implementing population resampling.
    The function provided is a first stab at resampling/reprojecting a
    population raster by way of converting to a population density, then
    reprojecting and converting back to a population count.  Correctness has
    yet to be determined, but this way we should pretty reasonably be
    preserving the population counts. RE:#722

--- src/natcap/invest/urban_nature_access.py
+++ src/natcap/invest/urban_nature_access.py
@@ -3,6 +3,9 @@ import logging
 
 import pygeoprocessing
 import taskgraph
+from osgeo import gdal
+from osgeo import osr
+import numpy
 
 from . import validation
 from . import spec_utils
@@ -11,6 +14,8 @@ from .spec_utils import u
 from .. import MODEL_METADATA
 
 LOGGER = logging.getLogger(__name__)
+UINT32_NODATA = numpy.finfo(numpy.uint32).max
+FLOAT32_NODATA = numpy.finfo(numpy.float32).max
 ARGS_SPEC = {
     'model_name': MODEL_METADATA['urban_nature_access'].model_title,
     'pyname': MODEL_METADATA['urban_nature_access'].pyname,
@@ -42,7 +47,10 @@ ARGS_SPEC = {
         'population_raster_path': {
             'type': 'raster',
             'name': 'population raster',
-            'bands': {1: {'type': 'float'}},
+            'bands': {
+                1: {'type': 'number', 'units': u.none}
+            },
+            'projected': True,
             'about': "",  # TODO,
         },
         'admin_unit_vector_path': {
@@ -71,6 +79,7 @@ ARGS_SPEC = {
 
 _OUTPUT_BASE_FILES = {}
 _INTERMEDIATE_BASE_FILES = {}
+
 
 def execute(args):
     """Urban Nature Access.
@@ -134,7 +143,114 @@ def execute(args):
         n_workers = -1  # Synchronous execution
     graph = taskgraph.TaskGraph(work_token_dir, n_workers)
 
+    graph.close()
+    graph.join()
     LOGGER.info('Finished Urban Nature Access Model')
+
+
+def _resample_population_raster(
+        source_population_raster_path, target_population_raster_path,
+        target_pixel_size, target_bb, target_projection_wkt, working_dir):
+    # Assumes population raster is linearly projected
+    # Reasoning: the total population of a pixel is great, but we need to be
+    # able to convert that pixel size without losing or gaining people due to
+    # resampling.
+    #
+    # Approach:
+    # * Assume population raster is linearly projected.
+    # * raster_calculator: convert population raster to density of people/pixel
+    # * align_and_resize_raster_stack: resample (bilinear should be fine) to
+    #   LULC raster's alignment/resolution
+    # * raster_calculator: Reconvert the population raster from density to
+    #   population
+
+    # Option 1: I could do each task right in the execute function
+    # Option 2: I could do the 2 raster_calculator calls and 1 warp_raster call
+    #     right here in this function
+
+    population_raster_info = pygeoprocessing.get_raster_info(
+        source_population_raster_path)
+    pixel_area = numpy.multiply(population_raster_info['pixel_size'])
+    population_nodata = population_raster_info['nodata'][0]
+
+    population_srs = osr.SpatialReference()
+    population_srs.ImportFromWKT(population_raster_info['projection_wkt'])
+
+    # Convert population pixel area to square km
+    population_pixel_area = (
+        pixel_area * population_srs.GetLinearUnits()) / 1e6
+
+    def _convert_population_to_density(population):
+        """Convert population counts to population per square km.
+
+        Args:
+            population (numpy.array): A numpy array where pixel values
+                represent the number of people who reside in a pixel.
+
+        Returns:
+            """
+        out_array = numpy.full(
+            population.shape, FLOAT32_NODATA, dtype=numpy.float32)
+
+        valid_mask = slice()
+        if population_nodata is not None:
+            valid_mask = ~numpy.isclose(population, population_nodata)
+
+        out_array[valid_mask] = population[valid_mask] / population_pixel_area
+        return out_array
+
+    # Step 1: convert the population raster to population density per sq. km
+    density_raster_path = os.path.join(working_dir, 'pop_density.tif')
+    pygeoprocessing.raster_calculator(
+        [(source_population_raster_path, 1)],
+        _convert_population_to_density,
+        density_raster_path, gdal.GDT_Float32, FLOAT32_NODATA)
+
+    # Step 2: align to the LULC
+    warped_density_path = os.path.join(working_dir, 'warped_density.tif')
+    pygeoprocessing.warp_raster(
+        density_raster_path,
+        target_pixel_size=target_pixel_size,
+        target_raster_path=warped_density_path,
+        resample_method='bilinear',
+        target_bb=target_bb,
+        target_projection_wkt=target_projection_wkt)
+
+    # Step 3: convert the warped population raster back from density to the
+    # population per pixel
+    target_srs = osr.SpatialReference()
+    target_srs.ImportFromWKT(target_projection_wkt)
+    # Calculate target pixel area in km to match above
+    target_pixel_area = (
+        numpy.multiply(target_pixel_size) * target_srs.GetLinearUnits()) / 1e6
+
+    def _convert_density_to_population(density):
+        """Convert a population density raster back to population counts.
+
+        Args:
+            density (numpy.array): An array of the population density per
+                square kilometer.
+
+        Returns:
+            A ``numpy.array`` of the population counts given the target pixel
+            size of the output raster."""
+        # We're using a float32 array here because doing these unit
+        # conversions is likely to end up with partial people spread out
+        # between multiple pixels.  So it's preserving an unrealistic degree of
+        # precision, but that's probably OK because pixels are imprecise
+        # measures anyways.
+        out_array = numpy.full(
+            density.shape, FLOAT32_NODATA, dtype=numpy.float32)
+
+        # We already know that the nodata value is FLOAT32_NODATA
+        valid_mask = ~numpy.isclose(density, FLOAT32_NODATA)
+        out_array[valid_mask] = density[valid_mask] * target_pixel_area
+        return out_array
+
+    pygeoprocessing.raster_calculator(
+        [(warped_density_path, 1)],
+        _convert_density_to_population,
+        target_population_raster_path, gdal.GDT_Float32, FLOAT32_NODATA)
 
 
 def validate(args, limit_to=None):
--- tests/test_urban_nature_access.py
+++ tests/test_urban_nature_access.py
@@ -3,6 +3,16 @@
 import unittest
 import tempfile
 import shutil
+import os
+
+import pygeoprocessing
+import numpy
+from osgeo import gdal
+from osgeo import osr
+
+_DEFAULT_ORIGIN = (444720, 3751320)
+_DEFAULT_PIXEL_SIZE = (30, -30)
+_DEFAULT_EPSG = 3116
 
 
 class UNATests(unittest.TestCase):
@@ -17,3 +27,22 @@ class UNATests(unittest.TestCase):
     def tearDown(self):
         """Override tearDown function to remove temporary directory."""
         shutil.rmtree(self.workspace_dir)
+
+    def test_resample_population_raster(self):
+        """UNA: Test population raster resampling."""
+
+        source_population_raster_path = os.path.join(
+            self.workspace_dir, 'population.tif')
+        population_pixel_size = (90, -90)
+        population_array_shape = (10, 10)
+        population_array = numpy.full(
+            population_array_shape, 100, dtype=numpy.uint32)
+        population_srs = osr.SpatialReference()
+        population_srs.ImportFromEPSG(_DEFAULT_EPSG)
+        pygeoprocessing.numpy_array_to_raster(
+            base_array=population_array,
+            target_nodata=-1,
+            pixel_size=population_pixel_size,
+            origin=_DEFAULT_ORIGIN,
+            projection_wkt=population_srs.ExportToWkt(),
+            target_path=source_population_raster_path)
