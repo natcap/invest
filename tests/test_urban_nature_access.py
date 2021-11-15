90955c93e51296d0723fbfeb12338cdcbd75bc03
James Douglass <jdouglass@stanford.edu>
2021-11-15 09:08:00 -0800

    Adding a skeleton for UNA model tests.
    RE#722

---
+++ tests/test_urban_nature_access.py
@@ -0,0 +1,19 @@
+# coding=UTF-8
+"""Tests for the Urban Nature Access Model."""
+import unittest
+import tempfile
+import shutil
+
+
+class UNATests(unittest.TestCase):
+    """Tests for the Urban Nature Access Model."""
+
+    def setUp(self):
+        """Override setUp function to create temp workspace directory."""
+        # this lets us delete the workspace after its done no matter the
+        # the rest result
+        self.workspace_dir = tempfile.mkdtemp(suffix='\U0001f60e')  # smiley
+
+    def tearDown(self):
+        """Override tearDown function to remove temporary directory."""
+        shutil.rmtree(self.workspace_dir)
