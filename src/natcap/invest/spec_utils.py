c9acc9a04884f041004d304ad33850b63fa8b811
emlys <esoth@stanford.edu>
2021-11-17 10:15:22 -0800

    initialize UnitRegistry with ignore warnings

--- src/natcap/invest/spec_utils.py
+++ src/natcap/invest/spec_utils.py
@@ -6,8 +6,8 @@ import pint
 
 # the same unit registry instance should be shared across everything
 # load from custom unit defintions file
-u = pint.UnitRegistry()
-u._on_redefinition = 'ignore'  # don't raise warnings when redefining units
+# don't raise warnings when redefining units
+u = pint.UnitRegistry(on_redefinition='ignore')
 u.load_definitions(os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'unit_definitions.txt'))
 
