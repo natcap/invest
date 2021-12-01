95815eeb8ad88207f73b588ef7287b7fa4f5d493
davemfish <davemfish@gmail.com>
2021-12-01 11:15:08 -0500

    removed models from metadata dict; cli entrypoints. #719.

--- src/natcap/invest/__init__.py
+++ src/natcap/invest/__init__.py
@@ -77,24 +77,6 @@ MODEL_METADATA = {
         gui='delineateit.Delineateit',
         userguide='delineateit.html',
         aliases=()),
-    'finfish_aquaculture': _MODELMETA(
-        model_title='Finfish Aquaculture',
-        pyname='natcap.invest.finfish_aquaculture.finfish_aquaculture',
-        gui='finfish.FinfishAquaculture',
-        userguide='marine_fish.html',
-        aliases=()),
-    'fisheries': _MODELMETA(
-        model_title='Fisheries',
-        pyname='natcap.invest.fisheries.fisheries',
-        gui='fisheries.Fisheries',
-        userguide='fisheries.html',
-        aliases=()),
-    'fisheries_hst': _MODELMETA(
-        model_title='Fisheries Habitat Scenario Tool',
-        pyname='natcap.invest.fisheries.fisheries_hst',
-        gui='fisheries.FisheriesHST',
-        userguide='fisheries.html',
-        aliases=()),
     'forest_carbon_edge_effect': _MODELMETA(
         model_title='Forest Carbon Edge Effect',
         pyname='natcap.invest.forest_carbon_edge_effect',
