5cd0de6fe0913cfa3ceb89eedd88a56a98bca1d0
emlys <esoth@stanford.edu>
2022-01-07 10:59:28 -0800

    refactor message catalog reading

--- src/main/setLanguage.js
+++ src/main/setLanguage.js
@@ -9,39 +9,40 @@ const i18n = new gettext_js();
 
 var gettextParser = require("gettext-parser");
 
+/** Read PO file into gettext.js formatted message catalog object. */
+function readMessageCatalog(messageCatalogPath) {
+  const input = fs.readFileSync(messageCatalogPath);
+  // parse PO file into an object in gettext-parser format
+  // https://github.com/smhg/gettext-parser#data-structure-of-parsed-mopo-files
+  const raw_po = gettextParser.po.parse(input)
+
+  // convert from gettext-parser format to gettext.js format
+  // https://github.com/guillaumepotier/gettext.js#required-json-format
+  const formatted_po = {};
+  formatted_po[''] = {};
+  formatted_po['']['language'] = raw_po['headers']['language']
+  formatted_po['']['plural-forms'] = raw_po['headers']['plural-forms']
+  delete raw_po['translations']['']['']
+  for (const msgid in raw_po['translations']['']) {
+    if (raw_po['translations'][''][msgid]['msgstr'].length === 1) {
+      formatted_po[msgid] = raw_po['translations'][''][msgid]['msgstr'][0]
+    } else {
+      formatted_po[msgid] = raw_po['translations'][''][msgid]['msgstr']
+    }
+  };
+}
+
+/** Load message catalogs for each language so they're available to i18n. */
 async function loadMessageCatalogs() {
   // load each message catalog JSON file into an object
   // for easy access when we switch languages
-
-
   fs.readdir(
     `${__dirname}/../static/internationalization/locales`,
     async function (err, languages) {
       if (languages) {
         for (const language of languages) {
           const messageCatalogPath = `${__dirname}/../static/internationalization/locales/${language}/LC_MESSAGES/messages.po`;
-
-          const input = fs.readFileSync(messageCatalogPath);
-          const raw_po = gettextParser.po.parse(input)
-          delete raw_po['translations']['']['']
-          console.log(raw_po)
-
-          // format in the way that i18n expects
-          const formatted_po = {};
-          formatted_po[''] = {};
-          formatted_po['']['language'] = raw_po['headers']['language']
-          formatted_po['']['plural-forms'] = raw_po['headers']['plural-forms']
-
-          for (const msgid in raw_po['translations']['']) {
-            if (raw_po['translations'][''][msgid]['msgstr'].length === 1) {
-              formatted_po[msgid] = raw_po['translations'][''][msgid]['msgstr'][0]
-            } else {
-              formatted_po[msgid] = raw_po['translations'][''][msgid]['msgstr']
-            }
-          };
-
-          console.log(formatted_po);
-          i18n.loadJSON(formatted_po, 'messages');
+          i18n.loadJSON(readMessageCatalog(messageCatalogPath), 'messages');
         }
       }
     }
