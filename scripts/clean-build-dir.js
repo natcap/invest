58d005c795d33019e4f039301833ac9512f93250
davemfish <davemfish@gmail.com>
2022-01-27 16:33:56 -0500

    build script no longer needs to manage invest binaries in the workbench build dir.

--- scripts/clean-build-dir.js
+++ scripts/clean-build-dir.js
@@ -1,8 +1,6 @@
 /* We can't rely on babel to clean it's output directory every time
-it compiles because,
-1. There's a bug https://github.com/babel/babel/issues/9293
-2. We have the special build/invest dir that is outside the scope
-   of babel and that should persist.
+it compiles because there's a bug
+https://github.com/babel/babel/issues/9293
 */
 
 const fs = require('fs-extra');
@@ -13,18 +11,10 @@ const BUILD_DIR = 'build';
 
 /** Remove all the files created during build()
  *
- * Do not remove other things in the build/ folder such as
- * invest binaries, which are not created during build().
  */
 function clean() {
   const files = glob.sync(
-    BUILD_DIR.concat(path.sep, '**', path.sep, '*'),
-    {
-      ignore: [
-        path.join(BUILD_DIR, 'invest/**'),
-        path.join(BUILD_DIR, 'pyi-build/**'),
-      ]
-    }
+    BUILD_DIR.concat(path.sep, '**', path.sep, '*')
   );
   files.forEach((file) => {
     try {
