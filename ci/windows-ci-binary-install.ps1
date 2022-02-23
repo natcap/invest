9de938fdcb0ce91a0e3503473415e4de377876e5
emlys <esoth@stanford.edu>
2022-02-23 10:29:36 -0800

    again

--- ci/windows-ci-binary-install.ps1
+++ ci/windows-ci-binary-install.ps1
@@ -1,13 +1,21 @@
-# This is an install script needed for our binary build.
+# This is an install script needed for our binary build on AppVeyor.
 #
-# It assumes that the applications choco and 7zip are available and on the PATH.
-
+# It assumes that the applications choco, wget, 7zip, unzip and curl are available
+# and on the PATH.  It also requires that these environment variables are defined:
+#
+#       GOOGLE_SERVICE_ACC_KEY - the base64-encoded version of the service account key to use
+#       STANFORD_CERT_KEY_PASS - the string password to use for our code signing cert
+#       PYTHON - the directory of the python installation to use.  Packages for this build
+#                are already assumed to be installed and available in this installation.
+#
 # NOTE: it turns out that `wget` is an alias for the powershell command `Invoke-WebRequest`,
 # which I've made a point of using here instead of the actual wget. See https://superuser.com/a/693179
 
 choco install zip unzip
 $env:PATH += ";C:\ProgramData\chocolatey\bin"
-refreshenv # Choco-provided command to reload environment variables
+
+# Choco-provided command to reload environment variables
+refreshenv
 
 # Download and install NSIS plugins to their correct places.
 Write-Host "Downloading and extracting NSIS"
@@ -25,3 +33,5 @@ Invoke-WebRequest https://storage.googleapis.com/natcap-build-dependencies/windo
 & 7z e NsisMultiUser.zip -o"C:\Program Files (x86)\NSIS\Include" Include\NsisMultiUserLang.nsh
 & 7z e NsisMultiUser.zip -o"C:\Program Files (x86)\NSIS\Include" Include\StdUtils.nsh
 & 7z e NsisMultiUser.zip -o"C:\Program Files (x86)\NSIS\Include" Include\UAC.nsh
+& 7z e NsisMultiUser.zip -o"C:\Program Files (x86)\NSIS\Include" Demos\Common\Utils.nsh
+& 7z e NsisMultiUser.zip -o"C:\Program Files (x86)\NSIS\Include" Demos\Common\un.Utils.nsh
