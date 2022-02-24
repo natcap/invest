63ff4fc931a698cb9e2363cc3a8b4931406b1504
davemfish <davemfish@gmail.com>
2022-02-24 08:55:45 -0500

    jest setup script needs to mirror preload script. #870.

--- workbench/tests/jest.overrides.js
+++ workbench/tests/jest.overrides.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const { ipcRenderer } = require('electron');
 
 // Cause tests to fail on console.error messages
 // Taken from https://stackoverflow.com/questions/28615293/is-there-a-jest-config-that-will-fail-tests-on-console-warn/50584643#50584643
@@ -55,7 +56,8 @@ if (global.window) {
          ${JSON.stringify(y)}
          ${JSON.stringify(z)}`
       ).digest('hex');
-    }
+    },
+    ipcRenderer: ipcRenderer,
   };
 
   // jsdom does not implement window.crypto
