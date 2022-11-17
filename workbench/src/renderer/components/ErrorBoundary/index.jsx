a168f0693869daebb0d0515db463d6a242aa0c79
davemfish <davemfish@gmail.com>
2022-11-17 09:50:22 -0500

    added ErrorBoundary component wrapping entire App. #1119

---
+++ workbench/src/renderer/components/ErrorBoundary/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const logger = window.Workbench.getLogger('ErrorBoundary');
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    logger.error(error);
+    logger.error(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+    return this.props.children;
+  }
+}
