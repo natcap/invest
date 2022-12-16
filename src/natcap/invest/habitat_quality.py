4357387743376a7a883d6e495026e5813ae1b1b1
Doug <dcdenu4@gmail.com>
2022-12-16 09:43:25 -0500

    I. #1104 initial setup for decay comparison

--- src/natcap/invest/habitat_quality.py
+++ src/natcap/invest/habitat_quality.py
@@ -1162,3 +1162,190 @@ def validate(args, limit_to=None):
                 invalid_keys.add('threats_table_path')
 
     return validation_warnings
+
+def _exponential_decay_kernel_raster(expected_distance, kernel_filepath):
+    """Create a raster-based exponential decay kernel.
+
+    The raster created will be a tiled GeoTiff, with 256x256 memory blocks.
+
+    Args:
+        expected_distance (int or float): The distance (in pixels) of the
+            kernel's radius, the distance at which the value of the decay
+            function is equal to `1/e`.
+        kernel_filepath (string): The path to the file on disk where this
+            kernel should be stored.  If this file exists, it will be
+            overwritten.
+
+    Returns:
+        None
+    """
+    max_distance = expected_distance * 5
+    kernel_size = int(numpy.round(max_distance * 2 + 1))
+
+    driver = gdal.GetDriverByName('GTiff')
+    kernel_dataset = driver.Create(
+        kernel_filepath.encode('utf-8'), kernel_size, kernel_size, 1,
+        gdal.GDT_Float32, options=[
+            'BIGTIFF=IF_SAFER', 'TILED=YES', 'BLOCKXSIZE=256',
+            'BLOCKYSIZE=256'])
+
+    # Make some kind of geotransform, it doesn't matter what but
+    # will make GIS libraries behave better if it's all defined
+    kernel_dataset.SetGeoTransform([0, 1, 0, 0, 0, -1])
+    srs = osr.SpatialReference()
+    srs.SetWellKnownGeogCS('WGS84')
+    kernel_dataset.SetProjection(srs.ExportToWkt())
+
+    kernel_band = kernel_dataset.GetRasterBand(1)
+    kernel_band.SetNoDataValue(-9999)
+
+    cols_per_block, rows_per_block = kernel_band.GetBlockSize()
+
+    n_cols = kernel_dataset.RasterXSize
+    n_rows = kernel_dataset.RasterYSize
+
+    n_col_blocks = int(math.ceil(n_cols / float(cols_per_block)))
+    n_row_blocks = int(math.ceil(n_rows / float(rows_per_block)))
+
+    integration = 0.0
+    for row_block_index in range(n_row_blocks):
+        row_offset = row_block_index * rows_per_block
+        row_block_width = n_rows - row_offset
+        if row_block_width > rows_per_block:
+            row_block_width = rows_per_block
+
+        for col_block_index in range(n_col_blocks):
+            col_offset = col_block_index * cols_per_block
+            col_block_width = n_cols - col_offset
+            if col_block_width > cols_per_block:
+                col_block_width = cols_per_block
+
+            # Numpy creates index rasters as ints by default, which sometimes
+            # creates problems on 32-bit builds when we try to add Int32
+            # matrices to float64 matrices.
+            row_indices, col_indices = numpy.indices((row_block_width,
+                                                      col_block_width),
+                                                     dtype=float)
+
+            row_indices += float(row_offset - max_distance)
+            col_indices += float(col_offset - max_distance)
+
+            kernel_index_distances = numpy.hypot(
+                row_indices, col_indices)
+            kernel = numpy.where(
+                kernel_index_distances > max_distance, 0.0,
+                numpy.exp(-kernel_index_distances / expected_distance))
+
+            kernel_band.WriteArray(kernel, xoff=col_offset,
+                                   yoff=row_offset)
+
+    # Need to flush the kernel's cache to disk before opening up a new Dataset
+    # object in interblocks()
+    kernel_band.FlushCache()
+    kernel_dataset.FlushCache()
+
+    for block_data in pygeoprocessing.iterblocks(
+            (kernel_filepath, 1), offset_only=True):
+        kernel_block = kernel_band.ReadAsArray(**block_data)
+        kernel_block /= integration
+        kernel_band.WriteArray(kernel_block, xoff=block_data['xoff'],
+                               yoff=block_data['yoff'])
+
+    kernel_band.FlushCache()
+    kernel_dataset.FlushCache()
+    kernel_band = None
+    kernel_dataset = None
+
+def _exponential_decay_scalar_kernel_raster(expected_distance, kernel_filepath):
+    """Create a raster-based exponential decay kernel.
+
+    The raster created will be a tiled GeoTiff, with 256x256 memory blocks.
+
+    Args:
+        expected_distance (int or float): The distance (in pixels) of the
+            kernel's radius, the distance at which the value of the decay
+            function is equal to `1/e`.
+        kernel_filepath (string): The path to the file on disk where this
+            kernel should be stored.  If this file exists, it will be
+            overwritten.
+
+    Returns:
+        None
+    """
+    max_distance = expected_distance * 5
+    kernel_size = int(numpy.round(max_distance * 2 + 1))
+
+    driver = gdal.GetDriverByName('GTiff')
+    kernel_dataset = driver.Create(
+        kernel_filepath.encode('utf-8'), kernel_size, kernel_size, 1,
+        gdal.GDT_Float32, options=[
+            'BIGTIFF=IF_SAFER', 'TILED=YES', 'BLOCKXSIZE=256',
+            'BLOCKYSIZE=256'])
+
+    # Make some kind of geotransform, it doesn't matter what but
+    # will make GIS libraries behave better if it's all defined
+    kernel_dataset.SetGeoTransform([0, 1, 0, 0, 0, -1])
+    srs = osr.SpatialReference()
+    srs.SetWellKnownGeogCS('WGS84')
+    kernel_dataset.SetProjection(srs.ExportToWkt())
+
+    kernel_band = kernel_dataset.GetRasterBand(1)
+    kernel_band.SetNoDataValue(-9999)
+
+    cols_per_block, rows_per_block = kernel_band.GetBlockSize()
+
+    n_cols = kernel_dataset.RasterXSize
+    n_rows = kernel_dataset.RasterYSize
+
+    n_col_blocks = int(math.ceil(n_cols / float(cols_per_block)))
+    n_row_blocks = int(math.ceil(n_rows / float(rows_per_block)))
+
+    integration = 0.0
+    for row_block_index in range(n_row_blocks):
+        row_offset = row_block_index * rows_per_block
+        row_block_width = n_rows - row_offset
+        if row_block_width > rows_per_block:
+            row_block_width = rows_per_block
+
+        for col_block_index in range(n_col_blocks):
+            col_offset = col_block_index * cols_per_block
+            col_block_width = n_cols - col_offset
+            if col_block_width > cols_per_block:
+                col_block_width = cols_per_block
+
+            # Numpy creates index rasters as ints by default, which sometimes
+            # creates problems on 32-bit builds when we try to add Int32
+            # matrices to float64 matrices.
+            row_indices, col_indices = numpy.indices((row_block_width,
+                                                      col_block_width),
+                                                     dtype=float)
+
+            row_indices += float(row_offset - max_distance)
+            col_indices += float(col_offset - max_distance)
+
+            kernel_index_distances = numpy.hypot(
+                row_indices, col_indices)
+            kernel = numpy.where(
+                kernel_index_distances > max_distance, 0.0,
+                numpy.exp(
+                    (-kernel_index_distances * 2.99) / expected_distance))
+
+            kernel_band.WriteArray(kernel, xoff=col_offset,
+                                   yoff=row_offset)
+
+    # Need to flush the kernel's cache to disk before opening up a new Dataset
+    # object in interblocks()
+    kernel_band.FlushCache()
+    kernel_dataset.FlushCache()
+
+    for block_data in pygeoprocessing.iterblocks(
+            (kernel_filepath, 1), offset_only=True):
+        kernel_block = kernel_band.ReadAsArray(**block_data)
+        kernel_block /= integration
+        kernel_band.WriteArray(kernel_block, xoff=block_data['xoff'],
+                               yoff=block_data['yoff'])
+
+    kernel_band.FlushCache()
+    kernel_dataset.FlushCache()
+    kernel_band = None
+    kernel_dataset = None
