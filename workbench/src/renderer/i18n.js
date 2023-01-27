1339b8c060d1703e700ed2539a55870b91b0cdc6
Emily Soth <esoth@stanford.edu>
2023-01-26 18:13:54 -0800

    load es and zh messages into i18next from js modules

--- workbench/src/renderer/i18n.js
+++ workbench/src/renderer/i18n.js
@@ -1,20 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import es_messages from './i18n/es.js';
+import zh_messages from './i18n/zh.js';
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-  en: {
-    translation: {
-      "Welcome to React": "Welcome to React and react-i18next"
-    }
+  es: {
+    translation: es_messages
   },
-  fr: {
-    translation: {
-      "Welcome to React": "Bienvenue à React et react-i18next"
-    }
-  }
+  zh: {
+    translation: zh_messages
+  },
 };
 
 i18n
@@ -24,7 +23,6 @@ i18n
     lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
-
     interpolation: {
       escapeValue: false // react already safes from xss
     },
