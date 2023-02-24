eb7ec842b396b285f9acfc77775f6beed23eb597
Emily Soth <esoth@stanford.edu>
2023-02-23 16:06:01 -0800

    fix i18n import path in menubar components

--- workbench/src/renderer/menubar/about.jsx
+++ workbench/src/renderer/menubar/about.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-import i18n from '../../shared/i18n';
 import { Translation } from 'react-i18next';
+
+import i18n from '../i18n/i18n';
 import { handleClickExternalURL } from './handlers';
 import { getSettingsValue } from '../components/SettingsModal/SettingsStorage';
 import { ipcMainChannels } from '../../main/ipcMainChannels';
