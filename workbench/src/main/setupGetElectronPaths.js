995564be1292b7a25d2954a82fe884f793093469
davemfish <davemfish@gmail.com>
2023-06-12 15:33:54 -0400

    checkin new file. #1223

---
+++ workbench/src/main/setupGetElectronPaths.js
@@ -0,0 +1,13 @@
+import { app, ipcMain } from 'electron';
+
+import { ipcMainChannels } from './ipcMainChannels';
+
+export default function setupGetElectronPaths() {
+  ipcMain.on(ipcMainChannels.GET_ELECTRON_PATHS, (event) => {
+    event.returnValue = {
+      resourcesPath: process.resourcesPath,
+      userDataPath: app.getPath('userData')
+    };
+    // return data;
+  });
+}
