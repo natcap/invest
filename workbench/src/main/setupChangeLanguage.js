15580adc34831f81f88a87e8283d27efa60e7ac8
Emily Soth <esoth@stanford.edu>
2023-06-16 10:58:50 -0700

    read and expose language setting through ipc #1168

--- workbench/src/main/setupChangeLanguage.js
+++ workbench/src/main/setupChangeLanguage.js
@@ -8,6 +8,11 @@ const logger = getLogger(__filename.split('/').slice(-1)[0]);
 const store = new Store();
 
 export default function setupChangeLanguage() {
+  ipcMain.on(ipcMainChannels.GET_LANGUAGE, (event) => {
+    // default to en if no language setting exists
+    event.returnValue = store.get('language', 'en');
+  });
+
   ipcMain.handle(
     ipcMainChannels.CHANGE_LANGUAGE,
     (e, languageCode) => {
