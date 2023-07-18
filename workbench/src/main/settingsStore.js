6baa6a6687160c5d6e2cc9d35054d20e45c88a68
davemfish <davemfish@gmail.com>
2023-07-18 15:57:25 -0400

    define a schema for settings store

--- workbench/src/main/settingsStore.js
+++ workbench/src/main/settingsStore.js
@@ -7,11 +7,29 @@ const defaults = {
   nWorkers: '-1',
   taskgraphLoggingLevel: 'INFO',
   loggingLevel: 'INFO',
-  sampleDataDir: '',
   language: 'en'
 };
 
-export const settingsStore = new Store({ defaults: defaults });
+const schema = {
+  nWorkers: {
+    type: 'string',
+    default: '-1',
+  },
+  taskgraphLoggingLevel: {
+    enum: ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+    default: 'INFO',
+  },
+  loggingLevel: {
+    enum: ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+    default: 'INFO',
+  },
+  language: {
+    enum: ['en', 'es', 'zh'],
+    default: 'en',
+  },
+};
+
+export const settingsStore = new Store({ schema });
 
 export function setupSettingsHandlers() {
   ipcMain.handle(ipcMainChannels.GET_SETTING, (event, key) => settingsStore.get(key));
