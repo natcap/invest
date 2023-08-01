4ee643fc7a9a9608e84cc41c13cc95f395da503c
davemfish <davemfish@gmail.com>
2023-08-01 15:33:39 -0400

    updated tests for new settings store

--- workbench/tests/main/settings.test.js
+++ workbench/tests/main/settings.test.js
@@ -18,26 +18,26 @@ test('an empty store initializes to defaults', () => {
 test('invalid items are reset, valid items are unchanged', () => {
   const data = defaults;
   data.nWorkers = '5'; // valid, but not default
-  data.taskgraphLoggingLevel = 'ERROR'; // valid, but no default
+  data.taskgraphLoggingLevel = 'ERROR'; // valid, but not default
   data.loggingLevel = 'FOO'; // wrong value
   data.language = 1; // wrong type
 
   const store = initStore(data);
 
-  // invalid should be reset to defaults
+  // invalid: should be reset to defaults
   expect(store.get('loggingLevel')).toBe(defaults.loggingLevel);
   expect(store.get('language')).toBe(defaults.language);
 
-  // valid should be not be reset to defaults
+  // valid: should be not be reset to defaults
   expect(store.get('taskgraphLoggingLevel')).toBe(data.taskgraphLoggingLevel);
   expect(store.get('nWorkers')).toBe(data.nWorkers);
 });
 
-test.only('store contains properties not present in schema', () => {
+test('properties not present in schema are untouched during validation', () => {
   const data = defaults;
   data.foo = 'bar';
 
   const store = initStore(data);
 
-  expect(store.store).toEqual(defaults);
+  expect(store.get('foo')).toEqual(data.foo);
 });
