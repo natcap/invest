e1b4944066fdeb62d205bba0a63b1cc35ce898e4
Emily Soth <esoth@stanford.edu>
2023-08-23 15:14:32 -0700

    add output specs for SWY outputs that were missing them

--- src/natcap/invest/seasonal_water_yield/seasonal_water_yield.py
+++ src/natcap/invest/seasonal_water_yield/seasonal_water_yield.py
@@ -413,6 +413,58 @@ MODEL_SPEC = {
                     "bands": {1: {
                         "type": "integer"
                     }}
+                },
+                'Si.tif': {
+                    "about": gettext("Map of the S_i factor derived from CN"),
+                    "bands": {1: {"type": "number", "units": u.inch}}
+                },
+                'lulc_aligned.tif': {
+                    "about": gettext("Copy of LULC input, aligned and clipped "
+                                     "to match the other spatial inputs"),
+                    "bands": {1: {"type": "integer"}}
+                },
+                'dem_aligned.tif': {
+                    "about": gettext("Copy of DEM input, aligned and clipped "
+                                     "to match the other spatial inputs"),
+                    "bands": {1: {"type": "number", "units": u.meter}}
+                },
+                'pit_filled_dem.tif': {
+                    "about": gettext("Pit filled DEM"),
+                    "bands": {1: {"type": "number", "units": u.meter}}
+                },
+                'soil_group_aligned.tif': {
+                    "about": gettext("Copy of soil groups input, aligned and "
+                                     "clipped to match the other spatial inputs"),
+                    "bands": {1: {"type": "integer"}}
+                },
+                'flow_accum.tif': spec_utils.FLOW_ACCUMULATION,
+                'prcp_a[MONTH].tif': {
+                    "bands": {1: {"type": "number", "units": u.millimeter/u.year}},
+                    "about": gettext("Monthly precipitation rasters, aligned and "
+                                     "clipped to match the other spatial inputs")
+                },
+                'n_events[MONTH].tif': {
+                    "about": gettext("Map of monthly rain events"),
+                    "bands": {1: {"type": "integer"}}
+                },
+                'et0_a[MONTH].tif': {
+                    "bands": {1: {"type": "number", "units": u.millimeter}},
+                    "about": gettext("Monthly ET0 rasters, aligned and "
+                                     "clipped to match the other spatial inputs")
+                },
+                'kc_[MONTH].tif': {
+                    "about": gettext("Map of monthly KC values"),
+                    "bands": {1: {"type": "number", "units": u.none}}
+                },
+                'l_aligned.tif': {
+                    "about": gettext("Copy of user-defined local recharge input, "
+                                     "aligned and clipped to match the other spatial inputs"),
+                    "bands": {1: {"type": "number", "units": u.millimeter}}
+                },
+                'cz_aligned.tif': {
+                    "about": gettext("Copy of user-defined climate zones raster, "
+                                     "aligned and clipped to match the other spatial inputs"),
+                    "bands": {1: {"type": "integer"}}
                 }
             }
         },
@@ -441,15 +493,10 @@ _INTERMEDIATE_BASE_FILES = {
     'flow_dir_mfd_path': 'flow_dir_mfd.tif',
     'qfm_path_list': ['qf_%d.tif' % (x+1) for x in range(N_MONTHS)],
     'stream_path': 'stream.tif',
-    'outflow_direction_path': 'outflow_direction.tif',
-    'outflow_weights_path': 'outflow_weights.tif',
-    'kc_path': 'kc.tif',
     'si_path': 'Si.tif',
     'lulc_aligned_path': 'lulc_aligned.tif',
     'dem_aligned_path': 'dem_aligned.tif',
     'dem_pit_filled_path': 'pit_filled_dem.tif',
-    'loss_path': 'loss.tif',
-    'zero_absorption_source_path': 'zero_absorption.tif',
     'soil_group_aligned_path': 'soil_group_aligned.tif',
     'flow_accum_path': 'flow_accum.tif',
     'precip_path_aligned_list': ['prcp_a%d.tif' % x for x in range(N_MONTHS)],
@@ -458,7 +505,6 @@ _INTERMEDIATE_BASE_FILES = {
     'kc_path_list': ['kc_%d.tif' % x for x in range(N_MONTHS)],
     'l_aligned_path': 'l_aligned.tif',
     'cz_aligned_raster_path': 'cz_aligned.tif',
-    'l_sum_pre_clamp': 'l_sum_pre_clamp.tif'
 }
 
 
