685a3b4ddaf159e47a40ae5df88b923382050244
Doug <dcdenu4@gmail.com>
2023-12-04 09:34:33 -0500

    Update src/natcap/invest/habitat_quality.py
    Co-authored-by: Emily Soth <43770515+emlys@users.noreply.github.com>

--- src/natcap/invest/habitat_quality.py
+++ src/natcap/invest/habitat_quality.py
@@ -131,7 +131,7 @@ MODEL_SPEC = {
                     "about": gettext(
                         "File system path to a raster of the threat's "
                         "distribution in a future scenario. Each pixel "
-                        "value in the threat raster is the relative intensity "
+                        "value in this raster is the relative intensity "
                         "of the threat at that location, with values between "
                         "0 and 1.")
                 },
