f04e87ef2c245ad63c43463d37b9020254188a4d
Emily Soth <esoth@stanford.edu>
2024-04-04 17:33:00 -0700

    refactor add plugin script from bash into javascript with execsync

--- workbench/src/main/setupAddPlugin.js
+++ workbench/src/main/setupAddPlugin.js
@@ -1,9 +1,12 @@
 import upath from 'upath';
+import fs from 'fs';
+import toml from 'toml';
 import { execSync } from 'child_process';
 import { app, ipcMain } from 'electron';
 
 import { getLogger } from './logger';
 import { ipcMainChannels } from './ipcMainChannels';
+import { settingsStore } from './settingsStore';
 
 const logger = getLogger(__filename.split('/').slice(-1)[0]);
 
@@ -12,13 +15,46 @@ export default function setupAddPlugin() {
     ipcMainChannels.ADD_PLUGIN,
     (e, pluginURL) => {
       logger.info('adding plugin at', pluginURL);
-      const pluginsPath = upath.join(app.getPath('userData'), 'plugins');
-      const configPath = upath.join(app.getPath('userData'), 'config.json');
-      logger.info(pluginsPath);
-      execSync(
-        `bash src/main/addPlugin.sh ${pluginURL} '${pluginsPath}' '${configPath}'`,
-        { encoding: 'utf-8', stdio: 'inherit' }
+      const pluginsDir = upath.join(app.getPath('userData'), 'plugins');
+      const investRootPath = `${__dirname}/../../..`;
+
+      // Download the plugin from its remote git repo
+      if (!fs.existsSync(pluginsDir)) {
+        fs.mkdirSync(pluginsDir);
+      }
+      const pluginRepoName = upath.basename(pluginURL, upath.extname(pluginURL));
+      fs.rmSync(`${pluginsDir}/${pluginRepoName}`, { recursive: true, force: true });
+      execSync(`git clone ${pluginURL} "${pluginsDir}/${pluginRepoName}"`, { stdio: 'inherit' });
+
+      // Read in plugin metadata from the pyproject.toml
+      const pyprojectTOML = toml.parse(fs.readFileSync(
+        `${pluginsDir}/${pluginRepoName}/pyproject.toml`
+      ).toString());
+      const pluginID = pyprojectTOML.tool.natcap.invest.model_id;
+      const pluginName = pyprojectTOML.tool.natcap.invest.model_name;
+      const pluginPyName = pyprojectTOML.tool.natcap.invest.pyname;
+
+      // Create a conda env containing the plugin and its dependencies
+      const envName = `invest_plugin_${pluginID}`;
+      fs.writeFileSync('tmp_env.txt', 'python<3.12\ngdal<3.6');
+      execSync(`micromamba create --yes --name ${envName} -f tmp_env.txt -c conda-forge`);
+      execSync(`micromamba run --name ${envName} pip install .`, { cwd: investRootPath, stdio: 'inherit' });
+      execSync(`micromamba run --name ${envName} pip install "git+${pluginURL}"`, { stdio: 'inherit' });
+
+      // Write plugin metadata to the workbench's config.json
+      const envInfo = execSync(`micromamba info --name ${envName}`).toString();
+      const envPath = envInfo.split('env location : ')[1].split('\n')[0];
+      settingsStore.set(
+        `models.${pluginID}`,
+        {
+          model_name: pluginName,
+          pyname: pluginPyName,
+          type: 'plugin',
+          source: pluginURL,
+          env: envPath,
+        }
       );
+      logger.info('successfully added plugin');
     }
   );
 }
