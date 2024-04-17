9bd27d8de3e86b1cd0cda05b37f22b869e6ff402
Emily Soth <esoth@stanford.edu>
2024-04-17 16:28:30 -0700

    clone plugin to temp dir; shallow clone and only checkout the pyproject.toml

--- workbench/src/main/setupAddPlugin.js
+++ workbench/src/main/setupAddPlugin.js
@@ -1,5 +1,6 @@
 import upath from 'upath';
 import fs from 'fs';
+import { tmpdir } from 'os';
 import toml from 'toml';
 import { execSync } from 'child_process';
 import { app, ipcMain } from 'electron';
@@ -15,20 +16,21 @@ export default function setupAddPlugin() {
     ipcMainChannels.ADD_PLUGIN,
     (e, pluginURL) => {
       logger.info('adding plugin at', pluginURL);
-      const pluginsDir = upath.join(app.getPath('userData'), 'plugins');
 
-      // Download the plugin from its remote git repo
-      if (!fs.existsSync(pluginsDir)) {
-        fs.mkdirSync(pluginsDir);
-      }
-      const pluginRepoName = upath.basename(pluginURL, upath.extname(pluginURL));
-      fs.rmSync(`${pluginsDir}/${pluginRepoName}`, { recursive: true, force: true });
-      execSync(`git clone ${pluginURL} "${pluginsDir}/${pluginRepoName}"`, { stdio: 'inherit' });
+      // Create a temporary directory and check out the plugin's pyproject.toml
+      const tmpPluginDir = fs.mkdtempSync(upath.join(tmpdir(), 'natcap-invest-'));
+      execSync(
+        `git clone --depth 1 --no-checkout ${pluginURL} "${tmpPluginDir}"`,
+        { stdio: 'inherit' });
+      execSync(`git checkout HEAD pyproject.toml`, { cwd: tmpPluginDir, stdio: 'inherit' });
 
-      // Read in plugin metadata from the pyproject.toml
+      // Read in the plugin's pyproject.toml, then delete it
       const pyprojectTOML = toml.parse(fs.readFileSync(
-        `${pluginsDir}/${pluginRepoName}/pyproject.toml`
+        upath.join(tmpPluginDir, 'pyproject.toml')
       ).toString());
+      fs.rmSync(tmpPluginDir, { recursive: true, force: true });
+
+      // Access plugin metadata from the pyproject.toml
       const pluginID = pyprojectTOML.tool.natcap.invest.model_id;
       const pluginName = pyprojectTOML.tool.natcap.invest.model_name;
       const pluginPyName = pyprojectTOML.tool.natcap.invest.pyname;
