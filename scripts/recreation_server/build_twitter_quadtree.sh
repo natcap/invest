bfad64cb2a3d4046d08e7c37cb1ace649f6c096c
davemfish <davemfish@gmail.com>
2024-04-28 16:30:05 -0400

    setup scripts for the full job

--- scripts/recreation_server/build_twitter_quadtree.sh
+++ scripts/recreation_server/build_twitter_quadtree.sh
@@ -3,22 +3,23 @@
 #SBATCH --partition normal,hns
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=500M
+#SBATCH --mem-per-cpu=8000M
 #SBATCH --nodes 1
 # Define how long the job will run d-hh:mm:ss
-#SBATCH --time 00:10:00
+#SBATCH --time=6-00:00:00
+#SBATCH --qos=long
 # Get email notification when job finishes or fails
 #SBATCH --mail-user=dfisher5@stanford.edu
 #SBATCH --mail-type=END,FAIL
-#SBATCH -J build_quadtree_test
-#SBATCH -o build_quadtree_test
+#SBATCH -J build_quadtree
+#SBATCH -o build_quadtree
 # ----------------Load Modules--------------------
 # ----------------Commands------------------------
 
 
 CONTAINER=ghcr.io/davemfish/invest:exp.rec-twitter
 TWEETS_DIR=/scratch/users/woodsp/invest/csv
-find $TWEETS_DIR -name '*.csv' | head -1200 > ~/projects/rec-twitter/tweets_list.txt
+find $TWEETS_DIR -name '*.csv' > ~/projects/rec-twitter/tweets_full_list.txt
 
 # invest repo already cloned into ~/invest
 cd ~/invest
@@ -28,7 +29,7 @@ set -x  # Be eXplicit about what's happening.
 FAILED=0
 singularity run \
     docker://$CONTAINER python scripts/recreation_server/build_twitter_quadtree.py \
-    --csv_file_list=$HOME/projects/rec-twitter/tweets_list.txt \
-    --workspace=$SCRATCH/quadtree_test \
-    --output_filename=twitter_test.pickle \
+    --csv_file_list=$HOME/projects/rec-twitter/tweets_full_list.txt \
+    --workspace=$SCRATCH/twitter_quadtree \
+    --output_filename=global_twitter_qt.pickle \
     --n_cores=4  # match --cpus-per-task value
