7db08c917a5bf045e5ad277e2a84da9c6d1abdc0
davemfish <davemfish@gmail.com>
2024-08-06 12:50:46 +0000

    updated cron script for clearing workspaces

--- scripts/recreation_server/cron_find_rm_cached_workspaces.sh
+++ scripts/recreation_server/cron_find_rm_cached_workspaces.sh
@@ -6,5 +6,5 @@
 # The following finds workspace directories that have not been modified in the past 7 days, and removes them.
 
 # Right now we have two cache dirs going, for two rec servers.
-find /usr/local/recreation-server/recserver_cache_py36/ -maxdepth 1 -mindepth 1 -type d -mtime +7 -regextype posix-egrep -regex "\/usr\/local\/recreation-server\/recserver_cache_py36\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$" -exec rm -r {} \;
-find /usr/local/recreation-server/recserver_cache_2017/ -maxdepth 1 -mindepth 1 -type d -mtime +7 -regextype posix-egrep -regex "\/usr\/local\/recreation-server\/recserver_cache_2017\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$" -exec rm -r {} \;
+find ~/server/flickr/local/ -maxdepth 1 -mindepth 1 -type d -mtime +7 -regextype posix-egrep -regex "\/home\/davemfish\/server\/flickr\/local\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$" -exec rm -r {} \;
+find ~/server/twitter/local/ -maxdepth 1 -mindepth 1 -type d -mtime +7 -regextype posix-egrep -regex "\/home\/davemfish\/server\/twitter\/local\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$" -exec rm -r {} \;
