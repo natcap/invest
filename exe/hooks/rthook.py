dc0628cf6fea2befaf01c7729548caac214821f8
davemfish <davemfish@gmail.com>
2024-12-03 15:06:04 -0500

    remove part of the runtime hook we no longer need since we no longer have a QT UI. #1643

--- exe/hooks/rthook.py
+++ exe/hooks/rthook.py
@@ -5,11 +5,6 @@ import sys
 os.environ['PROJ_LIB'] = os.path.join(sys._MEIPASS, 'proj')
 
 if platform.system() == 'Darwin':
-    # This allows Qt 5.13+ to start on Big Sur.
-    # See https://bugreports.qt.io/browse/QTBUG-87014
-    # and https://github.com/natcap/invest/issues/384
-    os.environ['QT_MAC_WANTS_LAYER'] = '1'
-
     # Rtree will look in this directory first for libspatialindex_c.dylib.
     # In response to issues with github mac binary builds:
     # https://github.com/natcap/invest/issues/594
