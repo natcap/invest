d50a2c9ee2a10943aa0d97849a3d0729c425a8ab
Emily Soth <esoth@stanford.edu>
2024-12-09 15:00:30 -0800

    downgrade compiler version to c++17

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ _REQUIREMENTS = [req.split(';')[0].split('#')[0].strip() for req in
 compiler_and_linker_args = []
 include_dirs = [numpy.get_include(), 'src/natcap/invest/managed_raster']
 if platform.system() == 'Windows':
-    compiler_args = ['/std:c++20']
+    compiler_args = ['/std:c++17']
     library_dirs = [f'{os.environ["CONDA_PREFIX"]}/Library/lib']
     include_dirs.append(f'{os.environ["CONDA_PREFIX"]}/Library/include')
 else:
     library_dirs = []
     compiler_args = []
-    compiler_and_linker_args = ['-stdlib=libc++', '-std=c++20']
+    compiler_and_linker_args = ['-stdlib=libc++', '-std=c++17']
     library_dirs = [subprocess.run(
         ['gdal-config', '--libs'], capture_output=True, text=True
     ).stdout.split()[0][2:]]  # get the first argument which is the library path
