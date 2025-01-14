84fe059a0e17ae9efbc1bd88ace16eb543bcb271
Emily Soth <esoth@stanford.edu>
2025-01-14 10:46:13 -0800

    UCM: align raster inputs to LULC instead of ET0

--- src/natcap/invest/urban_cooling_model.py
+++ src/natcap/invest/urban_cooling_model.py
@@ -484,7 +484,7 @@ def execute(args):
               'intersection'),
         kwargs={
             'base_vector_path_list': [args['aoi_vector_path']],
-            'raster_align_index': 1,
+            'raster_align_index': 0,
             'target_projection_wkt': lulc_raster_info['projection_wkt']},
         target_path_list=aligned_raster_path_list,
         task_name='align rasters')
