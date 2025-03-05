f5cf910c99dcb4c31e44356b90bfc40212cda3da
Emily Soth <esoth@stanford.edu>
2025-03-04 22:57:29 -0800

    clean up

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,7 +2,6 @@
 import functools
 import os
 import shutil
-import subprocess
 import stat
 import string
 import sys
@@ -343,14 +342,7 @@ class DirectoryValidationMacOnly(unittest.TestCase):
         from natcap.invest import validation
 
         with tempfile.TemporaryDirectory() as tempdir:
-            result = subprocess.run(['ls', '-ld', tempdir], capture_output=True, text=True)
-            print(result.stdout)
-            subprocess.run(['chmod', '0444', tempdir])
-            result = subprocess.run(['ls', '-ld', tempdir], capture_output=True, text=True)
-            print(result.stdout)
-            subprocess.run(['cd', tempdir])
-            # os.chmod(tempdir, stat.S_IREAD)
-
+            os.chmod(tempdir, stat.S_IREAD)
             validation_warning = validation.check_directory(tempdir,
                                                             permissions='rwx')
             self.assertEqual(
