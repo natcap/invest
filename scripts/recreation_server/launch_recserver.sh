c588ab35d898c8556f4163a2b82e583eeca0c019
davemfish <davemfish@gmail.com>
2025-03-14 10:58:16 -0400

    update launcher script for new filename. #1551

--- scripts/recreation_server/launch_recserver.sh
+++ scripts/recreation_server/launch_recserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # source /home/davemfish/miniconda3/etc/profile.d/conda.sh
 # mamba activate /usr/local/recreation-server/invest_3_15_0/invest/env
-nohup python -u /usr/local/recreation-server/invest_3_15_0/invest/scripts/recreation_server/execute_recmodel_server_twitter.py \
+nohup python -u /usr/local/recreation-server/invest_3_15_0/invest/scripts/recreation_server/execute_recmodel_server.py \
 -w /usr/local/recreation-server/invest_3_15_0/server > \
 /usr/local/recreation-server/invest_3_15_0/server/log.txt 2>&1 &
