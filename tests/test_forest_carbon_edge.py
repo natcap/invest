2038e9dd1314da637d42417df3deb8f17c774c3c
Claire Simpson <cesimpson@stanford.edu>
2025-04-08 13:57:51 -0600

    Revert nearest model pts and expected values; raise rtol

--- tests/test_forest_carbon_edge.py
+++ tests/test_forest_carbon_edge.py
@@ -508,7 +508,7 @@ class ForestCarbonEdgeTests(unittest.TestCase):
         _create_spatial_index_pickle(spatial_index_pickle_path,
                                      edge_distance_path)
 
-        n_nearest_model_points = 7
+        n_nearest_model_points = 6
         biomass_to_carbon_conversion_factor = 10
         tropical_forest_edge_carbon_map_path = os.path.join(self.workspace_dir,
                                                             "output.tif")
@@ -522,12 +522,12 @@ class ForestCarbonEdgeTests(unittest.TestCase):
             tropical_forest_edge_carbon_map_path)
         expected_output = numpy.array(
             [[-1, -1, -1, -1, -1, -1, -1],
-             [-1, 259.70303, 143.74193, 159.72873, 164.17456, 172.62431, -1],
-             [-1, 184.31717, 357.24908, 380.26678, 403.87314, 230.79578, -1],
-             [-1, 215.34108, 232.61235, 243.14659, 257.5953, 276.23138, -1],
+             [-1, 265.7061, 142.47273, 153.98717, 165.88376, 178.15967, -1],
+             [-1, 175.92271, 351.15747, 387.35364, 411.1204, 220.55798, -1],
+             [-1, 217.2353, 231.00058, 245.13129, 259.62473, 274.47815, -1],
              [-1, -1, -1, -1, -1, -1, -1]])
 
-        numpy.testing.assert_allclose(actual_output, expected_output)
+        numpy.testing.assert_allclose(actual_output, expected_output, rtol=.02)
 
     @staticmethod
     def _test_same_files(base_list_path, directory_path):
