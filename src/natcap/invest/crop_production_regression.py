d00afcf5b6a9cd9be6488a87cada9a24ca70dec7
Emily Davis <eadavis@stanford.edu>
2025-04-09 14:41:22 -0600

    Crop Production (Regression): log a warning if any values in LULC raster are not in crop table (#925)

--- src/natcap/invest/crop_production_regression.py
+++ src/natcap/invest/crop_production_regression.py
@@ -505,21 +505,28 @@ def execute(args):
         args['fertilization_rate_table_path'],
         **MODEL_SPEC['args']['fertilization_rate_table_path'])
 
-    crop_lucodes = list(crop_to_landcover_df[_EXPECTED_LUCODE_TABLE_HEADER])
+    lucodes_in_table = list(crop_to_landcover_df[_EXPECTED_LUCODE_TABLE_HEADER])
 
-    unique_lucodes = numpy.array([])
+    unique_lucodes_in_raster = numpy.array([])
     for _, lu_band_data in pygeoprocessing.iterblocks(
             (args['landcover_raster_path'], 1)):
         unique_block = numpy.unique(lu_band_data)
-        unique_lucodes = numpy.unique(numpy.concatenate(
-            (unique_lucodes, unique_block)))
+        unique_lucodes_in_raster = numpy.unique(numpy.concatenate(
+            (unique_lucodes_in_raster, unique_block)))
 
-    missing_lucodes = set(crop_lucodes).difference(
-        set(unique_lucodes))
-    if len(missing_lucodes) > 0:
+    lucodes_missing_from_raster = set(lucodes_in_table).difference(
+        set(unique_lucodes_in_raster))
+    if len(lucodes_missing_from_raster) > 0:
         LOGGER.warning(
             "The following lucodes are in the landcover to crop table but "
-            "aren't in the landcover raster: %s", missing_lucodes)
+            "aren't in the landcover raster: %s", lucodes_missing_from_raster)
+
+    lucodes_missing_from_table = set(unique_lucodes_in_raster).difference(
+        set(lucodes_in_table))
+    if len(lucodes_missing_from_table) > 0:
+        LOGGER.warning(
+            "The following lucodes are in the landcover raster but aren't "
+            "in the landcover to crop table: %s", lucodes_missing_from_table)
 
     LOGGER.info("Checking that crops correspond to known types.")
     for crop_name in crop_to_landcover_df.index:
