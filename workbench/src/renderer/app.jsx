32e597fcba5adf9b2be8f244db8b9aa67d2afd38
Emily Soth <esoth@stanford.edu>
2025-05-21 09:50:06 -0700

    move InvestJob migration to use lower level jobStore API

--- workbench/src/renderer/app.jsx
+++ workbench/src/renderer/app.jsx
@@ -63,20 +63,6 @@ export default class App extends React.Component {
   async componentDidMount() {
     const investList = await this.updateInvestList();
     let recentJobs = await InvestJob.getJobStore();
-    // Migrate old-style jobs
-    // We can eventually remove this code once it's likely that most users
-    // will have updated and ran a newer version of the workbench
-    InvestJob.clearStore();
-    recentJobs = recentJobs.map((job) => {
-      if (job.modelID === undefined) {
-        job.modelID = job.modelRunName;
-        job.modelTitle = job.modelHumanName;
-        delete job.modelRunName;
-        delete job.modelHumanName;
-        InvestJob.saveJob(job);
-      }
-      return job;
-    });
     this.setState({
       // filter out models that do not exist in current version of invest
       recentJobs: recentJobs.filter((job) => (
