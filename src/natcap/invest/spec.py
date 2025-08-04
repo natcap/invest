2d67ef9a014b9858d5c835aab8b77849e2cc3bf5
Megan Nissel <megnissel@gmail.com>
2025-08-04 10:16:52 -0400

    add `path` attribute to FileOutput (#2054)

--- src/natcap/invest/spec.py
+++ src/natcap/invest/spec.py
@@ -1315,7 +1315,17 @@ class OptionStringInput(Input):
         return sorted(lines, key=lambda line: line.casefold())
 
 
-class SingleBandRasterOutput(Output):
+class FileOutput(Output):
+    """A generic file output, or result, of an invest model.
+
+    This represents a not-otherwise-specified file output type. Use this only if
+    a more specific type, such as `CSVOutput` or `VectorOutput`, does not apply.
+    """
+    path: str
+    """Path to the output file within the workspace directory"""
+
+
+class SingleBandRasterOutput(FileOutput):
     """A single-band raster output, or result, of an invest model.
 
     This represents a raster file output (all GDAL-supported raster file types
@@ -1328,7 +1338,7 @@ class SingleBandRasterOutput(Output):
     """units of measurement of the raster values"""
 
 
-class RasterOutput(Output):
+class RasterOutput(FileOutput):
     """A raster output, or result, of an invest model.
 
     This represents a raster file output (all GDAL-supported raster file types
@@ -1339,7 +1349,7 @@ class RasterOutput(Output):
     the raster."""
 
 
-class VectorOutput(Output):
+class VectorOutput(FileOutput):
     """A vector output, or result, of an invest model.
 
     This represents a vector file output (all GDAL-supported vector file types
@@ -1361,7 +1371,7 @@ class VectorOutput(Output):
         return self
 
 
-class CSVOutput(Output):
+class CSVOutput(FileOutput):
     """A CSV table output, or result, of an invest model.
 
     For CSVs with a simple layout, `columns` or `rows` (but not both) may be
@@ -1424,15 +1434,6 @@ class DirectoryOutput(Output):
                 raise ValueError(
                     f'Directory contents {content} is not an allowed type')
         return self
-
-
-class FileOutput(Output):
-    """A generic file output, or result, of an invest model.
-
-    This represents a not-otherwise-specified file output type. Use this only if
-    a more specific type, such as `CSVOutput` or `VectorOutput`, does not apply.
-    """
-    pass
 
 
 class NumberOutput(Output):
@@ -1726,28 +1727,31 @@ FLOW_DIR_ALGORITHM = OptionStringInput(
 )
 
 # Specs for common outputs ####################################################
-TASKGRAPH_DIR = DirectoryOutput(
+TASKGRAPH_DIR = FileOutput(
     id="taskgraph_cache",
+    path="taskgraph_cache/taskgraph.db",
     about=gettext(
         "Cache that stores data between model runs. This directory contains no"
         " human-readable data and you may ignore it."
-    ),
-    contents=[FileOutput(id="taskgraph.db", about=None)]
+    )
 )
 FILLED_DEM = SingleBandRasterOutput(
-    id='',
+    id='filled_dem',
+    path='filled_dem.tif',
     about=gettext("Map of elevation after any pits are filled"),
     data_type=float,
     units=u.meter
 )
 FLOW_ACCUMULATION = SingleBandRasterOutput(
-    id='',
+    id='flow_accumulation',
+    path='flow_accumulation.tif',
     about=gettext("Map of flow accumulation"),
     data_type=float,
     units=u.none
 )
 FLOW_DIRECTION = SingleBandRasterOutput(
-    id='',
+    id='flow_direction',
+    path='flow_direction.tif',
     about=gettext(
         "MFD flow direction. Note: the pixel values should not be interpreted"
         " directly. Each 32-bit number consists of 8 4-bit numbers. Each 4-bit"
@@ -1758,7 +1762,8 @@ FLOW_DIRECTION = SingleBandRasterOutput(
     units=None
 )
 SLOPE = SingleBandRasterOutput(
-    id="slope.tif",
+    id="slope",
+    path='slope.tif',
     about=gettext(
         "Percent slope, calculated from the pit-filled DEM. 100 is equivalent to"
         " a 45 degree slope."
@@ -1767,7 +1772,8 @@ SLOPE = SingleBandRasterOutput(
     units=None
 )
 STREAM = SingleBandRasterOutput(
-    id='',
+    id='stream',
+    path='stream.tif',
     about=gettext(
         "Stream network, created using flow direction and flow accumulation"
         " derived from the DEM and Threshold Flow Accumulation. Values of 1"
