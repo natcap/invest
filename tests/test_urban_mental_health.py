5e834a6122fc42e1d0099d647c3840d38e5685f3
Claire Simpson <cesimpson@stanford.edu>
2025-09-23 15:44:11 -0600

    Adding to tests #2143

--- tests/test_urban_mental_health.py
+++ tests/test_urban_mental_health.py
@@ -20,33 +20,48 @@ from osgeo import ogr
 from osgeo import osr
 
 FLOAT32_NODATA = float(numpy.finfo(numpy.float32).min)
+_DEFAULT_ORIGIN = (444720, 3751320)
+_DEFAULT_PIXEL_SIZE = (30, -30)
+_DEFAULT_EPSG = 3116
+_DEFAULT_SRS = osr.SpatialReference()
+_DEFAULT_SRS.ImportFromEPSG(_DEFAULT_EPSG)
+
 
 
 def make_simple_vector(path_to_shp, fields={"id": ogr.OFTReal},
-                       attribute_list=[{"id": 0}]):
+                       attribute_list=[{"id": 0}], epsg=26910,
+                       shapely_geometry_list = [ # this polygon fits within middle pixel of raster
+                           Polygon([(461351, 4923191), (461451, 4923191),
+                                    (461451, 4923245), (461351, 4923245),
+                                    (461351, 4923191)])]):
+    
+    #Polygon([(461251, 4923195), (461501, 4923195),
+            # (461501, 4923445), (461251, 4923445),
+            # (461251, 4923195)])]): --> this is too large! good for last test rn
     """
     Generate shapefile with one rectangular polygon
+
     Args:
         path_to_shp (str): path to target shapefile
+        fields (dict): dictionary with attribute: datatype key/value pairs
+        attribute_list (list): list of attribute: value dictionaries 
+        epsg (int): EPSG code
+        shapely_geometry_list (list): list of shapely geometry elements. For
+            polygons, order of elements in list is:
+            (xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin)
+            Note: list length must equal length of values in attribute_list dicts.
+
     Returns:
-        None
+        None.
     """
-    # (xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin)
-    shapely_geometry_list = [
-        Polygon([(461251, 4923195), (461501, 4923195),
-                 (461501, 4923445), (461251, 4923445),
-                 (461251, 4923195)])
-    ]
-
+    
     srs = osr.SpatialReference()
-    srs.ImportFromEPSG(26910)
+    srs.ImportFromEPSG(epsg)
     projection_wkt = srs.ExportToWkt()
-
-    vector_format = "ESRI Shapefile"
 
     pygeoprocessing.shapely_geometry_to_vector(shapely_geometry_list,
                                                path_to_shp, projection_wkt,
-                                               vector_format, fields,
+                                               "ESRI Shapefile", fields,
                                                attribute_list)
 
 
@@ -63,7 +78,7 @@ def make_raster_from_array(base_raster_path, array):
     projection_wkt = srs.ExportToWkt()
 
     origin = (461251, 4923445)
-    pixel_size = (30, -30)
+    pixel_size = (100, -100)
     no_data = FLOAT32_NODATA
 
     pygeoprocessing.numpy_array_to_raster(
@@ -76,7 +91,11 @@ def make_synthetic_data_and_params(workspace_dir):
         baseline_prevalence_path = os.path.join(workspace_dir, "baseline_prevalence.shp")
         fields={"id": ogr.OFTReal, "risk_rate": ogr.OFTReal}
         attribute_list=[{"id": 0,"risk_rate": 10}]
-        make_simple_vector(baseline_prevalence_path, fields, attribute_list)
+        make_simple_vector(baseline_prevalence_path, fields, attribute_list,
+                           shapely_geometry_list=[
+                                Polygon([(461251, 4923195), (461501, 4923195),
+                                         (461501, 4923445), (461251, 4923445),
+                                         (461251, 4923195)])])
 
         ndvi_base_array = numpy.array([[.1, .2, .35], [.5, .6, .7], [.8, .9, .10], [.11, .12, FLOAT32_NODATA]])
         ndvi_base_path = os.path.join(workspace_dir, "ndvi_base.tif")
@@ -96,19 +115,11 @@ def make_synthetic_data_and_params(workspace_dir):
         aoi_path = os.path.join(workspace_dir, "aoi.shp")
         make_simple_vector(aoi_path)
 
-        health_effect_csv_path = os.path.join(workspace_dir, "health_effect.csv")
-        df = pandas.DataFrame({"health_indicator":['depression'],
-                               "effect_size":[0.94],
-                               "exposure_metric_percent":[.1],
-                               "exposure_metric_name":['NDVI'],
-                               "ratio_type":['risk']})
-        df.to_csv(health_effect_csv_path)
-
         args = {
             'aoi_vector_path': aoi_path,
             'baseline_prevalence_vector': baseline_prevalence_path,
-            'effect_size_csv': health_effect_csv_path,
-            'health_cost_rate_csv': None,
+            'effect_size': 0.94,
+            'health_cost_rate': None,
             'lulc_alt': '',
             'lulc_attr_csv': '',
             'lulc_base': '',
@@ -117,7 +128,7 @@ def make_synthetic_data_and_params(workspace_dir):
             'population_raster': pop_path,
             'results_suffix': 'test1',
             'scenario': 'ndvi',
-            'search_radius': 30, # 1 pixel
+            'search_radius': 100, # 1 pixel
             'tc_raster': '',
             'tc_target': '',
             'workspace_dir': workspace_dir,
@@ -139,26 +150,46 @@ class UMHTests(unittest.TestCase):
         """Overriding tearDown function to remove temporary directory."""
         shutil.rmtree(self.workspace_dir)
 
-    def test_delta_ndvi(self):
+    def test_ndvi_processing(self):
         """Test ``calc_delta_ndvi``"""
         from natcap.invest import urban_mental_health
         args = make_synthetic_data_and_params(self.workspace_dir)
         urban_mental_health.execute(args)
 
         intermediate = os.path.join(self.workspace_dir, "intermediate_outputs")
-        output_delta_ndvi = os.path.join(intermediate, f'delta_ndvi_{args['results_suffix']}.tif')
+        suffix = args['results_suffix']
+        # Assert that top row was removed as its outside of search_radius distance of AOI
+        expected_base_aligned = numpy.array( #this is copied from base_ndvi without top row
+            [[.5, .6, .7], 
+            [.8, .9, .10], 
+            [.11, .12, FLOAT32_NODATA]])
+        actual_base_aligned = pygeoprocessing.raster_to_numpy_array(os.path.join(intermediate, f'ndvi_base_aligned_{suffix}.tif'))
+        numpy.testing.assert_allclose(actual_base_aligned, expected_base_aligned, atol=1e-6)
+
+        # Assert that convolution was correct
+        expected_base_convolve = numpy.array( #calculated by hand
+            [[0.633333, 0.675, 0.4666667],
+            [0.5775, 0.504, 0.5666667],
+            [0.3433333, 0.3766667, FLOAT32_NODATA]]
+        )
+        actual_base_convolve = pygeoprocessing.raster_to_numpy_array(os.path.join(intermediate, f'ndvi_base_buffer_mean_{suffix}.tif'))
+        numpy.testing.assert_allclose(actual_base_convolve, expected_base_convolve, atol=1e-6)
+
+        expected_alt_convolve = numpy.array(
+            [[0.466667, 0.36, 0.413333],
+            [0.35, 0.33, 0.345],
+            [0.41, 0.1925, 0.203333]])
+        
+        output_delta_ndvi = os.path.join(intermediate, f'delta_ndvi_{suffix}.tif')
 
         urban_mental_health.calc_delta_ndvi(
-             os.path.join(intermediate, f'ndvi_base_buffer_mean_{args['results_suffix']}.tif'),
-             os.path.join(intermediate, f'ndvi_alt_buffer_mean_{args['results_suffix']}.tif'),
+             os.path.join(intermediate, f'ndvi_base_buffer_mean_{suffix}.tif'),
+             os.path.join(intermediate, f'ndvi_alt_buffer_mean_{suffix}.tif'),
              output_delta_ndvi)
         actual_delta_ndvi = pygeoprocessing.raster_to_numpy_array(output_delta_ndvi)
         
-        expected_delta_ndvi = numpy.array(
-             [[-8.66667000e-02, -1.27500000e-01, -4.33337000e-02],
-             [-1.20000000e-01, -2.48000000e-01, -1.02500000e-01],
-             [-2.27500000e-01, -1.74000000e-01, -2.21666700e-01],
-             [ 6.66667000e-02, -1.84166600e-01,  FLOAT32_NODATA]])
+        expected_delta_ndvi = expected_alt_convolve - expected_base_convolve
+        expected_delta_ndvi[2, 2] = FLOAT32_NODATA
 
         numpy.testing.assert_allclose(actual_delta_ndvi, expected_delta_ndvi, atol=1e-6)
 
@@ -171,53 +202,129 @@ class UMHTests(unittest.TestCase):
 
         urban_mental_health.execute(args)
 
-        expected_kernel = numpy.array([[0, 0.2, 0], [0.2, 0.2, 0.2], [0, 0.2, 0]])
-        kernel_path = os.path.join(self.workspace_dir, "intermediate_outputs",
-                                   f"kernel_{args['results_suffix']}.tif")
-        actual_kernel = pygeoprocessing.raster_to_numpy_array(kernel_path)
-
-        expected_mean_base_ndvi = numpy.array(
-             [[.2666667, .3125, .4166667],
-              [.5, .58, .4375],
-              [.5775, .504, .5666667],
-              [.3433333, .3766666, FLOAT32_NODATA]])
-        actual_mean_base_ndvi_path = os.path.join(self.workspace_dir, "intermediate_outputs",
-                                                  f"ndvi_base_buffer_mean_{args['results_suffix']}.tif")
-        actual_mean_base_ndvi = pygeoprocessing.raster_to_numpy_array(actual_mean_base_ndvi_path)
-        
-        expected_mean_alt_ndvi = numpy.array(
-             [[0.18, 0.185, 0.373333],
-              [0.38, 0.332, 0.335],
-              [0.35, 0.33, 0.345],
-              [0.41, 0.1925, 0.203333]])
-        actual_mean_alt_ndvi_path = actual_mean_base_ndvi_path.replace("_base_", "_alt_")
-        actual_mean_alt_ndvi = pygeoprocessing.raster_to_numpy_array(actual_mean_alt_ndvi_path)
-
-        numpy.testing.assert_allclose(actual_kernel, expected_kernel)
-        numpy.testing.assert_allclose(actual_mean_base_ndvi, expected_mean_base_ndvi, atol=1e-6)
-        numpy.testing.assert_allclose(actual_mean_alt_ndvi, expected_mean_alt_ndvi, atol=1e-6)
-         
-        expected_baseline_cases = numpy.array([[.12, .22, .1],
-                                [.2, .3, .8],
-                                [.9, .14, .14],
-                                [.16, .17, .3]])*1000
+        # expected_kernel = numpy.array([[0, 0.2, 0], [0.2, 0.2, 0.2], [0, 0.2, 0]])
+        # kernel_path = os.path.join(self.workspace_dir, "intermediate_outputs",
+        #                            f"kernel_{args['results_suffix']}.tif")
+        # actual_kernel = pygeoprocessing.raster_to_numpy_array(kernel_path)
+        # numpy.testing.assert_allclose(actual_kernel, expected_kernel)
+        expected_baseline_cases = numpy.array([
+                            [200, 300, 800],
+                            [900, 140, 140],
+                            [FLOAT32_NODATA, FLOAT32_NODATA, FLOAT32_NODATA]])
         actual_baseline_cases_path = os.path.join(self.workspace_dir, "intermediate_outputs",
                                                   f"baseline_cases_{args['results_suffix']}.tif")
         actual_baseline_cases = pygeoprocessing.raster_to_numpy_array(actual_baseline_cases_path)
         numpy.testing.assert_allclose(actual_baseline_cases, expected_baseline_cases, atol=1e-6)
 
-        expected_preventable_cases = numpy.array(
-             [[-6.61070802, -18.05903398, -2.71753617],
-             [-15.4153132, -49.75519078, -52.38134228],
-             [-136.04027944, -15.91416352, -20.58117008],
-             [6.46576569, -20.51906896, FLOAT32_NODATA]])
+        expected_preventable_cases = numpy.array([
+            [-21.72614, -64.55958, -26.8406096],
+            [-136.040285, -15.914164, -20.58118],
+            [FLOAT32_NODATA, FLOAT32_NODATA, FLOAT32_NODATA]])
         #^ calculated using (1 - numpy.exp(numpy.log(0.94)*10*actual_delta_ndvi)) * actual_baseline_cases
         # (1 - (exp(ln(RR0.1NE)10*NE))) * bc
+        expected_preventable_cases = numpy.array([ # only center pixel left bc AOI is small
+            [FLOAT32_NODATA, FLOAT32_NODATA, FLOAT32_NODATA],
+            [FLOAT32_NODATA, -15.914164, FLOAT32_NODATA],
+            [FLOAT32_NODATA, FLOAT32_NODATA, FLOAT32_NODATA]])
         actual_preventable_cases_path = os.path.join(self.workspace_dir,
                                                      f"preventable_cases_{args['results_suffix']}.tif")
         actual_preventable_cases = pygeoprocessing.raster_to_numpy_array(actual_preventable_cases_path)
 
-        numpy.testing.assert_allclose(actual_preventable_cases, expected_preventable_cases, atol=1e-6)
+        numpy.testing.assert_allclose(actual_preventable_cases, expected_preventable_cases, atol=1e-5)
+
+    # def test_resample_population_raster(self):
+    #     """UNA: Test population raster resampling."""
+    #     from natcap.invest import urban_mental_health
+
+    #     random.seed(-1)  # for our random number generation
+
+    #     source_population_raster_path = os.path.join(
+    #         self.workspace_dir, 'population.tif')
+    #     population_pixel_size = (90, -90)
+    #     population_array_shape = (10, 10)
+
+    #     array_of_100s = numpy.full(
+    #         population_array_shape, 100, dtype=numpy.uint32)
+    #     array_of_random_ints = numpy.array(
+    #         random.choices(range(0, 100), k=100),
+    #         dtype=numpy.uint32).reshape(population_array_shape)
+
+    #     for population_array in (
+    #             array_of_100s, array_of_random_ints):
+    #         population_srs = osr.SpatialReference()
+    #         population_srs.ImportFromEPSG(_DEFAULT_EPSG)
+    #         population_wkt = population_srs.ExportToWkt()
+    #         pygeoprocessing.numpy_array_to_raster(
+    #             base_array=population_array,
+    #             target_nodata=-1,
+    #             pixel_size=population_pixel_size,
+    #             origin=_DEFAULT_ORIGIN,
+    #             projection_wkt=population_wkt,
+    #             target_path=source_population_raster_path)
+
+    #         for target_pixel_size in (
+    #                 (30, -30),  # 1/3 the pixel size
+    #                 (4, -4),  # way smaller
+    #                 (100, -100)):  # bigger
+    #             target_population_raster_path = os.path.join(
+    #                 self.workspace_dir, 'resampled_population.tif')
+    #             urban_mental_health._resample_population_raster(
+    #                 source_population_raster_path,
+    #                 target_population_raster_path,
+    #                 target_pixel_size=target_pixel_size,
+    #                 target_bb=pygeoprocessing.get_raster_info(
+    #                     source_population_raster_path)['bounding_box'],
+    #                 target_projection_wkt=population_wkt,
+    #                 working_dir=os.path.join(self.workspace_dir, 'working'))
+
+    #             resampled_population_array = (
+    #                 pygeoprocessing.raster_to_numpy_array(
+    #                     target_population_raster_path))
+
+    #             # There should be no significant loss or gain of population due
+    #             # to warping, but the fact that this is aggregating across the
+    #             # whole raster (lots of pixels) means we need to lower the
+    #             # relative tolerance.
+    #             numpy.testing.assert_allclose(
+    #                 population_array.sum(), resampled_population_array.sum(),
+    #                 rtol=1e-3)
+                
+    
+    def test_diff_prj_inputs(self):
+        """Test model option 3 given inputs of different projections.
+        
+        Test that creating a target bounding box from inputs with
+        different projections still produce expected target bbox
+        """
+        from natcap.invest import urban_mental_health
+
+        args = make_synthetic_data_and_params(self.workspace_dir)
+
+        #create AOI w different projection
+        epsg = 5070
+        # (xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin)
+        # xmin = -2151419.7
+        # ymax = 2699283.3
+        # xmax = xmin + 1000
+        # ymin = ymax - 1000
+        #make tiny bbox for AOI
+        xmin = -2151375#-2151419.7 +10
+        ymax = 2699058.8 #2699283.3 +10
+        xmax = xmin + 50
+        ymin = ymax - 50
+        geom = [Polygon([(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax), (xmin, ymin)])]
+        new_aoi_path = os.path.join(self.workspace_dir, "AOI_5070.shp")
+
+        make_simple_vector(new_aoi_path, epsg=epsg, shapely_geometry_list=geom)
+
+        args["aoi_vector_path"] = new_aoi_path
+
+        urban_mental_health.execute(args)
+
+        #TODO: add assertion
+        
+    def test_AOI_too_large(self):
+        pass
 
 
 
