c0f8ab17bcc86a16514c01e4daf18f321842a2cc
Emily Soth <esoth@stanford.edu>
2025-11-05 15:06:14 -0800

    replace get_row in rst generating function

--- src/natcap/invest/spec.py
+++ src/natcap/invest/spec.py
@@ -2407,7 +2407,7 @@ def describe_arg_from_name(module_name, *arg_keys):
         elif i > 0 and arg_keys[i - 1] == 'columns':
             spec = spec.get_column(key)
         elif i > 0 and arg_keys[i - 1] == 'rows':
-            spec = spec.get_row(key)
+            spec = spec.get_column(key)
         elif key in {'bands', 'fields', 'contents', 'columns', 'rows'}:
             continue
         else:
