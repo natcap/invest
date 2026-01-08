90c17e1978d36d7375ff67ec131d61f7d257f85e
davemfish <davemfish@gmail.com>
2026-01-08 17:15:08 -0500

    moved scenario_generator to a subpackage. #2277

--- src/natcap/invest/scenario_gen_proximity/scenario_gen_proximity.py
+++ src/natcap/invest/scenario_gen_proximity/scenario_gen_proximity.py
@@ -13,14 +13,12 @@ import numpy
 import pygeoprocessing
 import pygeoprocessing.kernels
 import scipy
-import taskgraph
 from osgeo import gdal
 
-from . import gettext
-from . import spec
-from . import utils
-from . import validation
-from .unit_registry import u
+from natcap.invest import gettext
+from natcap.invest import spec
+from natcap.invest import validation
+from natcap.invest.unit_registry import u
 
 LOGGER = logging.getLogger(__name__)
 
