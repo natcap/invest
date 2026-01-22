beb21323defeb259cdb671d5f6d5d556c2e8f53f
Claire Simpson <cesimpson@stanford.edu>
2026-01-22 13:56:31 -0700

    Write crop regression defaults back to dataframe #2310

--- src/natcap/invest/crop_production_regression/crop_production_regression.py
+++ src/natcap/invest/crop_production_regression/crop_production_regression.py
@@ -645,10 +645,10 @@ def execute(args):
             CROP_TO_PATH_TABLES.regression_yield).get_column(
                 'path').get_validated_dataframe(
                     climate_regression_yield_table_path)
-        for _, row in crop_regression_df.iterrows():
+        for idx, row in crop_regression_df.iterrows():
             for header in _EXPECTED_REGRESSION_TABLE_HEADERS:
                 if numpy.isnan(row[header]):
-                    row[header] = 0
+                    crop_regression_df.at[idx, header] = 0.0
 
         yield_regression_headers = [
             x for x in crop_regression_df.columns if x != 'climate_bin']
