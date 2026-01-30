7f0f9d38d770d582ee50e7dbe038794f853c1e51
Emily Davis <eadavis@stanford.edu>
2026-01-30 14:20:05 -0700

    Add tests for extra-wide AOI, unit text, and distinctipy-generated color palettes.

--- tests/reports/test_raster_utils.py
+++ tests/reports/test_raster_utils.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import tempfile
 import unittest
+from unittest.mock import patch
 
 import matplotlib
 import matplotlib.testing.compare
@@ -13,8 +14,8 @@ import pygeoprocessing
 
 from natcap.invest import spec
 from natcap.invest.unit_registry import u
-from natcap.invest.reports import raster_utils
-from natcap.invest.reports import MATPLOTLIB_PARAMS
+from natcap.invest.reports import MATPLOTLIB_PARAMS, raster_utils
+from natcap.invest.reports.raster_utils import RasterDatatype, RasterTransform
 
 projection = osr.SpatialReference()
 projection.ImportFromEPSG(3857)
@@ -79,14 +80,14 @@ def compare_snapshots(reference, actual):
 
 
 class RasterPlotLayoutTests(unittest.TestCase):
-    """Unit tests for matplotlib utilities."""
+    """Snapshot tests for matplotlib figure layouts."""
 
     def setUp(self):
         """Override setUp function to create temp workspace directory."""
         self.workspace_dir = tempfile.mkdtemp()
         self.raster_config = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'foo.tif'),
-            'continuous', 'linear')
+            RasterDatatype.continuous, RasterTransform.linear)
 
     def tearDown(self):
         """Override tearDown function to remove temporary directory."""
@@ -119,13 +120,26 @@ class RasterPlotLayoutTests(unittest.TestCase):
         compare_snapshots(reference, actual_png)
 
     def test_plot_raster_list_tall_aoi(self):
-        """Test figure has 1 rows and 3 columns."""
+        """Test figure has 1 row and 3 columns."""
         figname = 'plot_raster_list_tall_aoi.png'
         reference = os.path.join(REFS_DIR, figname)
         shape = (8, 2)
         make_simple_raster(self.raster_config.raster_path, shape)
 
         config_list = [self.raster_config]*3
+        fig = raster_utils.plot_raster_list(config_list)
+        actual_png = os.path.join(self.workspace_dir, figname)
+        save_figure(fig, actual_png)
+        compare_snapshots(reference, actual_png)
+
+    def test_plot_raster_list_extra_wide_aoi(self):
+        """Test figure has 2 rows and 1 column."""
+        figname = 'plot_raster_list_extra_wide_aoi.png'
+        reference = os.path.join(REFS_DIR, figname)
+        shape = (2, 10)
+        make_simple_raster(self.raster_config.raster_path, shape)
+
+        config_list = [self.raster_config]*2
         fig = raster_utils.plot_raster_list(config_list)
         actual_png = os.path.join(self.workspace_dir, figname)
         save_figure(fig, actual_png)
@@ -145,8 +159,8 @@ class RasterPlotLayoutTests(unittest.TestCase):
         compare_snapshots(reference, actual_png)
 
 
-class RasterPlotConfig(unittest.TestCase):
-    """Unit tests for datatype and transform options."""
+class RasterPlotDatatypeAndTransformTests(unittest.TestCase):
+    """Snapshot tests for datatype and transform options."""
 
     def setUp(self):
         """Override setUp function to create temp workspace directory."""
@@ -163,12 +177,12 @@ class RasterPlotConfig(unittest.TestCase):
         shape = (2, 8)
         continuous_raster = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'continuous.tif'),
-            'continuous')
+            RasterDatatype.continuous)
         make_simple_raster(continuous_raster.raster_path, shape)
 
         binary_raster = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'binary.tif'),
-            'binary')
+            RasterDatatype.binary)
         binary_array = numpy.zeros(shape=shape)
         binary_array[0] = 1
         pygeoprocessing.numpy_array_to_raster(
@@ -178,7 +192,7 @@ class RasterPlotConfig(unittest.TestCase):
 
         divergent_raster = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'divergent.tif'),
-            'divergent')
+            RasterDatatype.divergent)
         divergent_array = numpy.linspace(
             -1, 1, num=numpy.multiply(*shape)).reshape(*shape)
         pygeoprocessing.numpy_array_to_raster(
@@ -188,7 +202,7 @@ class RasterPlotConfig(unittest.TestCase):
 
         nominal_raster = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'nominal.tif'),
-            'nominal')
+            RasterDatatype.nominal)
         make_simple_nominal_raster(nominal_raster.raster_path, shape)
 
         config_list = [
@@ -205,15 +219,15 @@ class RasterPlotConfig(unittest.TestCase):
         shape = (2, 8)
         continuous_raster_linear = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'continuous.tif'),
-            'continuous', 'linear')
+            RasterDatatype.continuous, RasterTransform.linear)
         continuous_raster_log = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'continuous.tif'),
-            'continuous', 'log')
+            RasterDatatype.continuous, RasterTransform.log)
         make_simple_raster(continuous_raster_linear.raster_path, shape)
 
         divergent_raster_log = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'divergent.tif'),
-            'divergent', 'log')
+            RasterDatatype.divergent, RasterTransform.log)
         divergent_array = numpy.linspace(
             -1, 1, num=numpy.multiply(*shape)).reshape(*shape)
         pygeoprocessing.numpy_array_to_raster(
@@ -230,15 +244,15 @@ class RasterPlotConfig(unittest.TestCase):
         compare_snapshots(reference, actual_png)
 
 
-class RasterPlotLegends(unittest.TestCase):
-    """Unit tests for legend placement on nominal rasters."""
+class RasterPlotLegendTests(unittest.TestCase):
+    """Snapshot tests for legend placement on nominal rasters."""
 
     def setUp(self):
         """Override setUp function to create temp workspace directory."""
         self.workspace_dir = tempfile.mkdtemp()
         self.raster_config = raster_utils.RasterPlotConfig(
             os.path.join(self.workspace_dir, 'foo.tif'),
-            'nominal', 'linear')
+            RasterDatatype.nominal, RasterTransform.linear)
 
     def tearDown(self):
         """Override tearDown function to remove temporary directory."""
@@ -270,9 +284,24 @@ class RasterPlotLegends(unittest.TestCase):
         save_figure(fig, actual_png)
         compare_snapshots(reference, actual_png)
 
+    def test_plot_raster_list_tall_nominal_many_classes(self):
+        """Test legend is multi-column on right side and has enough colors."""
+        figname = 'plot_raster_list_tall_nominal_many_classes.png'
+        reference = os.path.join(REFS_DIR, figname)
+        # More than 20 values will require a distinctipy-generated palette.
+        # More than 30 pixels will result in a multi-column legend.
+        shape = (9, 6)
+        make_simple_nominal_raster(self.raster_config.raster_path, shape)
 
-class RasterPlotFacets(unittest.TestCase):
-    """Unit tests for plotting multiple rasters on the same colorscale."""
+        config_list = [self.raster_config]
+        fig = raster_utils.plot_raster_list(config_list)
+        actual_png = os.path.join(self.workspace_dir, figname)
+        save_figure(fig, actual_png)
+        compare_snapshots(reference, actual_png)
+
+
+class RasterPlotFacetsTests(unittest.TestCase):
+    """Snapshot tests for plotting multiple rasters on the same colorscale."""
 
     def setUp(self):
         """Override setUp function to create temp workspace directory."""
@@ -289,8 +318,10 @@ class RasterPlotFacets(unittest.TestCase):
         shape = (4, 4)
         a_raster_filepath = os.path.join(self.workspace_dir, 'a.tif')
         b_raster_filepath = os.path.join(self.workspace_dir, 'b.tif')
-        a_array = numpy.linspace(0, 1, num=numpy.multiply(*shape)).reshape(*shape)
-        b_array = numpy.linspace(1, 2, num=numpy.multiply(*shape)).reshape(*shape)
+        a_array = numpy.linspace(
+            0, 1, num=numpy.multiply(*shape)).reshape(*shape)
+        b_array = numpy.linspace(
+            1, 2, num=numpy.multiply(*shape)).reshape(*shape)
         pygeoprocessing.numpy_array_to_raster(
             a_array, target_nodata=None, pixel_size=(1, 1), origin=(0, 0),
             projection_wkt=PROJ_WKT, target_path=a_raster_filepath)
@@ -299,13 +330,59 @@ class RasterPlotFacets(unittest.TestCase):
             projection_wkt=PROJ_WKT, target_path=b_raster_filepath)
 
         fig = raster_utils.plot_raster_facets(
-            [a_raster_filepath, b_raster_filepath], 'continuous')
+            [a_raster_filepath, b_raster_filepath], RasterDatatype.continuous)
         actual_png = os.path.join(self.workspace_dir, figname)
         save_figure(fig, actual_png)
         compare_snapshots(reference, actual_png)
 
 
-class RasterCaptions(unittest.TestCase):
+class RasterPlotUnitTextTests(unittest.TestCase):
+    """Snapshot tests for plotting rasters with unit text."""
+
+    def setUp(self):
+        """Override setUp function to create temp workspace directory."""
+        self.workspace_dir = tempfile.mkdtemp()
+        self.raster_config = raster_utils.RasterPlotConfig(
+            os.path.join(self.workspace_dir, 'test.tif'),
+            RasterDatatype.continuous, RasterTransform.linear)
+
+    def tearDown(self):
+        """Override tearDown function to remove temporary directory."""
+        shutil.rmtree(self.workspace_dir)
+
+    @patch('natcap.invest.reports.raster_utils._get_raster_units')
+    def test_plot_raster_list_unit_text_tall_aoi(self, mock_get_raster_units):
+        """Test unit text is above raster plot and not overlapping."""
+        mock_get_raster_units.return_value = 'flux capacitrons'
+        figname = 'plot_raster_list_unit_text_tall_aoi.png'
+        reference = os.path.join(REFS_DIR, figname)
+        shape = (8, 4)
+        make_simple_raster(self.raster_config.raster_path, shape)
+
+        config_list = [self.raster_config]*3
+        fig = raster_utils.plot_raster_list(config_list)
+        actual_png = os.path.join(self.workspace_dir, figname)
+        save_figure(fig, actual_png)
+        compare_snapshots(reference, actual_png)
+
+    @patch('natcap.invest.reports.raster_utils._get_raster_units')
+    def test_plot_raster_list_unit_text_wide_aoi(self, mock_get_raster_units):
+        """Test unit text is above raster plot and not overlapping."""
+        mock_get_raster_units.return_value = 'flux capacitrons'
+        figname = 'plot_raster_list_unit_text_wide_aoi.png'
+        reference = os.path.join(REFS_DIR, figname)
+        shape = (6, 12)
+        make_simple_raster(self.raster_config.raster_path, shape)
+
+        config_list = [self.raster_config]*2
+        fig = raster_utils.plot_raster_list(config_list)
+        actual_png = os.path.join(self.workspace_dir, figname)
+        save_figure(fig, actual_png)
+        compare_snapshots(reference, actual_png)
+
+
+class RasterCaptionTests(unittest.TestCase):
+    """Unit tests for caption-generating utility."""
 
     def test_generate_caption_from_raster_list(self):
         raster_list = [('raster_1', 'input'), ('raster_2', 'output')]
@@ -347,7 +424,8 @@ class RasterCaptions(unittest.TestCase):
         self.assertEqual(generated_caption, expected_caption)
 
 
-class TestRasterWorkspaceSummary(unittest.TestCase):
+class RasterWorkspaceSummaryTests(unittest.TestCase):
+    """Unit tests for output raster stats table-generating utility."""
 
     def setUp(self):
         """Override setUp function to create temp workspace directory."""
@@ -364,8 +442,10 @@ class TestRasterWorkspaceSummary(unittest.TestCase):
         # without running an invest model.
         args_dict = {'workspace_dir': self.workspace_dir}
         SAMPLE_MODEL_SPEC.create_output_directories(args_dict)
-        file_registry = fake_execute(SAMPLE_MODEL_SPEC.outputs, self.workspace_dir)
-        SAMPLE_MODEL_SPEC.generate_metadata_for_outputs(file_registry, args_dict)
+        file_registry = fake_execute(
+            SAMPLE_MODEL_SPEC.outputs, self.workspace_dir)
+        SAMPLE_MODEL_SPEC.generate_metadata_for_outputs(
+            file_registry, args_dict)
         dataframe = raster_utils.raster_workspace_summary(file_registry)
 
         # There are 2 rasters in the sample output spec
