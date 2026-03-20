6645ee4ce44127329ad468cda29edc133fee00ce
Megan Nissel <megnissel@gmail.com>
2026-03-20 14:51:17 -0400

    update MONTH_ID_TO_LABEL so that it is indexed in line with month numbers (to support consistent month indexing) (#2321)

--- src/natcap/invest/seasonal_water_yield/seasonal_water_yield.py
+++ src/natcap/invest/seasonal_water_yield/seasonal_water_yield.py
@@ -23,9 +23,9 @@ LOGGER = logging.getLogger(__name__)
 TARGET_NODATA = -1
 N_MONTHS = 12
 MONTH_RANGE = range(1, N_MONTHS+1)
-MONTH_ID_TO_LABEL = [
-    'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct',
-    'nov', 'dec']
+MONTH_ID_TO_LABEL = {
+    1: 'jan', 2: 'feb', 3: 'mar', 4: 'apr', 5: 'may', 6: 'jun',
+    7: 'jul', 8: 'aug', 9: 'sep', 10: 'oct',  11: 'nov', 12: 'dec'}
 
 _model_description = gettext(
     """
