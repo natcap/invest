2418d86b75a5ce46ffc66bca0870ca3b7d818648
Claire Simpson <cesimpson@stanford.edu>
2026-05-22 17:19:08 -0600

    UNA report tests #2534

--- tests/reports/test_urban_nature_access_template.py
+++ tests/reports/test_urban_nature_access_template.py
@@ -38,42 +38,43 @@ def _get_render_args(model_spec):
         'userguide_page': model_spec.userguide,
         'timestamp': timestamp,
         'args_dict': args_dict,
-        'agg_results_table': agg_results_table,
+        # 'agg_results_table': agg_results_table,
         'inputs_img_src': img_src,
         'inputs_caption': inputs_caption,
         'outputs_img_src': img_src,
         'outputs_caption': outputs_caption,
-        'intermediate_baseline_img_src': img_src,
-        'intermediate_baseline_img_caption': test_caption,
-        'intermediate_delta_ndvi_img_src': img_src,
-        'intermediate_delta_ndvi_img_caption': test_caption,
-        'intermediate_reclass_lulc_img_src': img_src,
-        'intermediate_reclass_lulc_img_caption': test_caption,
+        'output_aggregated_img_src': img_src,
+        'output_aggregated_raster_caption': test_caption,
         'raster_group_caption': test_caption,
         'lulc_pre_caption': lulc_pre_caption,
         'output_raster_stats_table': output_stats_table,
         'input_raster_stats_table': input_stats_table,
         'stats_table_note': stats_table_note,
-        'cases_map_json': vegalite_json,
-        'cost_map_json': vegalite_json,
-        'cases_map_caption': test_caption,
-        'cost_map_caption': test_caption,
         'aggregate_map_source_list': agg_map_source_list,
+        # 'vector_plot_list': [("fakeid", vegalite_json, test_caption)],
+        'admin_map_json': vegalite_json,
+        'vector_plot_captions': test_caption,
+        'admin_boundaries_map_source_list': agg_map_source_list,
         'model_spec_outputs': model_spec.outputs,
+        'output_accessible_img_src': img_src,
+        'output_accessible_raster_caption': test_caption,
+        'output_balance_img_src': img_src,
+        'output_balance_raster_caption': test_caption,
+        'output_balance_percapita_img_src': img_src,
+        'output_balance_percapita_raster_caption': test_caption,
+        'intermediates_img_src': img_src,
+        'intermediates_caption': test_caption
     }
 
 
 class UrbanNatureAccessTemplateTests(unittest.TestCase):
     """Unit tests for Urban Nature Access template."""
 
-    def test_render_lulc_option_without_cost(self):
+    def test_render_search_radius_by_lulc_option(self):
         """Test report rendering with LULC option without health_cost_rate"""
 
         render_args = _get_render_args(MODEL_SPEC)
-        render_args['args_dict']['model_option'] = 'lulc'
-        render_args['args_dict']['lulc_base'] = 'lulc_base.tif'
-        # if no health_cost_rate, no cost_map produced
-        render_args['cost_map_json'] = None
+        render_args['args_dict']['model_option'] = 'radius per urban nature class'
         html = TEMPLATE.render(render_args)
         soup = BeautifulSoup(html, BSOUP_HTML_PARSER)
         sections = soup.find_all(class_='accordion-section')
@@ -82,30 +83,28 @@ class UrbanNatureAccessTemplateTests(unittest.TestCase):
             soup.h1.string, f'InVEST Results: {MODEL_SPEC.model_title}')
 
         # should only have plot for preventable cases, not preventable cost
-        vega_plots = soup.find_all(class_='vega-fit-horizontal')
+        vega_plots = soup.find_all(class_='.vega-embed')  # TODO - in UMH test remove vega fit horizontal
         self.assertEqual(len(vega_plots), 1)
 
-    def test_render_lulc_option_with_ndvi(self):
+    def test_render_search_radius_by_pop_option(self):
         """Test report rendering with LULC option with baseline NDVI input"""
         render_args = _get_render_args(MODEL_SPEC)
-        render_args['args_dict']['model_option'] = 'lulc'
-        render_args['args_dict']['lulc_base'] = 'lulc_base.tif'
-        render_args['args_dict']['ndvi_base'] = 'ndvi_base.tif'
+        render_args['args_dict']['search_radius_mode'] = 'radius per population group'
         html = TEMPLATE.render(render_args)
         soup = BeautifulSoup(html, BSOUP_HTML_PARSER)
         sections = soup.find_all(class_='accordion-section')
         self.assertEqual(len(sections), 11)
 
-    def test_render_ndvi_option_with_cost(self):
-        """Test report rendering with NDVI model option with cost"""
-        render_args = _get_render_args(MODEL_SPEC)
-        render_args['args_dict']['model_option'] = 'ndvi'
-        html = TEMPLATE.render(render_args)
-        soup = BeautifulSoup(html, BSOUP_HTML_PARSER)
-        sections = soup.find_all(class_='accordion-section')
-        # Does not have reclassified LULC to NDVI section
-        self.assertEqual(len(sections), 10)
+    # def test_render_ndvi_option_with_cost(self):
+    #     """Test report rendering with NDVI model option with cost"""
+    #     render_args = _get_render_args(MODEL_SPEC)
+    #     render_args['args_dict']['model_option'] = 'ndvi'
+    #     html = TEMPLATE.render(render_args)
+    #     soup = BeautifulSoup(html, BSOUP_HTML_PARSER)
+    #     sections = soup.find_all(class_='accordion-section')
+    #     # Does not have reclassified LULC to NDVI section
+    #     self.assertEqual(len(sections), 10)
 
-        # should have plots for both preventable cases and cost
-        vega_plots = soup.find_all(class_='vega-fit-horizontal')
-        self.assertEqual(len(vega_plots), 2)
+    #     # should have plots for both preventable cases and cost
+    #     vega_plots = soup.find_all(class_='vega-fit-horizontal')
+    #     self.assertEqual(len(vega_plots), 2)
