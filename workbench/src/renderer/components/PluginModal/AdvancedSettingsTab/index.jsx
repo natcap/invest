7f14855027e28784fca29d262e07776ce4f050f3
Megan Nissel <megnissel@gmail.com>
2026-09-24 14:36:12 -0400

    drop unused propTypes imports

--- workbench/src/renderer/components/PluginModal/AdvancedSettingsTab/index.jsx
+++ workbench/src/renderer/components/PluginModal/AdvancedSettingsTab/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
--- workbench/src/renderer/components/PluginModal/PluginRegistryTab/PluginDetailPane/index.jsx
+++ workbench/src/renderer/components/PluginModal/PluginRegistryTab/PluginDetailPane/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 
 import { useTranslation } from 'react-i18next';
 
